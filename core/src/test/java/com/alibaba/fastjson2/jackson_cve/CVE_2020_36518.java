50c4704dc57848ddb3411763be7ff450270db5b2
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-19 23:43:49 +0800

    add testcase

---
+++ core/src/test/java/com/alibaba/fastjson2/jackson_cve/CVE_2020_36518.java
@@ -0,0 +1,45 @@
+package com.alibaba.fastjson2.jackson_cve;
+
+import com.alibaba.fastjson2.JSON;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CVE_2020_36518 {
+    // 2000 passes, 3000 fails
+    private final static int TOO_DEEP_NESTING = 4000;
+
+    @Test
+    public void testWithArray() throws Exception {
+        final String doc = _nestedDoc(TOO_DEEP_NESTING, "[ ", "] ");
+        Object ob = JSON.parseObject(doc, Object.class);
+        assertTrue(ob instanceof List<?>);
+    }
+
+    @Test
+    public void testWithObject() throws Exception {
+        final String doc = "{" + _nestedDoc(TOO_DEEP_NESTING, "\"x\":{", "} ") + "}";
+        Object ob = JSON.parseObject(doc, Object.class);
+        assertTrue(ob instanceof Map<?, ?>);
+    }
+
+    private String _nestedDoc(int nesting, String open, String close) {
+        StringBuilder sb = new StringBuilder(nesting * (open.length() + close.length()));
+        for (int i = 0; i < nesting; ++i) {
+            sb.append(open);
+            if ((i & 31) == 0) {
+                sb.append("\n");
+            }
+        }
+        for (int i = 0; i < nesting; ++i) {
+            sb.append(close);
+            if ((i & 31) == 0) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+}
