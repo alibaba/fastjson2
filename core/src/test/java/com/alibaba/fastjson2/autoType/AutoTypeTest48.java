e29ac8e05484888c1b10c6e93121739e73c74cd5
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-19 23:30:59 +0800

    add testcase

---
+++ core/src/test/java/com/alibaba/fastjson2/autoType/AutoTypeTest48.java
@@ -0,0 +1,22 @@
+package com.alibaba.fastjson2.autoType;
+
+import com.alibaba.fastjson2.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AutoTypeTest48 {
+    @Test
+    public void test_0() throws Exception {
+        assertThrows(JSONException.class, () -> {
+                    JSON.parse((String) JSONB
+                            .parse(Base64.getDecoder()
+                            .decode("eThueyJAdHlwZSI6Iltjb20uc3VuLnJvd3NldC5KZGJjUm93U2V0SW1wbCIsWyJkYXRhU291cmNlTmFtZSI6ImxkYXA6Ly8xMjcuMC4wLjE6MTM4OS9qcnRmbnkiLCJhdXRvQ29tbWl0Ijp0cnVlXX0=")
+                            ), JSONReader.Feature.SupportAutoType
+                    );
+                }
+        );
+    }
+}
