835334d586534efafcd3a2ce6b69e189f28dbc83
业余布道师 <Victor.Zxy@outlook.com>
2022-04-22 15:27:41 +0800

    add GenericFastJsonRedisSerializer and FastJsonRedisSerializer for compatibility with fastjson 1.x.

---
+++ fastjson1_compatible/src/main/java/com/alibaba/fastjson/support/spring/GenericFastJsonRedisSerializer.java
@@ -0,0 +1,42 @@
+package com.alibaba.fastjson.support.spring;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+/**
+ * Fastjson for Spring Data Redis Serializer(Generic implement).
+ * <p>
+ * Compatible fastjson 1.2.x
+ *
+ * @author lihengming
+ * @author Victor.Zxy
+ * @see RedisSerializer
+ * @since 2.0.2
+ */
+public class GenericFastJsonRedisSerializer implements RedisSerializer<Object> {
+
+    public byte[] serialize(Object object) throws SerializationException {
+        if (object == null) {
+            return new byte[0];
+        }
+        try {
+            return JSON.toJSONBytes(object, SerializerFeature.WriteClassName);
+        } catch (Exception ex) {
+            throw new SerializationException("Could not serialize: " + ex.getMessage(), ex);
+        }
+    }
+
+    public Object deserialize(byte[] bytes) throws SerializationException {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        try {
+            return JSON.parseObject(bytes, Object.class, Feature.SupportAutoType);
+        } catch (Exception ex) {
+            throw new SerializationException("Could not deserialize: " + ex.getMessage(), ex);
+        }
+    }
+}
