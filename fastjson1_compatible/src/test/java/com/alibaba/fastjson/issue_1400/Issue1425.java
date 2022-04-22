b641cf037cb72b208d47a05571a42707eb5ab4af
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-22 13:37:30 +0800

    junit4 -> junit5

--- fastjson1_compatible/src/test/java/com/alibaba/fastjson/issue_1400/Issue1425.java
+++ fastjson1_compatible/src/test/java/com/alibaba/fastjson/issue_1400/Issue1425.java
@@ -2,9 +2,10 @@ package com.alibaba.fastjson.issue_1400;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
-public class Issue1425 extends TestCase {
+public class Issue1425 {
+    @Test
     public void test_for_issue() throws Exception {
         DicDomain dicDomain = new DicDomain();
         dicDomain.setCode("A001");
