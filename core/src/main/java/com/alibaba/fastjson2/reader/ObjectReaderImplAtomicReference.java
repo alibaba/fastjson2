c6ae659ae200b63578d9f136036e3ed8cec46af8
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-23 12:00:14 +0800

    improved AtomicXXX support

---
+++ core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderImplAtomicReference.java
@@ -0,0 +1,37 @@
+package com.alibaba.fastjson2.reader;
+
+import com.alibaba.fastjson2.JSONReader;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+final class ObjectReaderImplAtomicReference extends ObjectReaderBaseModule.PrimitiveImpl {
+    static final ObjectReaderImplAtomicReference INSTANCE = new ObjectReaderImplAtomicReference(Object.class);
+
+    final Type referenceType;
+
+    public ObjectReaderImplAtomicReference(Type referenceType) {
+        this.referenceType = referenceType;
+    }
+
+    @Override
+    public Object readObject(JSONReader jsonReader, long features) {
+        if (jsonReader.nextIfNull()) {
+            return null;
+        }
+
+        Object value = jsonReader.read(referenceType);
+        return new AtomicReference<>(value);
+    }
+
+    @Override
+    public Object readJSONBObject(JSONReader jsonReader, long features) {
+        if (jsonReader.nextIfNull()) {
+            return null;
+        }
+
+        Object value = jsonReader.read(referenceType);
+        return new AtomicReference<>(value);
+    }
+}
---
+++ core/src/main/java/com/alibaba/fastjson2/writer/ObjectWriterImplAtomicReference.java
@@ -0,0 +1,51 @@
+package com.alibaba.fastjson2.writer;
+
+import com.alibaba.fastjson2.JSONB;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.util.Fnv;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+final class ObjectWriterImplAtomicReference extends ObjectWriterBaseModule.PrimitiveImpl {
+    static final ObjectWriterImplAtomicReference INSTANCE = new ObjectWriterImplAtomicReference(null);
+
+    final Class defineClass;
+
+    public ObjectWriterImplAtomicReference(Class defineClass) {
+        this.defineClass = defineClass;
+    }
+
+    @Override
+    public void writeJSONB(JSONWriter jsonWriter, Object object, Object fieldName, Type fieldType, long features) {
+        if (object == null) {
+            jsonWriter.writeNull();
+            return;
+        }
+
+        AtomicReference atomic = (AtomicReference) object;
+        Object ref = atomic.get();
+        if (ref == null) {
+            jsonWriter.writeNull();
+        }
+
+        jsonWriter.writeAny(ref);
+    }
+
+    @Override
+    public void write(JSONWriter jsonWriter, Object object, Object fieldName, Type fieldType, long features) {
+        if (object == null) {
+            jsonWriter.writeNull();
+            return;
+        }
+
+        AtomicReference atomic = (AtomicReference) object;
+        Object ref = atomic.get();
+        if (ref == null) {
+            jsonWriter.writeNull();
+        }
+
+        jsonWriter.writeAny(ref);
+    }
+}
