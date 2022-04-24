75dea4a90271952d14b75098a98ab318f0c9d627
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-24 20:37:47 +0800

    bug fix for jsonb autoType support

---
+++ core/src/test/java/com/alibaba/fastjson2/jsonb/ExceptionTest.java
@@ -0,0 +1,32 @@
+package com.alibaba.fastjson2.jsonb;
+
+import com.alibaba.fastjson2.JSONB;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExceptionTest {
+    @Test
+    public void test_exception() {
+        Object[] objects = new Object[]{new RuntimeException(), new IOException()};
+        byte[] bytes = JSONB.toBytes(objects, JSONWriter.Feature.WriteClassName,
+                JSONWriter.Feature.FieldBased,
+                JSONWriter.Feature.ReferenceDetection,
+                JSONWriter.Feature.WriteNulls,
+                JSONWriter.Feature.NotWriteDefaultValue,
+                JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+                JSONWriter.Feature.WriteNameAsSymbol);
+
+        System.out.println(JSONB.toJSONString(bytes));
+
+        Object[] parsed = JSONB.parseObject(bytes, objects.getClass(), JSONReader.Feature.SupportAutoType,
+                JSONReader.Feature.UseNativeObject,
+                JSONReader.Feature.FieldBased);
+        assertEquals(objects.length, parsed.length);
+
+    }
+}
