43da992875d7095f6d5f3ae386a29f22abd0c5ed
wangxu <495914790@qq.com>
2022-04-26 21:50:19 +0800

    add testcase

---
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_4000/Issue4069.java
@@ -0,0 +1,25 @@
+package com.alibaba.fastjson2.v1issues.issue_4000;
+
+import com.alibaba.fastjson.JSONValidator;
+import com.alibaba.fastjson2.JSON;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Issue4069 {
+    @Test
+    public void test_for_issue4069() {
+        String json_string = "[[{\"value\": \"aaa\",\"key\": \"input_aKLYFkHNhPk0\"},{\"value\": \"222\",\"key\": \"number_pPvGTENKofUM\"}]," +
+                "[{\"value\": \"ffdf\",\"key\": \"input_aKLYFkHNhPk0\"},{\"value\": \"1212\",\"key\": \"number_pPvGTENKofUM\"}]]";
+
+        assertTrue(JSON.isValidArray(json_string));
+        assertEquals("Array", JSONValidator.from(json_string).getType().toString());
+
+        assertTrue(JSON.isValidArray("[[]]"));
+        assertEquals("Array", JSONValidator.from("[[]]").getType().toString());
+
+        assertFalse(JSON.isValidArray("["));
+        assertFalse(JSON.isValidArray("[[]"));
+    }
+
+}
