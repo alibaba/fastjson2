4b89c57e5d702365fffa98b87a14f94913751de9
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-26 18:36:35 +0800

    improved compatibility for fastjson 1.x

---
+++ core/src/test/java/com/alibaba/fastjson2/OverrideTest.java
@@ -0,0 +1,30 @@
+package com.alibaba.fastjson2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OverrideTest {
+    @Test
+    public void test_nullAsEmptyStr() {
+        B b = new B();
+
+        String json = JSON.toJSONString(b, JSONWriter.Feature.WriteNulls, JSONWriter.Feature.NullAsDefaultValue);
+        assertEquals("{\"value\":\"\"}", json);
+    }
+
+    public static abstract class A {
+        public abstract Object getValue();
+    }
+
+    public static class B extends A {
+        private String value;
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+}
--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3600/Issue3655.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3600/Issue3655.java
@@ -1,4 +1,4 @@
-package com.alibaba.json.bvt.issue_3600;
+package com.alibaba.fastjson2.v1issues.issue_3600;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
