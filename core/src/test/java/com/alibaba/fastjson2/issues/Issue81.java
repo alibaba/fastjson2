1666f84024e3416ef583b5bda52ecb10fb88d7a9
wangxu <495914790@qq.com>
2022-04-26 13:44:37 +0800

    bug fixed for json isValid,fix #81

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue81.java
@@ -0,0 +1,25 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.JSONReader;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Issue81 {
+    @Test
+    public void test_issue() {
+        String jsono = "{\"a\":1}";
+        String jsona = "[{\"a\":1}]";
+        String value = "1111";
+        assertFalse(JSON.isValid(jsona));
+        assertTrue(JSON.isValid(jsono));
+        assertFalse(JSON.isValidArray(jsono));
+        assertTrue(JSON.isValidArray(jsona));
+        assertFalse(JSON.isValid(value));
+        assertFalse(JSON.isValidArray(value));
+    }
+}
