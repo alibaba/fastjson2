ece5d6a0399b6534029abb94bb91ec911898e3df
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-27 20:57:59 +0800

    add testcasse for #89

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue89.java
@@ -0,0 +1,55 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.TypeReference;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class Issue89 {
+    @Test
+    public void fetchReward() {
+        String str = "{ \n" +
+                "    \"_id\" : \"6195f08f5b210000a7002c14\", \n" +
+                "    \"activityContent\" : {\n" +
+                "        \"awardRule\" : {\n" +
+                "            \"ruleType\" : \"RANDOM\", \n" +
+                "            \"ruleData\" : [\n" +
+                "                {\n" +
+                "                    \"amount\" : \"0.33\", \n" +
+                "                    \"percentage\" : \"0.70\"\n" +
+                "                }, \n" +
+                "                {\n" +
+                "                    \"amount\" : \"0.66\", \n" +
+                "                    \"percentage\" : \"0.15\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+
+        JSONObject object = JSON.parseObject(str);
+
+        ActivityDO activityDO = object.toJavaObject(ActivityDO.class);
+        JSONObject activityContent = activityDO.getActivityContent();
+        // 这里getJSONObject就会报错
+        JSONObject awardRule = activityContent.getJSONObject("awardRule");
+        assertNotNull(awardRule);
+    }
+
+    public static class BaseDO {
+
+    }
+
+    @Getter
+    @Setter
+    public static class ActivityDO extends BaseDO {
+        private JSONObject activityContent;
+    }
+}
