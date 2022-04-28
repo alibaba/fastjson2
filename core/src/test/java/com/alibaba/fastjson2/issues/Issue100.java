1900b564c6675f51e9eb1caa116ba1bd6be1580f
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-28 15:30:11 +0800

    bug fix for long parse, fix #100

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue100.java
@@ -0,0 +1,70 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.primitves.Int64Field_1;
+import com.alibaba.fastjson2_vo.Long1;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue100 {
+    @Test
+    public void test_int() {
+        JSONObject json = new JSONObject();
+        json.put("t1", 3234567890L);
+
+        String tmp = json.toString();
+        JSONObject json2 = JSON.parseObject(tmp);
+        assertEquals("{\"t1\":3234567890}",json2.toString());
+    }
+
+    @Test
+    public void test_int_neg() {
+        JSONObject json = new JSONObject();
+        json.put("t1", -3234567890L);
+
+        String tmp = json.toString();
+        JSONObject json2 = JSON.parseObject(tmp);
+        assertEquals("{\"t1\":-3234567890}",json2.toString());
+    }
+
+    @Test
+    public void test_int_loop() {
+        long valueInit = 3234567890L - 1000 * 1000;
+
+        for (int i = 0; i < 10000; ++i) {
+            long val = valueInit + i * 100000L;
+            JSONObject json = new JSONObject();
+            json.put("t1", val);
+
+            String tmp = json.toString();
+            JSONObject json2 = JSON.parseObject(tmp);
+            assertEquals("{\"t1\":" + val + "}", json2.toString());
+        }
+    }
+
+    @Test
+    public void test_int_loop_neg() {
+        long valueInit = -3234567890L + 1000 * 1000;
+
+        for (int i = 0; i < 10000; ++i) {
+            long val = valueInit - i * 100000L;
+            JSONObject json = new JSONObject();
+            json.put("t1", val);
+
+            String tmp = json.toString();
+            JSONObject json2 = JSON.parseObject(tmp);
+            assertEquals("{\"t1\":" + val + "}", json2.toString());
+        }
+    }
+
+    @Test
+    public void test_long1() {
+        JSONObject json = new JSONObject();
+        json.put("v0000", 3234567890L);
+
+        Long1 vo = json.toJavaObject(Long1.class);
+        assertEquals(3234567890L, vo.getV0000());
+    }
+}
