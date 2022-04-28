40044d248922142567d7a47c024893f4548ab784
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-28 23:29:40 +0800

    fix testcase

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1200/Issue1265.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1200/Issue1265.java
@@ -2,7 +2,7 @@ package com.alibaba.fastjson.issue_1200;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
