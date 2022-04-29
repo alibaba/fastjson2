e85e0f7ffd01b72f785d47eb14a2507ae5af756d
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-29 20:34:51 +0800

    improved compatibility for fastjson 1.x

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1600/Issue1660.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1600/Issue1660.java
@@ -1,17 +1,23 @@
-package com.alibaba.json.bvt.issue_1600;
+package com.alibaba.fastjson.issue_1600;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-public class Issue1660 extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue1660 {
+    @BeforeEach
     protected void setUp() throws Exception {
         JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
         JSON.defaultLocale = Locale.CHINA;
     }
 
+    @Test
     public void test_for_issue() throws Exception {
         Model model = new Model();
         model.values.add(new Date(1513755213202L));
