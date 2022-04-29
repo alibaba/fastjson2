f2644efbc55697749c75d20b6cf71d7c6f3aab9e
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-29 19:37:49 +0800

    add JSONReader new features 'UseBigDecimalForFloats' & 'UseBigDecimalForDoubles', fix #106

---
+++ core/src/test/java/com/alibaba/fastjson2/features/UseBigDecimalForFloats.java
@@ -0,0 +1,29 @@
+package com.alibaba.fastjson2.features;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UseBigDecimalForFloats {
+    @Test
+    public void decimal_as_float() {
+        String str = "{\"val\":1.2}";
+        JSONObject object = JSON.parseObject(str, JSONReader.Feature.UseBigDecimalForFloats);
+        Object val = object.get("val");
+        assertNotNull(val);
+        assertEquals(Float.class, val.getClass());
+    }
+
+    @Test
+    public void decimal_as_double() {
+        String str = "{\"val\":1.2}";
+        JSONObject object = JSON.parseObject(str, JSONReader.Feature.UseBigDecimalForDoubles);
+        Object val = object.get("val");
+        assertNotNull(val);
+        assertEquals(Double.class, val.getClass());
+    }
+}
