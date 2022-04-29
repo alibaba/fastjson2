71837d9fad5fcb9ea3d5af4537d8c28d4d63c9be
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-29 23:50:16 +0800

    improved compatibility for fastjson 1.x

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1800/Issue1821C.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1800/Issue1821C.java
@@ -1,11 +1,14 @@
-package com.alibaba.json.bvt.issue_1800;
+package com.alibaba.fastjson2.v1issues.issue_1800;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.annotation.JSONType;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
-public class Issue1821 extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue1821C {
+    @Test
     public void test_for_issue() throws Exception {
         String str = "{\"type\":800,\"data\":\"HuYgMIxwfqdtvOJNv6kK025g5fh3yFHI2kaByO7udKk6FOBC3PGRWkGfwV0\\/vWQW6roN5ftKDHFZ3PWl0715OYue0rZj\\/VwrNsMvIL4MqTUNBBUGFU9SgZu87ss7RqmyijH6\\/sM968cK1Dv5U7Rrw79idl\\/hW8SILLn1YXvUa60=\"}";
         String expectStr = "{\"type\":800,\"data\":\"HuYgMIxwfqdtvOJNv6kK025g5fh3yFHI2kaByO7udKk6FOBC3PGRWkGfwV0/vWQW6roN5ftKDHFZ3PWl0715OYue0rZj/VwrNsMvIL4MqTUNBBUGFU9SgZu87ss7RqmyijH6/sM968cK1Dv5U7Rrw79idl/hW8SILLn1YXvUa60=\"}";
