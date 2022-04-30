33836d182fde42d330d154980b4d4a4dd534e3db
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-30 13:25:00 +0800

    add testcase

---
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1900/Issue1945.java
@@ -0,0 +1,32 @@
+package com.alibaba.fastjson2.v1issues.issue_1900;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.alibaba.fastjson2.JSONWriter.Feature.WriteClassName;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class Issue1945 {
+    @Test
+    public void test_0() throws Exception {
+        B b = new B();
+        b.clazz = new Class[]{String.class};
+        b.aInstance = new HashMap();
+        b.aInstance.put("test", "test");
+        String s = JSON.toJSONString(b, WriteClassName);
+        System.out.println(s);
+        B a1 = JSON.parseObject(s, B.class, JSONReader.Feature.SupportClassForName);
+        assertNotNull(a1);
+    }
+
+    static class B implements Serializable {
+        public Class[] clazz;
+        public Map aInstance;
+    }
+}
