36050efeb76af34dc4e9f2004d0bd0452b54f2bb
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-04-30 21:01:09 +0800

    add testcase

---
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_2400/Issue2428.java
@@ -0,0 +1,35 @@
+package com.alibaba.fastjson2.v1issues.issue_2400;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.annotation.JSONType;
+import com.alibaba.fastjson2.annotation.NamingStrategy;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@NoArgsConstructor
+@Data
+@JSONType(naming = NamingStrategy.SnakeCase)
+public class Issue2428 {
+    private String myName;
+    private NestedBean nestedBean;
+
+    @AllArgsConstructor
+    @Data
+    @JSONType(naming = NamingStrategy.SnakeCase)
+    public static class NestedBean {
+        private String myId;
+    }
+
+    @Test
+    public void test_for_issue() {
+        Issue2428 demoBean = new Issue2428();
+        demoBean.setMyName("test name");
+        demoBean.setNestedBean(new NestedBean("test id"));
+        String text = JSON.toJSONString(JSON.toJSON(demoBean));
+        assertEquals("{\"my_name\":\"test name\",\"nested_bean\":{\"my_id\":\"test id\"}}", text);
+    }
+}
--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2400/Issue2428.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2400/Issue2428.java
@@ -1,36 +1,36 @@
-package com.alibaba.json.bvt.issue_2400;
+package com.alibaba.fastjson.issue_2400;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.PropertyNamingStrategy;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import junit.framework.TestCase;
+import com.alibaba.fastjson.annotation.JSONType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @NoArgsConstructor
 @Data
-public class Issue2428 extends TestCase {
+@JSONType(naming = PropertyNamingStrategy.SnakeCase)
+public class Issue2428 {
     private String myName;
     private NestedBean nestedBean;
 
     @AllArgsConstructor
     @Data
+    @JSONType(naming = PropertyNamingStrategy.SnakeCase)
     public static class NestedBean {
         private String myId;
     }
 
+    @Test
     public void test_for_issue() {
         Issue2428 demoBean = new Issue2428();
         demoBean.setMyName("test name");
         demoBean.setNestedBean(new NestedBean("test id"));
         String text = JSON.toJSONString(JSON.toJSON(demoBean), SerializerFeature.SortField);
-        assertEquals("{\"nestedBean\":{\"myId\":\"test id\"},\"myName\":\"test name\"}", text);
-
-        SerializeConfig serializeConfig = new SerializeConfig();
-        serializeConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
-        text = JSON.toJSONString(JSON.toJSON(demoBean), SerializerFeature.SortField);
         assertEquals("{\"my_name\":\"test name\",\"nested_bean\":{\"my_id\":\"test id\"}}", text);
     }
 }
