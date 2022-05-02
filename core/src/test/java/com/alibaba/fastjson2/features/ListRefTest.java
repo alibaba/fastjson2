8e136e72c5cac63ddea6330a1aff40cc2318dec6
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-02 18:27:38 +0800

    list field support reference

---
+++ core/src/test/java/com/alibaba/fastjson2/features/ListRefTest.java
@@ -0,0 +1,34 @@
+package com.alibaba.fastjson2.features;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListRefTest {
+    @Test
+    public void test() {
+        Bean bean = new Bean();
+        bean.items = new ArrayList<>();
+
+        Item item = new Item();
+        bean.items.add(item);
+        bean.items.add(item);
+
+        assertEquals("{\"items\":[{},{\"$ref\":\"$[0]\"}]}",
+                JSON.toJSONString(bean, JSONWriter.Feature.ReferenceDetection)
+        );
+    }
+
+    public static class Bean {
+        public List<Item> items;
+    }
+
+    public static class Item {
+
+    }
+}
