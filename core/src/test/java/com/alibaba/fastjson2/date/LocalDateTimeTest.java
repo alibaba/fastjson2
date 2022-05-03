4387f3a4ad79a1a4503c5839db124ddc8503a68b
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-03 15:35:48 +0800

    add testcase

--- core/src/test/java/com/alibaba/fastjson2/date/LocalDateTimeTest.java
+++ core/src/test/java/com/alibaba/fastjson2/date/LocalDateTimeTest.java
@@ -1,6 +1,7 @@
 package com.alibaba.fastjson2.date;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.annotation.JSONField;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,25 @@ public class LocalDateTimeTest {
 
     public static class Student {
         @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+        public LocalDateTime birthday;
+    }
+
+    @Test
+    public void test1() {
+        String str = "{\"birthday\":\"2022-05-03T15:26:05\"}";
+        Student1 student = JSON.parseObject(str, Student1.class);
+        assertEquals(str, JSON.toJSONString(student));
+    }
+
+    @Test
+    public void test1_null() {
+        String str = "{\"birthday\":null}";
+        Student1 student = JSON.parseObject(str, Student1.class);
+        assertEquals(str, JSON.toJSONString(student, JSONWriter.Feature.WriteNulls));
+    }
+
+    public static class Student1 {
+        @JSONField(format = "iso8601")
         public LocalDateTime birthday;
     }
 }
