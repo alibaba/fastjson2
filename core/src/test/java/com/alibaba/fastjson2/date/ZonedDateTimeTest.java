79b6d3625b62ec96008aa5877049c9faf19f17de
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-03 15:52:56 +0800

    bug fix for ZonedDateTime serialize

--- core/src/test/java/com/alibaba/fastjson2/date/ZonedDateTimeTest.java
+++ core/src/test/java/com/alibaba/fastjson2/date/ZonedDateTimeTest.java
@@ -5,11 +5,11 @@ import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.annotation.JSONField;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LocalDateTimeTest {
+public class ZonedDateTimeTest {
     @Test
     public void test() {
         String str = "{\"birthday\":\"2022-05-03 15:26:05\"}";
@@ -19,14 +19,23 @@ public class LocalDateTimeTest {
 
     public static class Student {
         @JSONField(format = "yyyy-MM-dd HH:mm:ss")
-        public LocalDateTime birthday;
+        public ZonedDateTime birthday;
     }
 
     @Test
     public void test1() {
         String str = "{\"birthday\":\"2022-05-03T15:26:05\"}";
         Student1 student = JSON.parseObject(str, Student1.class);
-        assertEquals(str, JSON.toJSONString(student));
+        String str2 = JSON.toJSONString(student);
+        Student1 student1 = JSON.parseObject(str2, Student1.class);
+        assertEquals(student.birthday.getYear(), student1.birthday.getYear());
+        assertEquals(student.birthday.getMonthValue(), student1.birthday.getMonthValue());
+        assertEquals(student.birthday.getDayOfMonth(), student1.birthday.getDayOfMonth());
+        assertEquals(student.birthday.getHour(), student1.birthday.getHour());
+        assertEquals(student.birthday.getMinute(), student1.birthday.getMinute());
+        assertEquals(student.birthday.getSecond(), student1.birthday.getSecond());
+        assertEquals(student.birthday.getNano(), student1.birthday.getNano());
+        assertEquals(student.birthday.toInstant().toEpochMilli(), student1.birthday.toInstant().toEpochMilli());
     }
 
     @Test
@@ -38,7 +47,7 @@ public class LocalDateTimeTest {
 
     public static class Student1 {
         @JSONField(format = "iso8601")
-        public LocalDateTime birthday;
+        public ZonedDateTime birthday;
     }
 
     @Test
@@ -49,6 +58,6 @@ public class LocalDateTimeTest {
     }
 
     public static class Student2 {
-        public LocalDateTime birthday;
+        public ZonedDateTime birthday;
     }
 }
