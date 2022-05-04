34483769d647aa7f0e902664b5de0270a266357e
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-04 08:59:08 +0800

    add testcase

--- core/src/test/java/com/alibaba/fastjson2/jackson_support/JacksonJsonValueTest.java
+++ core/src/test/java/com/alibaba/fastjson2/jackson_support/JacksonJsonValueTest.java
@@ -22,4 +22,26 @@ public class JacksonJsonValueTest {
             this.name = name;
         }
     }
+
+    @Test
+    public void whenSerializingUsingJsonValue_thenCorrect() {
+        assertEquals("\"Type A\"", JSON.toJSONString(TypeEnumWithValue.TYPE1));
+    }
+
+    public enum TypeEnumWithValue {
+        TYPE1(1, "Type A"), TYPE2(2, "Type 2");
+
+        private Integer id;
+        private String name;
+
+        TypeEnumWithValue(Integer id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @JsonValue
+        public String getName() {
+            return name;
+        }
+    }
 }
