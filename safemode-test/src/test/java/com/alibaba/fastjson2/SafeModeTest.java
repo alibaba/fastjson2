579fabc8d9754cff273d53a0bca1a5be209c9f27
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-04 19:01:23 +0800

    add testcase

--- safemode-test/src/test/java/com/alibaba/fastjson2/SafeModeTest.java
+++ safemode-test/src/test/java/com/alibaba/fastjson2/SafeModeTest.java
@@ -2,8 +2,10 @@ package com.alibaba.fastjson2;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SafeModeTest {
@@ -18,5 +20,17 @@ public class SafeModeTest {
 
     public static class Bean {
 
+    }
+
+    @Test
+    public void test1() {
+        IOException ex = new IOException();
+        String jsonString = JSON.toJSONString(ex, JSONWriter.Feature.WriteClassName);
+        Throwable e1 = JSON.parseObject(jsonString, Throwable.class);
+        assertEquals(Throwable.class, e1.getClass());
+
+        JSONObject object = JSON.parseObject(jsonString);
+        Throwable e2 = object.toJavaObject(Throwable.class);
+        assertEquals(Throwable.class, e2.getClass());
     }
 }
