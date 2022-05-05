a16298e13e6d5e458a470071e10276ea05fe1c43
风起 <chengtao.yct@alibaba-inc.com>
2022-05-05 20:31:32 +0800

    fix randomIndex throw exception when use JSONPath.of(JSONReader)

--- core/src/test/java/com/alibaba/fastjson2/jsonpath/JSONPahRandomIndex.java
+++ core/src/test/java/com/alibaba/fastjson2/jsonpath/JSONPahRandomIndex.java
@@ -2,6 +2,8 @@ package com.alibaba.fastjson2.jsonpath;
 
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.JSONReader;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,8 +15,39 @@ public class JSONPahRandomIndex {
         JSONPath jsonPath = JSONPath.of("$[randomIndex()]");
         for (int i = 0; i < 100; ++i) {
             assertNotNull(
-                    jsonPath.eval(array)
+                jsonPath.eval(array)
             );
+        }
+    }
+
+    @Test
+    public void testEvalRandomIndexField() {
+        JSONPath path = JSONPath.of("$.data[randomIndex()].name");
+        Object value = path.eval("{\"data\": [{\"id\": 1, \"name\": \"a\"}, {\"id\": 2, \"name\": \"b\"}]}");
+        assertNotNull(value);
+    }
+
+    @Test
+    public void testExtractRandomIndex() {
+        for (int i = 0; i < 50; i++) {
+            JSONPath path = JSONPath.of("$.data[randomIndex()]");
+            JSONReader reader = JSONReader.of(
+                "{\"data\": [{\"id\": 1, \"name\": \"a\"}, {\"id\": 2, \"name\": \"b\"}]}");
+            Object value = path.extract(reader);
+            System.out.println(value);
+            assertNotNull(value);
+        }
+    }
+
+    @Test
+    public void testExtractRandomIndexField() {
+        for (int i = 0; i < 50; i++) {
+            JSONPath path = JSONPath.of("$.data[randomIndex()].name");
+            JSONReader reader = JSONReader.of(
+                "{\"data\": [{\"id\": 1, \"name\": \"a\"}, {\"id\": 2, \"name\": \"b\"}]}");
+            Object value = path.extract(reader);
+            System.out.println(value);
+            assertNotNull(value);
         }
     }
 }
