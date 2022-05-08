b57a17f4037315f2141d0627be55d42266a102f2
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-08 17:59:54 +0800

    improve compatible for fastjson 1.x, #188

--- core/src/test/java/com/alibaba/fastjson2/JSONObjectTest2.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONObjectTest2.java
@@ -2,6 +2,7 @@ package com.alibaba.fastjson2;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
@@ -26,6 +27,76 @@ public class JSONObjectTest2 {
     }
 
     @Test
+    public void testSqlDate1() {
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .getObject("date", Date.class));
+
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .getObject("date", Timestamp.class));
+
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .getObject("date", Time.class));
+    }
+
+    @Test
+    public void testSqlDate2() {
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .toJavaObject(new TypeReference<Bean<Date>>(){}.getType()));
+
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .toJavaObject(new TypeReference<Bean<Timestamp>>(){}.getType()));
+
+        assertNotNull(JSONObject
+                .of("date", "0000-00-00 00:00:00")
+                .toJavaObject(new TypeReference<Bean<Time>>(){}.getType()));
+    }
+
+    @Test
+    public void testSqlDate3() {
+        String str = "{\"date\":\"0000-00-00 00:00:00\"}";
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Date>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Timestamp>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Time>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+    }
+
+    @Test
+    public void testSqlDate4() {
+        byte[] str = "{\"date\":\"0000-00-00 00:00:00\"}".getBytes(StandardCharsets.UTF_8);
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Date>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Timestamp>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+        {
+            Bean<Date> bean = JSON.parseObject(str, new TypeReference<Bean<Time>>() {
+            }.getType());
+            assertNotNull(bean.date);
+        }
+    }
+
+    @Test
     public void testInterface() {
         BeanInterface bean =
                 JSONObject
@@ -37,5 +108,9 @@ public class JSONObjectTest2 {
     public interface BeanInterface {
         int getId();
         void setId(String id);
+    }
+
+    public static class Bean<T> {
+        public T date;
     }
 }
