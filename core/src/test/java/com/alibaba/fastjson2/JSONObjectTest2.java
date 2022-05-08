f3ea8823ffe313be3a092bf152584230fa71e744
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-08 16:21:26 +0800

    add testcase & remove unused code, #131

--- core/src/test/java/com/alibaba/fastjson2/JSONObjectTest2.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONObjectTest2.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class JSONObjectTest2 {
@@ -22,5 +23,19 @@ public class JSONObjectTest2 {
         assertNotNull(JSONObject
                 .of("date", "0000-00-00")
                 .getObject("date", Time.class));
+    }
+
+    @Test
+    public void testInterface() {
+        BeanInterface bean =
+                JSONObject
+                        .of("id", 123)
+                        .toJavaObject(BeanInterface.class);
+        assertEquals(123, bean.getId());
+    }
+
+    public interface BeanInterface {
+        int getId();
+        void setId(String id);
     }
 }
