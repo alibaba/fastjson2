ac3adc1e70721317107a728cb5fea8c8c4ab2b8f
presenth7s <present.h7s@gmail.com>
2022-05-08 20:10:52 +0800

    fix typo

--- core/src/main/java/com/alibaba/fastjson2/filter/PropertyPreFilter.java
+++ core/src/main/java/com/alibaba/fastjson2/filter/PropertyPreFilter.java
@@ -3,5 +3,5 @@ package com.alibaba.fastjson2.filter;
 import com.alibaba.fastjson2.JSONWriter;
 
 public interface PropertyPreFilter extends Filter {
-    boolean process(JSONWriter jsonWriter, Object source, String name);
+    boolean process(JSONWriter writer, Object source, String name);
 }
