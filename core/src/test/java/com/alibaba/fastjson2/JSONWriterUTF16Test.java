855c2a252c4a8efacca4eed08295ca8c9aea41d1
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-08 23:44:23 +0800

    add testcase, #131

--- core/src/test/java/com/alibaba/fastjson2/JSONWriterUTF16Test.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONWriterUTF16Test.java
@@ -2,6 +2,8 @@ package com.alibaba.fastjson2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JSONWriterUTF16Test {
     @Test
     public void test_write() {
@@ -25,5 +27,33 @@ public class JSONWriterUTF16Test {
         for (int i = 0; i < 8096; i++) {
             jsonWriter.writeNameRaw(new char[]{'a'}, 0, 1);
         }
+    }
+
+    @Test
+    public void testWriterString0() {
+        JSONWriterUTF16 jsonWriter = new JSONWriterUTF16(JSONFactory.createWriteContext());
+        jsonWriter.writeString((String) null);
+        assertEquals("null", jsonWriter.toString());
+    }
+
+    @Test
+    public void testWriterString1() {
+        JSONWriterUTF16 jsonWriter = new JSONWriterUTF16(JSONFactory.createWriteContext(JSONWriter.Feature.NullAsDefaultValue));
+        jsonWriter.writeString((String) null);
+        assertEquals("\"\"", jsonWriter.toString());
+    }
+
+    @Test
+    public void testWriterString2() {
+        JSONWriterUTF16 jsonWriter = new JSONWriterUTF16(JSONFactory.createWriteContext(JSONWriter.Feature.NullAsDefaultValue));
+        jsonWriter.writeString("abcdefghijk1234567890abcdefghijk1234567890");
+        assertEquals("\"abcdefghijk1234567890abcdefghijk1234567890\"", jsonWriter.toString());
+    }
+
+    @Test
+    public void testWriterString3() {
+        JSONWriterUTF16 jsonWriter = new JSONWriterUTF16(JSONFactory.createWriteContext(JSONWriter.Feature.NullAsDefaultValue));
+        jsonWriter.writeString("abcdefghijk1234567890\\\"abcdefghijk1234567890");
+        assertEquals("\"abcdefghijk1234567890\\\\\\\"abcdefghijk1234567890\"", jsonWriter.toString());
     }
 }
