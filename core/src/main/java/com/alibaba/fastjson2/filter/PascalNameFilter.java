dbc9cd018ed2ed8640765f2185c13b58fc4bd5ba
presenth7s <present.h7s@gmail.com>
2022-05-08 20:10:52 +0800

    fix typo

--- core/src/main/java/com/alibaba/fastjson2/filter/PascalNameFilter.java
+++ core/src/main/java/com/alibaba/fastjson2/filter/PascalNameFilter.java
@@ -10,7 +10,6 @@ public class PascalNameFilter implements NameFilter {
         char[] chars = name.toCharArray();
         chars[0] = Character.toUpperCase(chars[0]);
 
-        String pascalName = new String(chars);
-        return pascalName;
+        return new String(chars);
     }
 }
