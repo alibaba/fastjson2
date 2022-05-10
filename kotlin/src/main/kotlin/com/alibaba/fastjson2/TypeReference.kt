f3d31445fb47b043855b18c68f40f0e0af64f846
Kraity <kraits@qq.com>
2022-05-10 15:03:55 +0800

    update comments for kotlin module

--- kotlin/src/main/kotlin/com/alibaba/fastjson2/TypeReference.kt
+++ kotlin/src/main/kotlin/com/alibaba/fastjson2/TypeReference.kt
@@ -6,5 +6,6 @@ package com.alibaba.fastjson2
  *    val ref = reference<User>()
  *    val user = ref.parseObject(text)
  * }</pre>
+ * @since 2.0.3
  */
 inline fun <reified T : Any> reference() = object : TypeReference<T>() {}
