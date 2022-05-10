d067da762191ac1596beb5d0decbe2ffb1ea6334
业余布道师 <Victor.Zxy@outlook.com>
2022-05-10 16:13:25 +0800

    add new benchmark module and move benchmarks code.

---
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayParse.java
@@ -0,0 +1,21 @@
+package com.alibaba.fastjson2.benchmark.eishay;
+
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class EishayParse {
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(EishayParse.class.getName())
+                .mode(Mode.Throughput)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .forks(1)
+                .build();
+        new Runner(options).run();
+    }
+}
---
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/primitves/vo/StringField20.java
@@ -0,0 +1,24 @@
+package com.alibaba.fastjson2.benchmark.primitves.vo;
+
+public class StringField20 {
+    public String v0000;
+    public String v0001;
+    public String v0002;
+    public String v0003;
+    public String v0004;
+    public String v0005;
+    public String v0006;
+    public String v0007;
+    public String v0008;
+    public String v0009;
+    public String v0010;
+    public String v0011;
+    public String v0012;
+    public String v0013;
+    public String v0014;
+    public String v0015;
+    public String v0016;
+    public String v0017;
+    public String v0018;
+    public String v0019;
+}
