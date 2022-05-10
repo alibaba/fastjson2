8540e4837c1d7e6effd8ad7981598968ae4742f9
Kraity <kraits@qq.com>
2022-05-10 21:54:08 +0800

    add methods for kotlin module

--- kotlin/src/test/kotlin/com/alibaba/fastjson2/JSONTest.kt
+++ kotlin/src/test/kotlin/com/alibaba/fastjson2/JSONTest.kt
@@ -17,13 +17,42 @@ class JSONTest {
 
     @Test
     fun test_parseObject2() {
-        parseObject<User>(
-            """{"id":1,"name":"kraity"}"""
-        ).apply {
-            assertEquals(1, id)
-            assertEquals("kraity", name)
-            assertEquals("""{"id":1,"name":"kraity"}""", toJSONString())
-        }
+        val user = """{"id":1,"name":"kraity"}""".parseObject<User>()
+
+        assertEquals(1, user.id)
+        assertEquals("kraity", user.name)
+        assertEquals("""{"id":1,"name":"kraity"}""", user.toJSONString())
+    }
+
+    @Test
+    fun test_parseObject3() {
+        // JSONObject
+        val user = """{"id":1,"name":"kraity"}""".parseObject()
+
+        assertEquals(1, user.get("id"))
+        assertEquals("kraity", user.get("name"))
+    }
+
+    @Test
+    fun test_parseArray1() {
+        // JSONArray
+        val data = """[0,"1",true]""".parseArray()
+
+        assertEquals(0, data[0])
+        assertEquals("1", data[1])
+        assertEquals(true, data[2])
+    }
+
+    @Test
+    fun test_parseArray2() {
+        val list = """[{"id":1,"name":"kraity"}]""".parseArray<User>()
+
+        assertEquals(1, list.size)
+        val user = list[0]
+
+        assertEquals(1, user.id)
+        assertEquals("kraity", user.name)
+        assertEquals("""{"id":1,"name":"kraity"}""", user.toJSONString())
     }
 
     data class User(
