f3cbf7348741e853f9e45c1a0bf69087f5ca9c43
Kraity <kraits@qq.com>
2022-05-11 12:40:26 +0800

    update comments for kotlin doc

--- kotlin/src/main/kotlin/com/alibaba/fastjson2/JSONArray.kt
+++ kotlin/src/main/kotlin/com/alibaba/fastjson2/JSONArray.kt
@@ -1,34 +1,42 @@
 package com.alibaba.fastjson2
 
 /**
- * <pre>{@code
- *    val data = "...".parseArray()
- *    val user = data.toList<User>()
- * }</pre>
+ * E.g.
+ * ```
+ *   val data = "...".parseArray()
+ *   val user = data.toList<User>()
+ * ```
  *
  * @receiver JSONArray
  * @param features features to be enabled in parsing
- * @return T?
+ * @return [T]?
+ * @since 2.0.3
  */
 @Suppress("HasPlatformType")
 inline fun <reified T> JSONArray.toList(
     vararg features: JSONReader.Feature
-) = toJavaList(T::class.java, *features)
+) = toJavaList(
+    T::class.java, *features
+)
 
 /**
- * <pre>{@code
+ * E.g.
+ * ```
  *   // JSONArray
  *   val data = "...".parseArray()
  *   val user = data.getObject<User>(0)
- * }</pre>
+ * ```
  *
  * @receiver JSONArray
  * @param index index of the element to return
  * @param features features to be enabled in parsing
- * @return T?
+ * @return [T]?
+ * @since 2.0.3
  */
 @Suppress("HasPlatformType")
 inline fun <reified T> JSONArray.getObject(
     index: Int,
     vararg features: JSONReader.Feature
-) = getObject(index, T::class.java, *features)
+) = getObject(
+    index, T::class.java, *features
+)
