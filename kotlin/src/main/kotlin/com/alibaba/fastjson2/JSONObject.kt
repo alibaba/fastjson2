0dc398aa84286a774383720ec0ff0b3ab22f714d
Kraity <kraits@qq.com>
2022-05-11 12:40:26 +0800

    update comments for kotlin doc

--- kotlin/src/main/kotlin/com/alibaba/fastjson2/JSONObject.kt
+++ kotlin/src/main/kotlin/com/alibaba/fastjson2/JSONObject.kt
@@ -1,34 +1,42 @@
 package com.alibaba.fastjson2
 
 /**
- * <pre>{@code
- *    val data = "...".parseObject()
- *    val user = data.toObject<User>()
- * }</pre>
+ * E.g.
+ * ```
+ *   val data = "...".parseObject()
+ *   val user = data.toObject<User>()
+ * ```
  *
  * @receiver JSONObject
  * @param features features to be enabled in parsing
- * @return T?
+ * @return [T]?
+ * @since 2.0.3
  */
 @Suppress("HasPlatformType")
 inline fun <reified T> JSONObject.toObject(
     vararg features: JSONReader.Feature
-) = toJavaObject(T::class.java, *features)
+) = toJavaObject(
+    T::class.java, *features
+)
 
 /**
- * <pre>{@code
- *   // JSONObject
+ * E.g.
+ * ```
+ *  // JSONObject
  *   val data = "...".parseObject()
  *   val user = data.getObject<User>("key")
- * }</pre>
+ * ```
  *
  * @receiver JSONObject
  * @param key the key whose associated value is to be returned
  * @param features features to be enabled in parsing
- * @return T?
+ * @return [T]?
+ * @since 2.0.3
  */
 @Suppress("HasPlatformType")
 inline fun <reified T> JSONObject.getObject(
     key: String,
     vararg features: JSONReader.Feature
-) = getObject(key, T::class.java, *features)
+) = getObject(
+    key, T::class.java, *features
+)
