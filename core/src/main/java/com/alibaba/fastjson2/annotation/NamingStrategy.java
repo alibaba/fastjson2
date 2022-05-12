91124b9fc6ddd63168e94959f3da7f82cb86f056
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-12 19:08:05 +0800

    JSONObject support nameFilter & valueFilter, #225

--- core/src/main/java/com/alibaba/fastjson2/annotation/NamingStrategy.java
+++ core/src/main/java/com/alibaba/fastjson2/annotation/NamingStrategy.java
@@ -9,4 +9,34 @@ public enum NamingStrategy {
     SnakeCase,
     UpperCase,
     KebabCase;
+
+    public static String snakeToCamel(String name) {
+        if (name == null || name.indexOf('_') == -1) {
+            return name;
+        }
+
+        int underscoreCount = 0;
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch == '_') {
+                underscoreCount++;
+            }
+        }
+
+        char[] chars = new char[name.length() - underscoreCount];
+        for (int i = 0, j = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch == '_') {
+                continue;
+            }
+            if (i > 0 && name.charAt(i - 1) == '_') {
+                if (ch >= 'a' && ch <= 'z') {
+                    ch -= 32;
+                }
+            }
+            chars[j++] = ch;
+        }
+
+        return new String(chars);
+    }
 }
---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue225.java
@@ -0,0 +1,92 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.annotation.NamingStrategy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue225 {
+    @Test
+    public void test() {
+        JSONObject jsonObject = JSONObject.of("item_id", 101);
+
+        jsonObject.nameFilter(
+                (Object object, String name, Object value) -> name
+        );
+        assertEquals(1, jsonObject.size());
+        assertEquals(101, jsonObject.get("item_id"));
+
+
+        jsonObject.nameFilter(
+                (Object object, String name, Object value) -> null
+        );
+        assertEquals(1, jsonObject.size());
+        assertEquals(101, jsonObject.get("item_id"));
+
+        jsonObject.nameFilter(
+                (object, name, value) -> NamingStrategy.snakeToCamel(name)
+        );
+        assertEquals(1, jsonObject.size());
+        assertEquals(101, jsonObject.get("itemId"));
+    }
+
+    @Test
+    public void test1() {
+        JSONObject jsonObject = JSONObject
+                .of(
+                        "items",
+                        JSONArray.of(
+                                JSONObject.of("item_id", 101)
+                        )
+                );
+
+        jsonObject.nameFilter(
+                (object, name, value) -> NamingStrategy.snakeToCamel(name)
+        );
+
+        assertEquals(
+                101,
+                jsonObject
+                        .getJSONArray("items")
+                        .getJSONObject(0)
+                        .get("itemId")
+        );
+    }
+
+    @Test
+    public void test2() {
+        JSONObject jsonObject = JSON.parseObject("{\"items\":[{\"item_id\":101}]}");
+
+        jsonObject.nameFilter(
+                (object, name, value) -> NamingStrategy.snakeToCamel(name)
+        );
+
+        assertEquals(
+                101,
+                jsonObject
+                        .getJSONArray("items")
+                        .getJSONObject(0)
+                        .get("itemId")
+        );
+    }
+
+    @Test
+    public void test3() {
+        JSONObject jsonObject = JSON.parseObject("{\"items\":[{\"item_id\":101}]}");
+
+        jsonObject.valueFilter(
+                (object, name, value) -> name.equals("item_id") ? ((Integer) value).intValue() + 1 : value
+        );
+
+        assertEquals(
+                102,
+                jsonObject
+                        .getJSONArray("items")
+                        .getJSONObject(0)
+                        .get("item_id")
+        );
+    }
+}
