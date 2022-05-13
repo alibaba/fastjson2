085fe6272909eef1867415bfa9f6966f652f65dc
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-13 10:00:39 +0800

    refactor testcase

--- core/src/test/java/com/alibaba/fastjson2/jsonb/StringMessageTest.java
+++ core/src/test/java/com/alibaba/fastjson2/jsonb/StringMessageTest.java
@@ -8,21 +8,28 @@ import org.junit.jupiter.api.Test;
 import java.io.Serializable;
 
 public class StringMessageTest {
+    static final JSONWriter.Feature[] READER_FEATURES = {
+            JSONWriter.Feature.WriteClassName,
+            JSONWriter.Feature.FieldBased,
+            JSONWriter.Feature.ReferenceDetection,
+            JSONWriter.Feature.WriteNulls,
+            JSONWriter.Feature.NotWriteDefaultValue,
+            JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+            JSONWriter.Feature.WriteNameAsSymbol
+    };
+
+    static final JSONReader.Feature[] WRITER_FEATURES = {
+            JSONReader.Feature.SupportAutoType,
+            JSONReader.Feature.UseDefaultConstructorAsPossible,
+            JSONReader.Feature.UseNativeObject,
+            JSONReader.Feature.FieldBased
+    };
+
     @Test
     public void test() {
         StringMessage test = new StringMessage("Test");
-        byte[] bytes = JSONB.toBytes(test, JSONWriter.Feature.WriteClassName,
-                JSONWriter.Feature.FieldBased,
-                JSONWriter.Feature.ReferenceDetection,
-                JSONWriter.Feature.WriteNulls,
-                JSONWriter.Feature.NotWriteDefaultValue,
-                JSONWriter.Feature.NotWriteHashMapArrayListClassName,
-                JSONWriter.Feature.WriteNameAsSymbol);
-
-        JSONB.parseObject(bytes, Object.class, JSONReader.Feature.SupportAutoType,
-                JSONReader.Feature.UseDefaultConstructorAsPossible,
-                JSONReader.Feature.UseNativeObject,
-                JSONReader.Feature.FieldBased);
+        byte[] bytes = JSONB.toBytes(test, READER_FEATURES);
+        JSONB.parseObject(bytes, Object.class, WRITER_FEATURES);
     }
 
     static class StringMessage implements Serializable {
