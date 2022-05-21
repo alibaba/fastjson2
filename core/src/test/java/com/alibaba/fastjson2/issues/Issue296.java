210e6a55d4d3919c137f0dc40b6723968c70118d
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-21 20:32:28 +0800

    bug fix for JSONArray toJavaList #296

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue296.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue296.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,5 +26,26 @@ public class Issue296 {
         assertEquals(2, array.length);
         assertEquals(1, array[0]);
         assertEquals(2, array[1]);
+
+        float[] floatArray = JSONArray.of("1", 2, 3L, BigDecimal.valueOf(4)).to(float[].class);
+        assertEquals(4, floatArray.length);
+        assertEquals(1, floatArray[0]);
+        assertEquals(2, floatArray[1]);
+        assertEquals(3, floatArray[2]);
+        assertEquals(4, floatArray[3]);
+
+        double[] doubles = JSONArray.of("1", 2, 3L, BigDecimal.valueOf(4)).to(double[].class);
+        assertEquals(4, doubles.length);
+        assertEquals(1, doubles[0]);
+        assertEquals(2, doubles[1]);
+        assertEquals(3, doubles[2]);
+        assertEquals(4, doubles[3]);
+
+        Float[] floats = JSONArray.of("1", 2, 3L, BigDecimal.valueOf(4)).to(Float[].class);
+        assertEquals(4, floats.length);
+        assertEquals(1, floats[0]);
+        assertEquals(2, floats[1]);
+        assertEquals(3, floats[2]);
+        assertEquals(4, floats[3]);
     }
 }
