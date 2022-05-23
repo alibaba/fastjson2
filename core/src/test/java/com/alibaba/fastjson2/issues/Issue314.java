4dc7b4a1e36636132318c96ba87e46c189095f1f
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-23 09:21:01 +0800

    bug fix for enum Field serializeFeatures doesn't work #314

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue314.java
@@ -0,0 +1,26 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.annotation.JSONField;
+import org.junit.jupiter.api.Test;
+
+import static com.alibaba.fastjson2.JSONWriter.Feature.WriteEnumsUsingName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue314 {
+    @Test
+    public void test() {
+        Bean bean = new Bean();
+        bean.type = Type.Big;
+        assertEquals("{\"type\":\"Big\"}", JSON.toJSONString(bean));
+    }
+
+    public static class Bean {
+        @JSONField(serializeFeatures = WriteEnumsUsingName)
+        public Type type;
+    }
+
+    public static enum Type {
+        Big, Small
+    }
+}
