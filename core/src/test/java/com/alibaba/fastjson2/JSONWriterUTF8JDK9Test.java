3868133beb984c983ba610302abff5ae11378413
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-23 21:33:25 +0800

    bug fix for JSONWriterUTF8

--- core/src/test/java/com/alibaba/fastjson2/JSONWriterUTF8JDK9Test.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONWriterUTF8JDK9Test.java
@@ -1,8 +1,11 @@
 package com.alibaba.fastjson2;
 
+import com.alibaba.fastjson2.writer.ObjectWriter;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -50,19 +53,45 @@ public class JSONWriterUTF8JDK9Test {
     }
 
     @Test
+    public void test_writeString_utf8_1() {
+        JSONWriterUTF8JDK9 jsonWriter = new JSONWriterUTF8JDK9(JSONFactory.createWriteContext());
+        jsonWriter.writeString("^á");
+        assertEquals("\"^á\"", jsonWriter.toString());
+    }
+
+    @Test
     public void test_writeString_utf82() {
         StringBuffer buf = new StringBuffer();
         for (int i = 0; i < 512; i++) {
             char ch = (char) i;
             buf.append(ch);
         }
+        buf.append('á');
         String origin = buf.toString();
 
         JSONWriterUTF8JDK9 jsonWriter = new JSONWriterUTF8JDK9(JSONFactory.createWriteContext());
         jsonWriter.writeString(origin);
         String json = jsonWriter.toString();
         String str = (String) JSON.parse(json);
+        assertEquals(origin.length(), str.length());
+        for (int i = 0; i < origin.length(); i++) {
+            assertEquals(origin.charAt(i), str.charAt(i));
+        }
         assertEquals(origin, str);
+    }
+
+
+    @Test
+    public void test_writeString_utf8_3() {
+        JSONWriterUTF8JDK9 jsonWriter = new JSONWriterUTF8JDK9(JSONFactory.createWriteContext());
+
+        JSONObject map = new JSONObject();
+        map.put("^á", 0);
+
+        ObjectWriter objectWriter = JSONFactory.defaultObjectWriterProvider.getObjectWriter(JSONObject.class);
+        objectWriter.write(jsonWriter, map);
+
+        assertEquals("{\"^á\":0}", jsonWriter.toString());
     }
 
     @Test
