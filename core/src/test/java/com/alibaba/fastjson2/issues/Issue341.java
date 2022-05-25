d5977e0aad099f6d16b8715c46f6f7b6eb405b7a
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-26 00:00:34 +0800

    FieldReaderListStrMethod support java.util.Arrays$ArrayList

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue341.java
@@ -0,0 +1,39 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONB;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue341 {
+    @Test
+    public void test() {
+        Bean bean = new Bean();
+        bean.values = Arrays.asList("cacheObject", "cacheObject2", "cacheObject3");
+
+        byte[] bytes = JSONB.toBytes(bean, JSONWriter.Feature.WriteClassName);
+        Bean bean1 = JSONB.parseObject(bytes, Bean.class, JSONReader.Feature.SupportAutoType);
+        assertEquals(bean.values.size(), bean1.values.size());
+    }
+
+    private static class Bean {
+        private List<String> values;
+
+        public Bean() {
+        }
+
+        public List<String> getValues() {
+            return values;
+        }
+
+        public void setValues(List<String> values) {
+            this.values = values;
+        }
+    }
+}
