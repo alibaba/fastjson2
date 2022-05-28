2b91dec532fd36c01d6a1476875b201f456e5e20
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-28 09:25:31 +0800

    add benchmark

---
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/Eishay.java
@@ -0,0 +1,21 @@
+package com.alibaba.fastjson2.benchmark.eishay;
+
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class Eishay {
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(Eishay.class.getName())
+                .mode(Mode.Throughput)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .forks(1)
+                .build();
+        new Runner(options).run();
+    }
+}
