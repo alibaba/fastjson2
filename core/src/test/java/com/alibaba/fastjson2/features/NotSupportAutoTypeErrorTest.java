b7eda2665b3c69534882af4f4d37cff707a20bc6
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-29 15:23:04 +0800

    add more error info

--- core/src/test/java/com/alibaba/fastjson2/features/NotSupportAutoTypeErrorTest.java
+++ core/src/test/java/com/alibaba/fastjson2/features/NotSupportAutoTypeErrorTest.java
@@ -7,6 +7,9 @@ import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2_vo.Int1;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NotSupportAutoTypeErrorTest {
@@ -15,5 +18,24 @@ public class NotSupportAutoTypeErrorTest {
         Int1 vo = new Int1();
         String str = JSON.toJSONString(vo, JSONWriter.Feature.WriteClassName);
         assertThrows(JSONException.class, () -> JSON.parseObject(str, Object.class, JSONReader.Feature.ErrorOnNotSupportAutoType));
+    }
+
+    @Test
+    public void test2() {
+        Bean bean = new Bean(Arrays.asList(new Int1()));
+        String str = JSON.toJSONString(bean, JSONWriter.Feature.WriteClassName);
+        assertThrows(JSONException.class, () -> JSON.parseObject(str, Object.class, JSONReader.Feature.ErrorOnNotSupportAutoType));
+    }
+
+    public static class Bean {
+        private final List<Int1> values;
+
+        public Bean(List<Int1> values) {
+            this.values = values;
+        }
+
+        public List<Int1> getValues() {
+            return values;
+        }
     }
 }
