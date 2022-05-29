229233fa3573f808199f2729460c6809d3d893cc
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-29 22:58:45 +0800

    bug fix for reference support

--- core/src/main/java/com/alibaba/fastjson2/JSONReader.java
+++ core/src/main/java/com/alibaba/fastjson2/JSONReader.java
@@ -351,7 +351,7 @@ public abstract class JSONReader
         if (resolveTasks == null) {
             resolveTasks = new ArrayList<>();
         }
-        resolveTasks.add(new ResolveTask(fieldReader, object, null, path));
+        resolveTasks.add(new ResolveTask(fieldReader, object, fieldReader.getFieldName(), path));
     }
 
     public void addResolveTask(Map object, Object key, JSONPath reference) {
