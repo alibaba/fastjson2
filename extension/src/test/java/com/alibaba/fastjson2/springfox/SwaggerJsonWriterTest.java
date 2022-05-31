346d7051c27691ecaa1f2c55780bf5d79c82efae
业余布道师 <Victor.Zxy@outlook.com>
2022-05-31 22:35:27 +0800

    add OpenApiJsonWriter for Issue 387.

--- extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
+++ extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
@@ -1,15 +1,26 @@
 package com.alibaba.fastjson2.springfox;
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.support.springfox.SwaggerJsonWriter;
 import org.junit.jupiter.api.Test;
 import springfox.documentation.spring.web.json.Json;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SwaggerJsonWriterTest {
+    String jsonStr = "{\"abc\":\"cde中文\"}";
+
     @Test
     public void test() {
         SwaggerJsonWriter writer = SwaggerJsonWriter.INSTANCE;
         writer.write(JSONWriter.of(), null);
-        writer.write(JSONWriter.of(), new Json("{\"abc\":\"cde中文\"}"));
+        writer.write(JSONWriter.of(), new Json(jsonStr));
+    }
+
+    @Test
+    public void test1() {
+        JSON.register(Json.class, SwaggerJsonWriter.INSTANCE);
+        assertEquals(jsonStr, JSON.toJSONString(new Json(jsonStr)));
     }
 }
