141fb7d19923c70dbefb20c780df96eb33ba0b3a
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-01 10:10:12 +0800

    support jsonDirect

--- extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
+++ extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
@@ -1,9 +1,6 @@
 package com.alibaba.fastjson2.springfox;
 
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONFactory;
-import com.alibaba.fastjson2.JSONWriter;
-import com.alibaba.fastjson2.support.springfox.SwaggerJsonWriter;
 import org.junit.jupiter.api.Test;
 import springfox.documentation.spring.web.json.Json;
 
@@ -14,15 +11,11 @@ public class SwaggerJsonWriterTest {
 
     @Test
     public void test() {
-        SwaggerJsonWriter writer = SwaggerJsonWriter.INSTANCE;
-        writer.write(JSONWriter.of(), null);
-        writer.write(JSONWriter.of(), new Json(jsonStr));
+        assertEquals(jsonStr, JSON.toJSONString(new Json(jsonStr)));
     }
 
     @Test
     public void test1() {
-        JSONFactory.getDefaultObjectWriterProvider().register(Json.class, SwaggerJsonWriter.INSTANCE);
-        assertEquals(jsonStr, JSON.toJSONString(new Json(jsonStr)));
-        JSONFactory.getDefaultObjectWriterProvider().unregister(Json.class, SwaggerJsonWriter.INSTANCE);
+        assertEquals(jsonStr, new String(JSON.toJSONBytes(new Json(jsonStr))));
     }
 }
