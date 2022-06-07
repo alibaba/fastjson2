6755d0ea6935bacb5df76b5a5979b0d6adf9c244
业余布道师 <Victor.Zxy@outlook.com>
2022-06-07 12:55:56 +0800

    fix LocalTime type use JSONField format issue.

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue416.java
@@ -0,0 +1,39 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class Issue416 {
+    @Test
+    public void test() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalTest localTest1 = new LocalTest();
+        localTest1.setLocalDate(localDateTime.toLocalDate());
+        localTest1.setLocalTime(localDateTime.toLocalTime());
+        localTest1.setLocalDateTime(localDateTime);
+        LocalTest localTest2 = JSON.parseObject(JSON.toJSONString(localTest1), LocalTest.class);
+
+        assertEquals(localTest1.getLocalDate(), localTest2.getLocalDate());
+        assertNotEquals(localTest1.getLocalTime(), localTest2.getLocalTime()); // without nano
+        assertNotEquals(localTest1.getLocalDateTime(), localTest2.getLocalDateTime()); // without nano
+    }
+
+    @Data
+    static class LocalTest {
+        @JSONField(format = "yyyy-MM-dd")
+        private LocalDate localDate;
+        @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+        @JSONField(format = "HH:mm:ss")
+        private LocalTime localTime;
+    }
+}
