d655f3d1c9f5ccc861189f33a8df7e44f1b1b5bf
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-10 09:34:22 +0800

    reimplement PascalNameFilter use BeanUtils.fieldName

--- core/src/main/java/com/alibaba/fastjson2/filter/PascalNameFilter.java
+++ core/src/main/java/com/alibaba/fastjson2/filter/PascalNameFilter.java
@@ -1,16 +1,12 @@
 package com.alibaba.fastjson2.filter;
 
+import com.alibaba.fastjson2.PropertyNamingStrategy;
+import com.alibaba.fastjson2.util.BeanUtils;
+
 public class PascalNameFilter
         implements NameFilter {
     @Override
     public String process(Object source, String name, Object value) {
-        if (name == null || name.length() == 0) {
-            return name;
-        }
-
-        char[] chars = name.toCharArray();
-        chars[0] = Character.toUpperCase(chars[0]);
-
-        return new String(chars);
+        return BeanUtils.fieldName(name, PropertyNamingStrategy.PascalCase.name());
     }
 }
