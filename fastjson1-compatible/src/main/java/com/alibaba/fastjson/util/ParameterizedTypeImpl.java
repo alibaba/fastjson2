112f61b4cdcb14acaf0e3c09430259d5b8b617d0
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-21 08:59:57 +0800

    fix check style

--- fastjson1-compatible/src/main/java/com/alibaba/fastjson/util/ParameterizedTypeImpl.java
+++ fastjson1-compatible/src/main/java/com/alibaba/fastjson/util/ParameterizedTypeImpl.java
@@ -1,17 +1,10 @@
 package com.alibaba.fastjson.util;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 
 public class ParameterizedTypeImpl
         extends com.alibaba.fastjson2.util.ParameterizedTypeImpl {
     public ParameterizedTypeImpl(Type[] actualTypeArguments, Type ownerType, Type rawType) {
         super(actualTypeArguments, ownerType, rawType);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
     }
 }
