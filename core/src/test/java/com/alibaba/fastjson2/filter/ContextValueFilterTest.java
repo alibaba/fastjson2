8df92150c181c6a23117687d14385816086cd167
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-22 19:39:45 +0800

    add testcase & remove unused code

--- core/src/test/java/com/alibaba/fastjson2/filter/ContextValueFilterTest.java
+++ core/src/test/java/com/alibaba/fastjson2/filter/ContextValueFilterTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ContextValueFilterTest {
     @Test
@@ -28,14 +29,54 @@ public class ContextValueFilterTest {
         assertEquals(int.class, context.getFieldClass());
         assertEquals(int.class, context.getFieldType());
         assertEquals(Bean.class.getField("id"), context.getField());
+        assertEquals(null, context.getMethod());
         assertEquals("userId", context.getAnnotation(JSONField.class).name());
         assertEquals(null, context.getFormat());
         assertEquals(null, context.getLabel());
         assertEquals(0, context.getFeatures());
+        assertEquals("userId", context.getName());
+        assertFalse(context.isJsonDirect());
     }
 
     public static class Bean {
         @JSONField(name = "userId")
         public int id;
+    }
+
+    @Test
+    public void test1() throws Exception {
+        Bean1 bean = new Bean1();
+        bean.id = 10;
+
+        AtomicReference<BeanContext> contextReference = new AtomicReference<>();
+
+        ContextValueFilter filter = (BeanContext context, Object object, String name, Object value) -> {
+            contextReference.set(context);
+            return ((Integer) value).intValue() + 1;
+        };
+
+        assertEquals("{\"userId\":11}", JSON.toJSONString(bean, filter));
+
+        BeanContext context = contextReference.get();
+        assertEquals(Bean1.class, context.getBeanClass());
+        assertEquals(int.class, context.getFieldClass());
+        assertEquals(int.class, context.getFieldType());
+        assertEquals(Bean1.class.getMethod("getId"), context.getMethod());
+        assertEquals(null, context.getField());
+        assertEquals("userId", context.getAnnotation(JSONField.class).name());
+        assertEquals(null, context.getFormat());
+        assertEquals(null, context.getLabel());
+        assertEquals(0, context.getFeatures());
+        assertEquals("userId", context.getName());
+        assertFalse(context.isJsonDirect());
+    }
+
+    public static class Bean1 {
+        private int id;
+
+        @JSONField(name = "userId")
+        public int getId() {
+            return id;
+        }
     }
 }
