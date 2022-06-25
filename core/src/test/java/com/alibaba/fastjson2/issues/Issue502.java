1beed57e1377254cccb32fa8b6e168444f9fa0bf
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-25 19:55:48 +0800

    add testcase for issue #502

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue502.java
@@ -0,0 +1,29 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue502 {
+    @Test
+    public void test() {
+        String result = "{\"taskId\":\"xxx\"}";
+        ThirdCommonResult thirdCommonResult = JSONObject.parseObject(result, ThirdCommonResult.class);
+        assertEquals("xxx", thirdCommonResult.getTaskId());
+    }
+
+    @Data
+    public static class ThirdCommonResult {
+
+        private String msg;
+
+        private String flag;
+
+        private String taskId;
+
+        private JSONArray data;
+    }
+}
