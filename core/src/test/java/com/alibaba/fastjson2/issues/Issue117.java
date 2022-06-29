306e9f2a63363dee70bc79ffd09ca2617c8cb2fb
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-06-29 09:07:17 +0800

    support Feature.AllowUnQuotedFieldNames, bug fix for issue #478

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue117.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue117.java
@@ -3,13 +3,14 @@ package com.alibaba.fastjson2.issues;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.JSONReader;
 import org.junit.jupiter.api.Test;
 
 public class Issue117 {
     @Test
     public void test() {
         String text = ("{code:1,msg:'Hello world',data:{list:[1,2,3,4,5], ary2:[{a:2},{a:3,b:{c:'ddd'}}]}}");
-        JSONObject obj = JSON.parseObject(text);
+        JSONObject obj = JSON.parseObject(text, JSONReader.Feature.AllowUnQuotedFieldNames);
 
         JSONPath.eval(obj, "$..ary2[0].a");
 
