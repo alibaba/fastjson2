dcb9d64e49e3d55ee831b7812c8dc087917677d1
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-10 18:15:47 +0800

    1. fury 0.7.2 2. jsonb use hsf default features

--- benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayCodec.java
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayCodec.java
@@ -3,6 +3,8 @@ package com.alibaba.fastjson2.benchmark.eishay;
 import com.alibaba.fastjson2.JSONB;
 import com.alibaba.fastjson2.JSONReader;
 import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.benchmark.eishay.vo.Image;
+import com.alibaba.fastjson2.benchmark.eishay.vo.Media;
 import com.alibaba.fastjson2.benchmark.eishay.vo.MediaContent;
 import io.fury.Fury;
 import io.fury.Language;
@@ -22,10 +24,38 @@ public class EishayCodec {
     static MediaContent mc;
     static byte[] fastjson2JSONBBytes;
 
-    static Fury fury = Fury.builder().withLanguage(Language.JAVA).build();
+    static Fury fury = Fury
+            .builder()
+            .withLanguage(Language.JAVA)
+            .build();
     static byte[] furyBytes;
 
+    static final JSONWriter.Feature[] jsonbWriteFeatures = {
+            JSONWriter.Feature.WriteClassName,
+            JSONWriter.Feature.IgnoreNoneSerializable,
+            JSONWriter.Feature.FieldBased,
+            JSONWriter.Feature.ReferenceDetection,
+            JSONWriter.Feature.WriteNulls,
+            JSONWriter.Feature.NotWriteDefaultValue,
+            JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+            JSONWriter.Feature.WriteNameAsSymbol
+    };
+
+    static final JSONReader.Feature[] jsonbReaderFeatures = {
+            JSONReader.Feature.SupportAutoType,
+            JSONReader.Feature.IgnoreNoneSerializable,
+            JSONReader.Feature.UseDefaultConstructorAsPossible,
+            JSONReader.Feature.UseNativeObject,
+            JSONReader.Feature.FieldBased
+    };
+
     static {
+        fury.register(MediaContent.class);
+        fury.register(Image.class);
+        fury.register(Image.Size.class);
+        fury.register(Media.class);
+        fury.register(Media.Player.class);
+
         try {
             InputStream is = EishayParseBinary.class.getClassLoader().getResourceAsStream("data/eishay.json");
             String str = IOUtils.toString(is, "UTF-8");
@@ -42,17 +72,21 @@ public class EishayCodec {
 
     @Benchmark
     public void deserialize_jsonb(Blackhole bh) {
-        bh.consume(JSONB.parseObject(fastjson2JSONBBytes, Object.class, JSONReader.Feature.SupportAutoType));
+        bh.consume(
+                JSONB.parseObject(fastjson2JSONBBytes, Object.class, jsonbReaderFeatures)
+        );
     }
 
     @Benchmark
     public void deserialize_fury(Blackhole bh) {
-        bh.consume(fury.deserialize(furyBytes));
+        bh.consume(
+                fury.deserialize(furyBytes)
+        );
     }
 
     @Benchmark
     public void serialize_jsonb(Blackhole bh) {
-        byte[] bytes = JSONB.toBytes(mc, JSONWriter.Feature.WriteClassName);
+        byte[] bytes = JSONB.toBytes(mc, jsonbWriteFeatures);
         bh.consume(bytes);
     }
 
