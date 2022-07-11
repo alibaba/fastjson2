68cd8467fe37ebc1730529a6d71f53b04638c166
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-11 10:29:28 +0800

    add jsonb arrayMapping codec

--- benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayCodec.java
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayCodec.java
@@ -1,33 +1,28 @@
 package com.alibaba.fastjson2.benchmark.eishay;
 
-import com.alibaba.fastjson2.JSONB;
-import com.alibaba.fastjson2.JSONException;
-import com.alibaba.fastjson2.JSONReader;
-import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.*;
 import com.alibaba.fastjson2.benchmark.eishay.vo.MediaContent;
 import io.fury.Fury;
 import io.fury.Language;
 import org.apache.commons.io.IOUtils;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 
 public class EishayCodec {
+    static final Blackhole BH = new Blackhole("Today's password is swordfish. I understand instantiating Blackholes directly is dangerous.");
+
     static MediaContent mc;
-    static byte[] fastjson2JSONBBytes;
+    static byte[] jsonbBytes;
+    static byte[] jsonbBytesArrayMapping;
+    static byte[] furyBytes;
 
     static Fury fury = Fury
             .builder()
             .withLanguage(Language.JAVA)
             .build();
-    static byte[] furyBytes;
 
     static final JSONWriter.Feature[] jsonbWriteFeatures = {
             JSONWriter.Feature.WriteClassName,
@@ -39,11 +34,23 @@ public class EishayCodec {
             JSONWriter.Feature.WriteNameAsSymbol
     };
 
+    static final JSONWriter.Feature[] jsonbWriteFeaturesArrayMapping = {
+            JSONWriter.Feature.WriteClassName,
+            JSONWriter.Feature.FieldBased,
+            JSONWriter.Feature.ReferenceDetection,
+            JSONWriter.Feature.WriteNulls,
+            JSONWriter.Feature.NotWriteDefaultValue,
+            JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+            JSONWriter.Feature.WriteNameAsSymbol,
+            JSONWriter.Feature.BeanToArray
+    };
+
     static final JSONReader.Feature[] jsonbReaderFeatures = {
             JSONReader.Feature.SupportAutoType,
             JSONReader.Feature.UseDefaultConstructorAsPossible,
             JSONReader.Feature.UseNativeObject,
-            JSONReader.Feature.FieldBased
+            JSONReader.Feature.FieldBased,
+            JSONReader.Feature.SupportArrayToBean
     };
 
     static {
@@ -53,10 +60,16 @@ public class EishayCodec {
             mc = JSONReader.of(str)
                     .read(MediaContent.class);
 
-            fastjson2JSONBBytes = JSONB.toBytes(mc, jsonbWriteFeatures);
+            jsonbBytes = JSONB.toBytes(mc, jsonbWriteFeatures);
+            jsonbBytesArrayMapping = JSONB.toBytes(mc, jsonbWriteFeaturesArrayMapping);
 
-            MediaContent obj = (MediaContent) JSONB.parseObject(fastjson2JSONBBytes, Object.class, jsonbReaderFeatures);
+            MediaContent obj = (MediaContent) JSONB.parseObject(jsonbBytes, Object.class, jsonbReaderFeatures);
             if (!mc.equals(obj)) {
+                throw new JSONException("not equals");
+            }
+
+            MediaContent obj2 = (MediaContent) JSONB.parseObject(jsonbBytesArrayMapping, Object.class, jsonbReaderFeatures);
+            if (!mc.equals(obj2)) {
                 throw new JSONException("not equals");
             }
 
@@ -68,7 +81,13 @@ public class EishayCodec {
 
     @Benchmark
     public void deserialize_jsonb(Blackhole bh) {
-        MediaContent obj = (MediaContent) JSONB.parseObject(fastjson2JSONBBytes, Object.class, jsonbReaderFeatures);
+        MediaContent obj = (MediaContent) JSONB.parseObject(jsonbBytes, Object.class, jsonbReaderFeatures);
+        bh.consume(obj);
+    }
+
+    @Benchmark
+    public void deserialize_jsonbArrayMapping(Blackhole bh) {
+        MediaContent obj = (MediaContent) JSONB.parseObject(jsonbBytesArrayMapping, Object.class, jsonbReaderFeatures);
         bh.consume(obj);
     }
 
@@ -85,22 +104,79 @@ public class EishayCodec {
     }
 
     @Benchmark
+    public void serialize_jsonb_arrayMapping(Blackhole bh) {
+        byte[] bytes = JSONB.toBytes(mc, jsonbWriteFeaturesArrayMapping);
+        bh.consume(bytes);
+    }
+
+    @Benchmark
     public void serialize_fury(Blackhole bh) {
         byte[] bytes = fury.serialize(mc);
         bh.consume(bytes);
     }
 
+    public void serialize_fury_perf() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000 * 1000; ++i) {
+            serialize_fury(BH);
+        }
+        long millis = System.currentTimeMillis() - start;
+        System.out.println("fury millis : " + millis);
+        // zulu11.52.13  :
+        // zulu17.32.13  :
+        // zulu8.58.0.13 :
+    }
+
+    public void serialize_fury_perf_test() {
+        for (int i = 0; i < 10; i++) {
+            serialize_fury_perf();
+        }
+    }
+
+    public void serialize_jsonb_perf() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000 * 1000; ++i) {
+            serialize_jsonb(BH);
+        }
+        long millis = System.currentTimeMillis() - start;
+        System.out.println("millis : " + millis);
+        // zulu11.52.13 :
+        // zulu17.32.13 :
+        // zulu8.58.0.13 :
+    }
+
+    public void serialize_jsonb_arrayMapping_perf() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000 * 1000; ++i) {
+            serialize_jsonb_arrayMapping(BH);
+        }
+        long millis = System.currentTimeMillis() - start;
+        System.out.println("jsonb_arrayMapping millis : " + millis);
+        // zulu11.52.13 :
+        // zulu17.32.13 :
+        // zulu8.58.0.13 :
+    }
+
+    public void serialize_jsonb_arrayMapping_perf_test() {
+        for (int i = 0; i < 10; i++) {
+            serialize_jsonb_arrayMapping_perf();
+        }
+    }
+
     public static void main(String[] args) throws RunnerException {
-        System.out.println("fury  size : " + fury.serialize(mc).length);
-        System.out.println("jsonb size : " + JSONB.toBytes(mc, jsonbWriteFeatures).length);
-        Options options = new OptionsBuilder()
-                .include(EishayCodec.class.getName())
-                .exclude(EishayCodecOnlyJSONB.class.getName())
-                .mode(Mode.Throughput)
-                .warmupIterations(3)
-                .timeUnit(TimeUnit.MILLISECONDS)
-                .forks(1)
-                .build();
-        new Runner(options).run();
+//        System.out.println("fury  size : " + fury.serialize(mc).length);
+//        System.out.println("jsonb size : " + JSONB.toBytes(mc, jsonbWriteFeatures).length);
+
+//        new EishayCodec().serialize_jsonb_arrayMapping_perf_test();
+        new EishayCodec().serialize_fury_perf_test();
+//        Options options = new OptionsBuilder()
+//                .include(EishayCodec.class.getName())
+//                .exclude(EishayCodecOnlyJSONB.class.getName())
+//                .mode(Mode.Throughput)
+//                .warmupIterations(3)
+//                .timeUnit(TimeUnit.MILLISECONDS)
+//                .forks(1)
+//                .build();
+//        new Runner(options).run();
     }
 }
