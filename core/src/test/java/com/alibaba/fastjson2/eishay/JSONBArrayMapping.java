f8a1d4716eb1fe179fa342bb74e48fbb70458143
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-11 12:18:01 +0800

    bug fix for arrayMapping

--- core/src/test/java/com/alibaba/fastjson2/eishay/JSONBArrayMapping.java
+++ core/src/test/java/com/alibaba/fastjson2/eishay/JSONBArrayMapping.java
@@ -13,6 +13,25 @@ import java.net.URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JSONBArrayMapping {
+    static final JSONWriter.Feature[] jsonbWriteFeaturesArrayMapping = {
+            JSONWriter.Feature.WriteClassName,
+            JSONWriter.Feature.FieldBased,
+            JSONWriter.Feature.ReferenceDetection,
+            JSONWriter.Feature.WriteNulls,
+            JSONWriter.Feature.NotWriteDefaultValue,
+            JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+            JSONWriter.Feature.WriteNameAsSymbol,
+            JSONWriter.Feature.BeanToArray
+    };
+
+    static final JSONReader.Feature[] jsonbReaderFeaturesSupportBeanArray = {
+            JSONReader.Feature.SupportAutoType,
+            JSONReader.Feature.UseDefaultConstructorAsPossible,
+            JSONReader.Feature.UseNativeObject,
+            JSONReader.Feature.FieldBased,
+            JSONReader.Feature.SupportArrayToBean,
+    };
+
     String str;
     MediaContent object;
 
@@ -25,8 +44,13 @@ public class JSONBArrayMapping {
 
     @Test
     public void test() {
-        byte[] bytes = JSONB.toBytes(object, JSONWriter.Feature.BeanToArray, JSONWriter.Feature.WriteClassName);
-        Object object1 = JSONB.parseObject(bytes, Object.class, JSONReader.Feature.SupportAutoType, JSONReader.Feature.SupportArrayToBean);
-        assertEquals(object, object1);
+        byte[] bytes = JSONB.toBytes(object, jsonbWriteFeaturesArrayMapping);
+        System.out.println(JSONB.toJSONString(bytes));
+        Object object1 = JSONB.parseObject(bytes, Object.class, jsonbReaderFeaturesSupportBeanArray);
+
+        assertEquals(
+                JSON.toJSONString(object, JSONWriter.Feature.PrettyFormat),
+                JSON.toJSONString(object1, JSONWriter.Feature.PrettyFormat)
+        );
     }
 }
