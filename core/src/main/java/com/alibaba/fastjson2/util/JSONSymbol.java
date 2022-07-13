2c368ab7c85a5edf0a6ba98adbeeee85ad568c59
DESKTOP-DSUDK64\LENOVO <1494797292@qq.com>
2022-07-13 17:44:31 +0800

    feat:JSON语法字符聚合 后续会逐渐替换语句中离散的字符串为这些常量字符

---
+++ core/src/main/java/com/alibaba/fastjson2/util/JSONSymbol.java
@@ -0,0 +1,10 @@
+package com.alibaba.fastjson2.util;
+
+public class JSONSymbol {
+    static final char LEFT_BRACE = '{';
+    static final char RIGHT_BRACE = '}';
+    static final char LEFT_BRACKET = '[';
+    static final char RIGHT_BRACKET = ']';
+    static final char COLON = ':';
+    static final char DOUBLE_QUOTE = '"';
+}
