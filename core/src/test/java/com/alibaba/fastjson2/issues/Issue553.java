46d14c8ecc342164852fabb32d18e4e6e12e1183
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-14 00:14:39 +0800

    fix testcase

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue553.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue553.java
@@ -3,14 +3,15 @@ package com.alibaba.fastjson2.issues;
 import com.alibaba.fastjson2.JSONPath;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Issue553 {
     @Test
     public void test() {
         String json = "{\"status\":0}";
         String path = "$.status";
         Object rt1 = JSONPath.extract(json, path);
-        System.out.println(rt1);// 0
         Object rt2 = JSONPath.eval(json, path);
-        System.out.println(rt2);// null
+        assertEquals(rt1, rt2);
     }
 }
