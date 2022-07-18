c22a61ca6252aeef7572271183f18a291dcb544c
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-18 23:03:50 +0800

    add benchmark case

--- benchmark/src/main/java/com/alibaba/fastjson2/benchmark/ASCIICheck.java
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/ASCIICheck.java
@@ -14,7 +14,7 @@ public class ASCIICheck {
     static char[] chars = "http://javaone.com/keynote_large.jpg".toCharArray();
 
     @Benchmark
-    public void f0(Blackhole bh) {
+    public void f0_vec(Blackhole bh) {
         boolean ascii = true;
         {
             int i = 0;
@@ -43,6 +43,46 @@ public class ASCIICheck {
 
     @Benchmark
     public void f1(Blackhole bh) {
+        boolean ascii = true;
+        for (int i = 0; i < chars.length; ++i) {
+            if (chars[i] > 0x007F) {
+                ascii = false;
+                break;
+            }
+        }
+        bh.consume(ascii);
+    }
+
+    @Benchmark
+    public void f2_vec(Blackhole bh) {
+        boolean ascii = true;
+        {
+            int i = 0;
+            while (i + 4 <= chars.length) {
+                char c0 = chars[i];
+                char c1 = chars[i + 1];
+                char c2 = chars[i + 2];
+                char c3 = chars[i + 3];
+                if (c0 > 0x007F || c1 > 0x007F || c2 > 0x007F || c3 > 0x007F) {
+                    ascii = false;
+                    break;
+                }
+                i += 4;
+            }
+            if (ascii) {
+                for (; i < chars.length; ++i) {
+                    if (chars[i] > 0x007F) {
+                        ascii = false;
+                        break;
+                    }
+                }
+            }
+        }
+        bh.consume(ascii);
+    }
+
+    @Benchmark
+    public void f3(Blackhole bh) {
         boolean ascii = true;
         for (int i = 0; i < chars.length; ++i) {
             if (chars[i] > 0x007F) {
