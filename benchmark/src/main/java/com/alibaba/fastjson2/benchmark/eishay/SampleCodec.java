a31466a17f200002435ecf16c3e39da2c737c561
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-22 20:30:48 +0800

    fix check style

--- benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/SampleCodec.java
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/SampleCodec.java
@@ -8,6 +8,7 @@ public class SampleCodec {
     private static Sample sample;
     private static byte[] furyBytes;
     private static Fury fury;
+
     static {
         sample = new Sample();
         sample.populate(true);
@@ -20,9 +21,8 @@ public class SampleCodec {
 
     @Benchmark
     public Object fury_serialize() {
-       return fury.serialize(sample);
+        return fury.serialize(sample);
     }
-
 
     @Benchmark
     public Object fury_deserialize() {
