61f6d9ec5e764254ac483e702a246c577e23dff4
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-07-31 22:56:30 +0800

    improved BigDecimal & Float & Double deserialize performance #602

---
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/primitves/vo/BigDecimal20Field.java
@@ -0,0 +1,26 @@
+package com.alibaba.fastjson2.benchmark.primitves.vo;
+
+import java.math.BigDecimal;
+
+public class BigDecimal20Field {
+    public BigDecimal v0000;
+    public BigDecimal v0001;
+    public BigDecimal v0002;
+    public BigDecimal v0003;
+    public BigDecimal v0004;
+    public BigDecimal v0005;
+    public BigDecimal v0006;
+    public BigDecimal v0007;
+    public BigDecimal v0008;
+    public BigDecimal v0009;
+    public BigDecimal v0010;
+    public BigDecimal v0011;
+    public BigDecimal v0012;
+    public BigDecimal v0013;
+    public BigDecimal v0014;
+    public BigDecimal v0015;
+    public BigDecimal v0016;
+    public BigDecimal v0017;
+    public BigDecimal v0018;
+    public BigDecimal v0019;
+}
