f4a54e403f7b8ccf939ff9901563e813c22a9981
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-03 21:02:00 +0800

    add testcase for issue #610

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue610.java
@@ -0,0 +1,17 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Issue610 {
+    @Test
+    public void test() {
+        assertThrows(
+                JSONException.class,
+                () -> JSON.parse("{\"bbbb\":\"cccc\"\f[+D�HPndroid 10\"}")
+        );
+    }
+}
