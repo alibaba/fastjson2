893f9e422f6e8246e8b229b5a6e1e0d13b95b7b7
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-04 09:22:19 +0800

    bug fix for Map deserialize input null

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue605.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue605.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class Issue605 {
     @Test
@@ -28,6 +29,13 @@ public class Issue605 {
         assertEquals(bo.parems, bo1.parems);
     }
 
+    @Test
+    public void test1() {
+        String str = "{\"parems\":null,\"success\":true,\"wxId\":\"abc\"}";
+        WinningInformationBo bo1 = JSON.parseObject(str, WinningInformationBo.class);
+        assertNull(bo1.parems);
+    }
+
     @Setter
     @Getter
     @Accessors(chain = true)
@@ -35,5 +43,33 @@ public class Issue605 {
         private boolean isSuccess;
         private String wxId;
         private Map<String, String> parems;
+    }
+
+    @Test
+    public void test2() {
+        String str = "{\"params\":null,\"success\":true,\"wxId\":\"abc\"}";
+        Bean bo1 = JSON.parseObject(str, Bean.class);
+        assertNull(bo1.params);
+    }
+
+    @Setter
+    @Getter
+    @Accessors(chain = true)
+    public static class Bean {
+        private Map<String, Integer> params;
+    }
+
+    @Test
+    public void test3() {
+        String str = "{\"params\":null,\"success\":true,\"wxId\":\"abc\"}";
+        Bean bo1 = JSON.parseObject(str, Bean.class);
+        assertNull(bo1.params);
+    }
+
+    @Setter
+    @Getter
+    @Accessors(chain = true)
+    public static class Bean3 {
+        private Map params;
     }
 }
