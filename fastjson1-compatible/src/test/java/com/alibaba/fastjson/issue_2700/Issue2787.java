6393a661f83447875d09905024e4bb5208f60082
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-07 14:37:33 +0800

    remove testcase

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2700/Issue2787.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2700/Issue2787.java
@@ -1,15 +1,10 @@
 package com.alibaba.fastjson.issue_2700;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Issue2787 {
     @Test
@@ -21,16 +16,5 @@ public class Issue2787 {
 
     public static class Model {
         public int[] value;
-    }
-
-    public static class Issue2752 {
-        @Test
-        public void test_for_issue() {
-            Pageable pageRequest = PageRequest.of(0, 10, Sort.Direction.DESC, "id");
-            SerializeConfig config = new SerializeConfig();
-//                    config.register(new MyModule());
-            String result = JSON.toJSONString(pageRequest, config);
-            assertTrue(result.indexOf("\"property\":\"id\"") != -1);
-        }
     }
 }
