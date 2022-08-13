f68a15863de23e877af7c88996aef54e42f65ab5
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-13 22:32:44 +0800

    private CACHE_THRESHOLD & BYTE_ARRAY_CACHE & CHAR_ARRAY_CACHE

--- core/src/main/java/com/alibaba/fastjson2/JSONReaderUTF8.java
+++ core/src/main/java/com/alibaba/fastjson2/JSONReaderUTF8.java
@@ -12,7 +12,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.*;
 import java.util.*;
 
-import static com.alibaba.fastjson2.JSONFactory.CACHE_THRESHOLD;
 import static com.alibaba.fastjson2.JSONFactory.NAME_CACHE;
 import static com.alibaba.fastjson2.JSONFactory.NAME_CACHE2;
 import static com.alibaba.fastjson2.JSONFactory.NameCacheEntry;
@@ -6898,7 +6897,7 @@ class JSONReaderUTF8
 
     @Override
     public void close() {
-        if (cacheIndex != -1 && bytes.length < CACHE_THRESHOLD) {
+        if (cacheIndex != -1) {
             JSONFactory.releaseByteArray(cacheIndex, bytes);
         }
 
