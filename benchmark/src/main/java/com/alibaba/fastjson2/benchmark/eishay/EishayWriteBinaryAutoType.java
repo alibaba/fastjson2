5dd87b4ea7d7e8800281033f900d7f33c079d64c
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-24 23:40:01 +0800

    add benchmark data

---
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/EishayWriteBinaryAutoType.java
@@ -0,0 +1,75 @@
+package com.alibaba.fastjson2.benchmark.eishay;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONB;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.benchmark.eishay.vo.MediaContent;
+import com.caucho.hessian.io.Hessian2Output;
+import org.apache.commons.io.IOUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.TimeUnit;
+
+public class EishayWriteBinaryAutoType {
+    static MediaContent mc;
+
+    static {
+        try {
+            InputStream is = EishayWriteBinaryAutoType.class.getClassLoader().getResourceAsStream("data/eishay.json");
+            String str = IOUtils.toString(is, "UTF-8");
+            mc = JSONReader.of(str)
+                    .read(MediaContent.class);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Benchmark
+    public void fastjson2UTF8Bytes(Blackhole bh) {
+        bh.consume(JSON.toJSONBytes(mc, JSONWriter.Feature.WriteClassName));
+    }
+
+    @Benchmark
+    public void fastjson2JSONB(Blackhole bh) {
+        bh.consume(JSONB.toBytes(mc, JSONWriter.Feature.WriteClassName));
+    }
+
+    @Benchmark
+    public void javaSerialize(Blackhole bh) throws Exception {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(mc);
+        objectOutputStream.flush();
+        bh.consume(byteArrayOutputStream.toByteArray());
+    }
+
+    @Benchmark
+    public void hessian(Blackhole bh) throws Exception {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Hessian2Output hessian2Output = new Hessian2Output(byteArrayOutputStream);
+        hessian2Output.writeObject(mc);
+        hessian2Output.flush();
+        bh.consume(byteArrayOutputStream.toByteArray());
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(EishayWriteBinaryAutoType.class.getName())
+                .mode(Mode.Throughput)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .warmupIterations(3)
+                .forks(1)
+                .build();
+        new Runner(options).run();
+    }
+}
