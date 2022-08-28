42350acd27f43dadda0ea9f106c371c39048b122
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-28 15:32:38 +0800

    bug fix for lombok public field serialize

--- core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderCreator.java
+++ core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderCreator.java
@@ -935,7 +935,20 @@ public class ObjectReaderCreator {
             if (methodName.startsWith("set")) {
                 fieldName = BeanUtils.setterName(methodName, namingStrategy);
             } else {
-                fieldName = BeanUtils.getterName(methodName, namingStrategy);
+                fieldName = BeanUtils.getterName(method, namingStrategy);
+            }
+
+            if (fieldName.length() > 2
+                    && fieldName.charAt(0) >= 'A' && fieldName.charAt(0) <= 'Z'
+                    && fieldName.charAt(1) >= 'A' && fieldName.charAt(1) <= 'Z'
+            ) {
+                char[] chars = fieldName.toCharArray();
+                chars[0] = (char) (chars[0] + 32);
+                String fieldName1 = new String(chars);
+                Field field = BeanUtils.getDeclaredField(objectClass, fieldName1);
+                if (field != null && Modifier.isPublic(field.getModifiers())) {
+                    fieldName = field.getName();
+                }
             }
         } else {
             fieldName = fieldInfo.fieldName;
