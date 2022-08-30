592a398a3a71dabd22b500a1a58e0631923b231a
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-08-30 22:03:42 +0800

    improved android kotlin support

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue715K.kt
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue715K.kt
@@ -1,6 +1,7 @@
 package com.alibaba.fastjson2.issues
 
 import com.alibaba.fastjson2.JSONB
+import com.alibaba.fastjson2.JSONReader
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -9,7 +10,7 @@ class Issue715K {
     fun test() {
         val student = Student("bin", 12, "湖北")
         var jsonbBytes = JSONB.toBytes(student);
-        var student2 = JSONB.parseObject(jsonbBytes, Student::class.java);
+        var student2 = JSONB.parseObject(jsonbBytes, Student::class.java, JSONReader.Feature.FieldBased);
         assertEquals(student.name, student2.name);
         assertEquals(student.sge, student2.sge);
         assertEquals(student.address, student2.address);
