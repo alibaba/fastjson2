7b63bc420aac2f3d6dc62454e6ad948303d111a1
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-02 15:09:24 +0800

    add testcase

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue715K.kt
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue715K.kt
@@ -2,6 +2,7 @@ package com.alibaba.fastjson2.issues
 
 import com.alibaba.fastjson2.JSONB
 import com.alibaba.fastjson2.JSONReader
+import com.alibaba.fastjson2.annotation.JSONField
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -14,6 +15,19 @@ class Issue715K {
         assertEquals(student.name, student2.name);
         assertEquals(student.sge, student2.sge);
         assertEquals(student.address, student2.address);
+    }
+
+    @Test
+    fun test1() {
+        var o = JNote("1001", "BigBook", "a1001", 1, 2, 3);
+        val jsonbBytes = JSONB.toBytes(o);
+        var o1 = JSONB.parseObject(jsonbBytes, JNote::class.java, JSONReader.Feature.FieldBased);
+        assertEquals(o.nodeId, o1.nodeId)
+        assertEquals(o.name, o1.name)
+        assertEquals(o.firstPageId, o1.firstPageId)
+        assertEquals(o.type, o1.type)
+        assertEquals(o.maxPageNumber, o1.maxPageNumber)
+        assertEquals(o.deleteFlag, o1.deleteFlag)
     }
 }
 
@@ -36,3 +50,12 @@ class Student {
 
     }
 }
+
+data class JNote (
+    var nodeId: String,
+    var name: String? = null,
+    var firstPageId: String? = null,
+    var type: Int = 0,
+    var maxPageNumber: Int = 0,
+    var deleteFlag:Int = 0
+)
