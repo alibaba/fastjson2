44a58727f40b1f6ff6eac7c5e17a28cd5be6b28c
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-04 00:35:41 +0800

    add benchmark data

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryTest.java
@@ -5,29 +5,42 @@ import static com.alibaba.fastjson2.benchmark.JMH.BH;
 public class EishayParseBinaryTest {
     static final EishayParseBinary benchmark = new EishayParseBinary();
 
-    public static void fastjson2JSONB_test() throws Exception {
+    public static void fastjson2JSONB() throws Exception {
         System.out.println("fastjson2_jsonb size " + EishayParseBinaryArrayMapping.fastjson2JSONBBytes.length); // 348
         System.out.println();
 
-        for (int i = 0; i < 10; i++) {
-            fastjson2JSONB();
+        for (int j = 0; j < 10; j++) {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 1000 * 1000; ++i) {
+                benchmark.fastjson2JSONB(BH);
+            }
+            long millis = System.currentTimeMillis() - start;
+            System.out.println("EishayParseBinary-fastjson2JSONB millis : " + millis);
+            // zulu8.58.0.13 : 305
+            // zulu11.52.13 :
+            // zulu17.32.13 :
         }
     }
 
-    public static void fastjson2JSONB() throws Exception {
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000 * 1000; ++i) {
-            benchmark.fastjson2JSONB(BH);
+    public static void kryo() throws Exception {
+        System.out.println("fastjson2_jsonb size " + EishayParseBinaryArrayMapping.fastjson2JSONBBytes.length); // 348
+        System.out.println();
+
+        for (int j = 0; j < 10; j++) {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 1000 * 1000; ++i) {
+                benchmark.kryo(BH);
+            }
+            long millis = System.currentTimeMillis() - start;
+            System.out.println("EishayParseBinary-kryo millis : " + millis);
+            // zulu8.58.0.13 : 462
+            // zulu11.52.13 :
+            // zulu17.32.13 :
         }
-        long millis = System.currentTimeMillis() - start;
-        System.out.println("fastjson2_jsonb millis : " + millis);
-        // zulu8.58.0.13 : 334 331
-        // zulu11.52.13 :
-        // zulu17.32.13 : 314 310 301
     }
 
     public static void main(String[] args) throws Exception {
-        fastjson2JSONB_test();
-//        kryo_test();
+        fastjson2JSONB();
+//        kryo();
     }
 }
