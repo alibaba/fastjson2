804d0f25c655b3f49b03cd9795d55ea9c5362de3
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-09 07:27:56 +0800

    fix checkstyle

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/v2issues/Issue739.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/v2issues/Issue739.java
@@ -1,9 +1,9 @@
 package com.alibaba.fastjson.v2issues;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
