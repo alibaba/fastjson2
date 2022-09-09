dfedce35316978602a3cc94528d7b8b7e764a8ae
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-09 23:40:58 +0800

    bug fix for JSON.isValid

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3500/Issue3521.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3500/Issue3521.java
@@ -1,10 +1,29 @@
 package com.alibaba.fastjson2.v1issues.issue_3500;
 
+import com.alibaba.fastjson2.JSON;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Issue3521 {
     @Test
     public void test_for_issue() throws Exception {
-//         assertFalse(JSON.isValid("{\"cat\":\"dog\"\"cat\":\"dog\"}")); // TODO true
+        String str = "{\"cat\":\"dog\"\"cat\":\"dog\"}";
+
+        char[] chars = str.toCharArray();
+        assertFalse(
+                JSON.isValid(chars)
+        );
+
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        assertFalse(
+                JSON.isValid(bytes)
+        );
+
+        assertFalse(
+                JSON.isValid(str)
+        );
     }
 }
