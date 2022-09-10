9b63ff21af24910cbc6a448ae3863e96c4e788a7
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-11 03:02:38 +0800

    2.0.14 init

--- core/src/main/java/com/alibaba/fastjson2/JSON.java
+++ core/src/main/java/com/alibaba/fastjson2/JSON.java
@@ -33,7 +33,7 @@ public interface JSON {
     /**
      * FASTJSON2 version name
      */
-    String VERSION = "2.0.13";
+    String VERSION = "2.0.14";
 
     /**
      * Parse JSON {@link String} into {@link JSONArray} or {@link JSONObject}
