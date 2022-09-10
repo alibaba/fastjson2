ea9d28b4eaf296bf809664d9a67e18a53c8ef516
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-11 03:35:28 +0800

    add testcase for issue #739

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue739.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue739.java
@@ -1,11 +1,17 @@
 package com.alibaba.fastjson2.issues;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.TypeReference;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Issue739 {
     @Test
@@ -22,6 +28,18 @@ public class Issue739 {
                                 new TypeReference<Map<String, Event>>() {}
                         )
                 );
+    }
+
+    @Test
+    public void test1() {
+        List<String> strings = getUEKms();
+        assertEquals(1, strings.size());
+        assertEquals("abc", strings.get(0));
+    }
+
+    public static List<String> getUEKms() {
+        JSONObject config = JSONObject.of("uekms", JSONArray.of("abc"));
+        return config.getJSONArray("uekms").stream().map(Object::toString).collect(Collectors.toList());
     }
 
     public static class Event {
