813673afd4076125148497ab35e64ca1514d7522
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-12 11:20:53 +0800

    add testcase for issue #756

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue756.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue756.java
@@ -1,11 +1,13 @@
 package com.alibaba.fastjson2.issues;
 
 import com.alibaba.fastjson2.JSONB;
+import com.alibaba.fastjson2.JSONException;
 import com.alibaba.fastjson2.JSONReader;
 import com.alibaba.fastjson2.JSONWriter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Issue756 {
     @Test
@@ -15,6 +17,16 @@ public class Issue756 {
         byte[] bytes = JSONB.toBytes(noneSerializable, JSONWriter.Feature.WriteClassName, JSONWriter.Feature.IgnoreNoneSerializable);
         Object o = JSONB.parseObject(bytes, Object.class, JSONReader.Feature.SupportAutoType);
         assertNull(o);
+    }
+
+    @Test
+    public void test_error() {
+        NoneSerializable noneSerializable = new NoneSerializable();
+        noneSerializable.setParam("Test");
+        assertThrows(
+                JSONException.class,
+                () -> JSONB.toBytes(noneSerializable, JSONWriter.Feature.WriteClassName, JSONWriter.Feature.ErrorOnNoneSerializable)
+        );
     }
 
     public static class NoneSerializable {
