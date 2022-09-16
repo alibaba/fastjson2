72332b8d8a416aeda052326c62b27bb8cb20e626
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-16 19:52:38 +0800

    bug fix for IgnoreNoneSerializable

--- core/src/main/java/com/alibaba/fastjson2/reader/FieldReaderObjectField.java
+++ core/src/main/java/com/alibaba/fastjson2/reader/FieldReaderObjectField.java
@@ -133,6 +133,16 @@ class FieldReaderObjectField<T>
 
     @Override
     public void readFieldValueJSONB(JSONReader jsonReader, T object) {
+        if (!fieldClassSerializable) {
+            long contextFeatures = jsonReader.getContext().getFeatures();
+            if ((contextFeatures & JSONReader.Feature.IgnoreNoneSerializable.mask) != 0) {
+                jsonReader.skipValue();
+                return;
+            } else if ((contextFeatures & JSONReader.Feature.ErrorOnNoneSerializable.mask) != 0) {
+                throw new JSONException("not support none-Serializable");
+            }
+        }
+
         if (fieldObjectReader == null) {
             fieldObjectReader = jsonReader.getContext().getObjectReader(fieldType);
         }
