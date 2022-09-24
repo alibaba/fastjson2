02aab9e38cabdad6454f6b1c7e011d28fd2a588e
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-09-24 19:32:10 +0800

    add fury test

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryAutoTypeTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryAutoTypeTest.java
@@ -5,6 +5,25 @@ import static com.alibaba.fastjson2.benchmark.JMH.BH;
 public class EishayParseBinaryAutoTypeTest {
     static final EishayParseBinaryAutoType benchmark = new EishayParseBinaryAutoType();
 
+    public static void fury() throws Exception {
+        System.out.println("EishayParseBinaryAutoType-fury size " + benchmark.furyBytes.length); // 703
+        System.out.println();
+
+        for (int j = 0; j < 10; j++) {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 1000 * 1000; ++i) {
+                benchmark.fury(BH);
+            }
+            long millis = System.currentTimeMillis() - start;
+            System.out.println("EishayParseBinaryAutoType-fury millis : " + millis);
+            // zulu8.58.0.13 : 374
+            // zulu11.52.13 : 388
+            // zulu17.32.13 : 508
+            // zulu18.28.13 : 574
+            // zulu19.28.81 : 504
+        }
+    }
+
     public static void fastjson2JSONB() throws Exception {
         System.out.println("EishayParseBinaryAutoType-fastjson2_jsonb size " + benchmark.fastjson2JSONBBytes.length); // 409
         System.out.println();
@@ -17,8 +36,10 @@ public class EishayParseBinaryAutoTypeTest {
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseBinaryAutoType-fastjson2_jsonb millis : " + millis);
             // zulu8.58.0.13 : 394
-            // zulu11.52.13 :
-            // zulu17.32.13 : 324 319
+            // zulu11.52.13 : 336
+            // zulu17.32.13 : 324 319 326
+            // zulu18.28.13 : 321
+            // zulu19.28.81 : 324
 
             // reflect-zulu17.32.13 : 406
         }
@@ -63,9 +84,10 @@ public class EishayParseBinaryAutoTypeTest {
     }
 
     public static void main(String[] args) throws Exception {
-        fastjson2JSONB();
+//        fastjson2JSONB();
 //        fastjson2JSONB_autoTypeFilter();
 //        fastjson2JSONB_symbols();
 //        kryo_test();
+        fury();
     }
 }
