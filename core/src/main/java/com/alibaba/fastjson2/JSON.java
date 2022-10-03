016886d9bea30f5d09f2a1cdf61539aef5be6575
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-10-03 14:48:33 +0800

    2.0.15.android init

--- core/src/main/java/com/alibaba/fastjson2/JSON.java
+++ core/src/main/java/com/alibaba/fastjson2/JSON.java
@@ -2212,7 +2212,7 @@ public interface JSON {
         if (JVM_VERSION == 8) {
             jsonWriter = new JSONWriterUTF16JDK8(writeContext);
         } else if ((writeContext.features & JSONWriter.Feature.OptimizedForAscii.mask) != 0) {
-            jsonWriter = new JSONWriterUTF8JDK9(writeContext);
+            jsonWriter = new JSONWriterUTF8(writeContext);
         } else {
             jsonWriter = new JSONWriterUTF16(writeContext);
         }
@@ -2278,7 +2278,7 @@ public interface JSON {
         if (JVM_VERSION == 8) {
             jsonWriter = new JSONWriterUTF16JDK8(writeContext);
         } else if ((writeContext.features & JSONWriter.Feature.OptimizedForAscii.mask) != 0) {
-            jsonWriter = new JSONWriterUTF8JDK9(writeContext);
+            jsonWriter = new JSONWriterUTF8(writeContext);
         } else {
             jsonWriter = new JSONWriterUTF16(writeContext);
         }
