c59ab1cff6c1df486d168c4d69b6c7241d5fdcc8
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-10-29 15:45:40 +0800

    fix JSONPath#set, for issue #842

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue842.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue842.java
@@ -1,6 +1,7 @@
 package com.alibaba.fastjson2.issues;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.JSONPath;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +25,19 @@ public class Issue842 {
         Map data = new LinkedHashMap<>();
         JSONPath.set(data, jsonPath, "设置成功");
         assertEquals("{\"items\":[{\"data\":[\"设置成功\"]}]}", JSON.toJSONString(data));
+    }
+
+    @Test
+    public void test2() {
+        Map data = new LinkedHashMap<>();
+        JSONPath.set(data, "$.obj.5", "设置成功");
+        assertEquals("{\"obj\":[null,null,null,null,null,\"设置成功\"]}", JSONObject.toJSONString(data));
+    }
+
+    @Test
+    public void test3() {
+        Map data = new LinkedHashMap<>();
+        JSONPath.set(data, "$.obj.2.5", "设置成功");
+        assertEquals("{\"obj\":[null,null,[null,null,null,null,null,\"设置成功\"]]}", JSONObject.toJSONString(data));
     }
 }
