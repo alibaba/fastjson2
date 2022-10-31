25486e47a78541f46fe59efc6bd282e08286b40e
zhangwd3 <zhangwd3@asiainfo.com>
2022-10-31 17:07:01 +0800

    [fix]:[881][泛型字段WriteNullListAsEmpty无效问题]

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue881.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue881.java
@@ -1,6 +1,5 @@
 package com.alibaba.fastjson2.issues;
 
-
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import lombok.Getter;
@@ -9,19 +8,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Issue881 {
 
     @Test
     public void fastjson2Test() {
         TestClass<List<String>> testClass = new TestClass<>();
         JSON.config(JSONWriter.Feature.WriteNullListAsEmpty, JSONWriter.Feature.WriteNulls);
-        System.out.println(JSON.toJSONString(testClass));
-
+        System.out.println();
+        assertEquals("{\"str\":null,\"stringList\":[]}",
+                JSON.toJSONString(testClass));
     }
 
     @Getter
     @Setter
-    static class TestClass<T>{
+    static class TestClass<T> {
         private T stringList;
         private String str;
     }
