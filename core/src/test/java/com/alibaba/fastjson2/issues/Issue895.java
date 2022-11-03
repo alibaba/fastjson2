6e53110cfeb06a14412a15b5d6f71e4b2364fbc5
Kraity <kraits@qq.com>
2022-11-03 21:35:56 +0800

    fix: checkstyle for testcase issue895

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue895.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue895.java
@@ -2,7 +2,6 @@ package com.alibaba.fastjson2.issues;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
