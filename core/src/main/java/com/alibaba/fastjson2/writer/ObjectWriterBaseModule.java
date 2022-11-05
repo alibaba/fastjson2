a425f356cfc262c00c72093fe075694f1613be59
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-11-05 23:21:53 +0800

    2.0.18.android init

--- core/src/main/java/com/alibaba/fastjson2/writer/ObjectWriterBaseModule.java
+++ core/src/main/java/com/alibaba/fastjson2/writer/ObjectWriterBaseModule.java
@@ -608,10 +608,6 @@ public class ObjectWriterBaseModule
                 fieldInfo.fieldClassMixIn = true;
             }
 
-            if (JDKUtils.CLASS_TRANSIENT != null && method.getAnnotation(JDKUtils.CLASS_TRANSIENT) != null) {
-                fieldInfo.ignore = true;
-            }
-
             Annotation[] annotations = getAnnotations(method);
             processAnnotations(fieldInfo, annotations);
 
