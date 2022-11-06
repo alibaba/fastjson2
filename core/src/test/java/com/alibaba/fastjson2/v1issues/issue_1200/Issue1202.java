22bf662197344100207c0eef3650d5688dc69daf
zhatian <289460202@qq.com>
2022-11-06 21:39:27 +0800

    [fix]:[889][2.0.17-SNAPSHOT core 模块下运行单元测试有 13 个没有通过] #889
    1、本次提交代码只修复了 Issue1202 下的 6 个测试
    2、为了阅读测试代码更方便：合并了不同 Model 的测试到 Nested 类；提取了公共变量 DATE_TEXT
    3、虽然设置 Locale.setDefault(Locale.ENGLISH); 后单元测试可以通过，但可能和原本的测试意图不一致
    4、尝试过在 @JSONField 中添加属性（locale = "en"），但依然不能通过测试。
    5、真正要实现的功能是不需要在 @JSONField 中设置 locale 属性，仅根据 format 属性就能正确解析日期字符串吗？

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1200/Issue1202.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1200/Issue1202.java
@@ -6,9 +6,13 @@ import com.alibaba.fastjson2.TestUtils;
 import com.alibaba.fastjson2.annotation.JSONField;
 import com.alibaba.fastjson2.reader.ObjectReader;
 import com.alibaba.fastjson2.reader.ObjectReaderCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -16,94 +20,104 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Created by wenshao on 16/05/2017.
  */
 public class Issue1202 {
-    @Test
-    public void test_for_issue_0() throws Exception {
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
-        Model1 model = JSON.parseObject(text, Model1.class);
-        assertNotNull(model.date);
+
+    private final String DATE_TEXT = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
+
+    @BeforeEach
+    void setup() {
+        Locale.setDefault(Locale.ENGLISH);
     }
 
-    @Test
-    public void test_for_issue_0_creators() throws Exception {
-        ObjectReaderCreator[] creators = TestUtils.readerCreators2();
+    @Nested
+    class Model1Test {
 
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
+        @Test
+        public void test_for_issue_0() {
+            Model1 model = JSON.parseObject(DATE_TEXT, Model1.class);
+            assertNotNull(model.getDate());
+        }
 
-        for (ObjectReaderCreator creator : creators) {
-            ObjectReader<Model1> objectReader = creator.createObjectReader(Model1.class);
-            Model1 model = objectReader.readObject(JSONReader.of(text));
+        @Test
+        public void test_for_issue_0_creators() {
+            ObjectReaderCreator[] creators = TestUtils.readerCreators2();
+
+            for (ObjectReaderCreator creator : creators) {
+                ObjectReader<Model1> objectReader = creator.createObjectReader(Model1.class);
+                Model1 model = objectReader.readObject(JSONReader.of(DATE_TEXT));
+                assertNotNull(model.getDate());
+            }
+        }
+
+        public class Model1 {
+            @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
+            private Date date;
+
+            public Date getDate() {
+                return date;
+            }
+
+            public void setDate(Date date) {
+                this.date = date;
+            }
+        }
+    }
+
+    @Nested
+    class Model2Test {
+        @Test
+        public void test_for_issue_2() throws Exception {
+            Model2 model = JSON.parseObject(DATE_TEXT, Model2.class);
             assertNotNull(model.date);
         }
+
+        @Test
+        public void test_for_issue_2_creators() {
+            ObjectReaderCreator[] creators = TestUtils.readerCreators();
+
+            for (ObjectReaderCreator creator : creators) {
+                ObjectReader<Model2> objectReader = creator.createObjectReader(Model2.class);
+                Model2 model = objectReader.readObject(JSONReader.of(DATE_TEXT));
+                assertNotNull(model.date);
+            }
+        }
+
+        public class Model2 {
+            @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
+            public Date date;
+        }
     }
 
-    @Test
-    public void test_for_issue_2() throws Exception {
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
-        Model2 model = JSON.parseObject(text, Model2.class);
-        assertNotNull(model.date);
-    }
-
-    @Test
-    public void test_for_issue_2_creators() throws Exception {
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
-
-        ObjectReaderCreator[] creators = TestUtils.readerCreators();
-
-        for (ObjectReaderCreator creator : creators) {
-            ObjectReader<Model2> objectReader = creator.createObjectReader(Model2.class);
-            Model2 model = objectReader.readObject(JSONReader.of(text));
+    @Nested
+    class Model3Test {
+        @Test
+        public void test_for_issue_3() throws Exception {
+            Model3 model = JSON.parseObject(DATE_TEXT, Model3.class);
             assertNotNull(model.date);
         }
-    }
 
-    @Test
-    public void test_for_issue_3() throws Exception {
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
-        Model3 model = JSON.parseObject(text, Model3.class);
-        assertNotNull(model.date);
-    }
+        @Test
+        public void test_for_issue_3_creators() {
+            ObjectReaderCreator[] creators = TestUtils.readerCreators2();
 
-    @Test
-    public void test_for_issue_3_creators() throws Exception {
-        String text = "{\"date\":\"Apr 27, 2017 5:02:17 PM\"}";
+            for (ObjectReaderCreator creator : creators) {
+                ObjectReader<Model3> objectReader = creator.createObjectReader(Model3.class);
+                Model3 model = objectReader.readObject(JSONReader.of(DATE_TEXT));
+                assertNotNull(model.date);
+            }
+        }
 
-        ObjectReaderCreator[] creators = TestUtils.readerCreators2();
+        private class Model3 {
+            @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
+            private Date date;
 
-        for (ObjectReaderCreator creator : creators) {
-            ObjectReader<Model3> objectReader = creator.createObjectReader(Model3.class);
-            Model3 model = objectReader.readObject(JSONReader.of(text));
-            assertNotNull(model.date);
+            public Date getDate() {
+                return date;
+            }
+
+            public void setDate(Date date) {
+                this.date = date;
+            }
         }
     }
 
-    public static class Model1 {
-        @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
-        private Date date;
-
-        public Date getDate() {
-            return date;
-        }
-
-        public void setDate(Date date) {
-            this.date = date;
-        }
-    }
-
-    public static class Model2 {
-        @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
-        public Date date;
-    }
-
-    private static class Model3 {
-        @JSONField(format = "MMM dd, yyyy h:mm:ss aa")
-        private Date date;
-
-        public Date getDate() {
-            return date;
-        }
-
-        public void setDate(Date date) {
-            this.date = date;
-        }
-    }
 }
