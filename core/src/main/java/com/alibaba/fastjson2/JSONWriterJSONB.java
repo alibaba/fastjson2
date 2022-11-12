b782e5eaa0e770f257a18580896b76d4d6a42417
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-11-12 12:35:28 +0800

    2.0.19.android init

--- core/src/main/java/com/alibaba/fastjson2/JSONWriterJSONB.java
+++ core/src/main/java/com/alibaba/fastjson2/JSONWriterJSONB.java
@@ -1902,6 +1902,11 @@ final class JSONWriterJSONB
     }
 
     @Override
+    public void writeHex(byte[] bytes) {
+        throw new JSONException("UnsupportedOperation");
+    }
+
+    @Override
     public void writeRaw(char ch) {
         throw new JSONException("UnsupportedOperation");
     }
@@ -1976,6 +1981,11 @@ final class JSONWriterJSONB
     @Override
     public byte[] getBytes() {
         return Arrays.copyOf(bytes, off);
+    }
+
+    @Override
+    public byte[] getBytes(Charset charset) {
+        throw new JSONException("not support operator");
     }
 
     @Override
