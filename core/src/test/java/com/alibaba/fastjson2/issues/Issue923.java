e8f91df9398b12edf9951a09c0dcd342e5012c9b
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-11-12 09:20:31 +0800

    add testcase for issue #923

---
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue923.java
@@ -0,0 +1,43 @@
+package com.alibaba.fastjson2.issues;
+
+import com.alibaba.fastjson2.JSON;
+import lombok.AccessLevel;
+import lombok.ToString;
+import lombok.Value;
+import lombok.With;
+import lombok.experimental.NonFinal;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue923 {
+    @Test
+    public void test() {
+        Bean bean = new Bean("DataWorks", 101, 102, new String[]{"abc"});
+        String str = JSON.toJSONString(bean);
+        assertEquals("{\"age\":101,\"name\":\"DataWorks\",\"score\":102.0,\"tags\":[\"abc\"]}", str);
+        Bean bean1 = JSON.parseObject(str, Bean.class);
+        assertEquals(bean.age, bean1.age);
+        assertEquals(bean.name, bean1.name);
+        assertEquals(bean.score, bean1.score);
+        assertArrayEquals(bean.tags, bean1.tags);
+    }
+
+    @Value
+    public static class Bean {
+        String name;
+        @With(AccessLevel.PACKAGE)
+        @NonFinal
+        int age;
+        double score;
+        protected String[] tags;
+
+        @ToString(includeFieldNames = true)
+        @Value(staticConstructor = "of")
+        public static class Exercise<T> {
+            String name;
+            T value;
+        }
+    }
+}
