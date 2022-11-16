f2fcf6b0a41180c203337e8a0cc88db90a4f89f3
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-11-17 00:37:11 +0800

    update fury benchmark code

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryParseTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryParseTest.java
@@ -2,22 +2,25 @@ package com.alibaba.fastjson2.benchmark.eishay;
 
 import static com.alibaba.fastjson2.benchmark.JMH.BH;
 
-public class EishayParseBinaryArrayMappingTest {
-    static final EishayParseBinaryArrayMapping benchmark = new EishayParseBinaryArrayMapping();
+public class EishayFuryParseTest {
+    static final EishayFuryParse benchmark = new EishayFuryParse();
 
-    public static void kryo() throws Exception {
-        System.out.println("kryo size " + EishayParseBinaryArrayMapping.kryoBytes.length); // 214
+    public static void fury() throws Exception {
+        System.out.println("EishayParseBinaryArrayMapping-fury size " + benchmark.furyBytes.length); // 703
         System.out.println();
+
         for (int j = 0; j < 10; j++) {
             long start = System.currentTimeMillis();
             for (int i = 0; i < 1000 * 1000; ++i) {
-                benchmark.kryo(BH);
+//                benchmark.fury(BH);
             }
             long millis = System.currentTimeMillis() - start;
-            System.out.println("EishayParseBinaryArrayMapping-kryo millis : " + millis);
-            // zulu8.58.0.13 : 457
-            // zulu11.52.13 :
-            // zulu17.32.13 :
+            System.out.println("EishayParseBinaryArrayMapping-fury millis : " + millis);
+            // zulu8.58.0.13 : 374 392
+            // zulu11.52.13 : 388 493
+            // zulu17.32.13 : 508 437
+            // zulu18.28.13 : 574
+            // zulu19.28.81 : 504
         }
     }
 
@@ -41,6 +44,5 @@ public class EishayParseBinaryArrayMappingTest {
     public static void main(String[] args) throws Exception {
         fastjson2JSONB();
 //        kryo();
-//        fury();
     }
 }
