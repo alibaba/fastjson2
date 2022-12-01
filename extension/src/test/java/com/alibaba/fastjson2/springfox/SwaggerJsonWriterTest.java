f0d0e8b3e07e0175f81f862de9f0616c43329f92
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-12-01 23:29:11 +0800

    fix testcase for issue #973

--- extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
+++ extension/src/test/java/com/alibaba/fastjson2/springfox/SwaggerJsonWriterTest.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson2.JSON;
 import org.junit.jupiter.api.Test;
 import springfox.documentation.spring.web.json.Json;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SwaggerJsonWriterTest {
@@ -16,6 +18,7 @@ public class SwaggerJsonWriterTest {
 
     @Test
     public void test1() {
-        assertEquals(jsonStr, new String(JSON.toJSONBytes(new Json(jsonStr))));
+        byte[] bytes = JSON.toJSONBytes(new Json(jsonStr));
+        assertEquals(jsonStr, new String(bytes, StandardCharsets.UTF_8));
     }
 }
