649d7dac4a9c3f73ad392c81827020b86ed6cd29
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-12-14 21:18:08 +0800

    add testcase

--- adapter/src/main/java/com/alibaba/fastjson2/adapter/jackson/core/JsonPointer.java
+++ adapter/src/main/java/com/alibaba/fastjson2/adapter/jackson/core/JsonPointer.java
@@ -1,9 +1,16 @@
 package com.alibaba.fastjson2.adapter.jackson.core;
 
-import com.alibaba.fastjson2.JSONException;
+import com.alibaba.fastjson2.JSONPath;
 
 public class JsonPointer {
+    final JSONPath path;
+
+    JsonPointer(JSONPath path) {
+        this.path = path;
+    }
+
     public static JsonPointer compile(String expr) throws IllegalArgumentException {
-        throw new JSONException("TODO");
+        JSONPath jsonPath = JSONPath.of(expr);
+        return new JsonPointer(jsonPath);
     }
 }
