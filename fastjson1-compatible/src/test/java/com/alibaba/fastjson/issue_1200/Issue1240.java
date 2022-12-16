13d31731310d4e35926d7a6e977493e017cb7465
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-12-16 21:25:36 +0800

    fix testcase for spring 3

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1200/Issue1240.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1200/Issue1240.java
@@ -2,9 +2,11 @@ package com.alibaba.fastjson.issue_1200;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.LinkedMultiValueMap;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by wenshao on 01/06/2017.
@@ -14,9 +16,21 @@ public class Issue1240 {
     public void test_for_issue() throws Exception {
         ParserConfig parserConfig = new ParserConfig();
 //        parserConfig.setAutoTypeSupport(true);
-        LinkedMultiValueMap<String, String> result = new LinkedMultiValueMap();
-        result.add("test", "11111");
-        String test = JSON.toJSONString(result, SerializerFeature.WriteClassName);
+        Class<?> objectClass;
+        try {
+            objectClass = Class.forName("org.springframework.util.LinkedMultiValueMap");
+        } catch (Throwable ignored) {
+            return;
+        }
+        Method add = objectClass.getMethod("add", Object.class, Object.class);
+        if (add == null) {
+            return;
+        }
+
+        Object map = objectClass.newInstance();
+        add.invoke(map, "test", "11111");
+        String str = JSON.toJSONString(map);
+        assertEquals("{\"test\":[\"11111\"]}", str);
 //        JSON.parseObject(test, Object.class, parserConfig, JSON.DEFAULT_PARSER_FEATURE);
     }
 }
