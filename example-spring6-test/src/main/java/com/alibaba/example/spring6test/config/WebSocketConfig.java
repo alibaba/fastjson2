94cde4bd9886ac30e195e6456e71314ab6a2f9ab
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-12-19 16:34:35 +0800

    spring6 support

--- example-spring6-test/src/main/java/com/alibaba/example/spring6test/config/WebSocketConfig.java
+++ example-spring6-test/src/main/java/com/alibaba/example/spring6test/config/WebSocketConfig.java
@@ -4,12 +4,11 @@ import com.alibaba.fastjson2.support.spring6.websocket.sockjs.FastjsonSockJsMess
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig
+        implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         System.out.println("初始化路径拦截");
         webSocketHandlerRegistry.addHandler(new ChatMessageHandler(), "/websocket/*").withSockJS().setMessageCodec(new FastjsonSockJsMessageCodec());
     }
-
 }
-
