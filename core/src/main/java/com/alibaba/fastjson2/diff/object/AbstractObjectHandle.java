f0bd52e5b44082fc1beac7e02c13100606067393
windwheel <yuluoxinsheng@gmail.com>
2022-12-26 21:51:36 +0800

    remove comment

--- core/src/main/java/com/alibaba/fastjson2/diff/object/AbstractObjectHandle.java
+++ core/src/main/java/com/alibaba/fastjson2/diff/object/AbstractObjectHandle.java
@@ -15,13 +15,12 @@ public abstract class AbstractObjectHandle extends AbstractDiffHandle implements
 
     @Override
     public JsonCompareResult handle(JSONObject expectObject, JSONObject actualObject) {
-        // 两个都为null
         if (expectObject == null && actualObject == null) {
             return RunTimeDataFactory.getResultInstance();
         }
         Set<String> expectKeys = expectObject.keySet();
         Set<String> actualKeys = actualObject.keySet();
-        // 空对象
+
         if (expectKeys.size() == 0 && actualKeys.size() == 0) {
             return RunTimeDataFactory.getResultInstance();
         }
