d69000b162463ceef85629db3e4501a13e84005f
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-12-28 11:08:58 +0800

    add testcase for issue #1019

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1019.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1019.java
@@ -1,7 +1,11 @@
 package com.alibaba.fastjson2.issues_1000;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONFactory;
+import com.alibaba.fastjson2.PropertyNamingStrategy;
 import com.alibaba.fastjson2.annotation.JSONField;
+import com.alibaba.fastjson2.reader.ObjectReaderProvider;
+import com.alibaba.fastjson2.writer.ObjectWriterProvider;
 import lombok.Data;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +24,23 @@ public class Issue1019 {
     @Data
     static class Bean {
         @JSONField(name = "oAuthName")
+        private String oAuthName;
+        private Integer age;
+    }
+
+    @Test
+    public void test1() {
+        ObjectWriterProvider provider = new ObjectWriterProvider(PropertyNamingStrategy.CamelCase1x);
+
+        Bean1 bean = new Bean1();
+        bean.age = 20;
+        bean.oAuthName = "xxx";
+        String str = JSON.toJSONString(bean, JSONFactory.createWriteContext(provider));
+        assertEquals("{\"age\":20,\"oAuthName\":\"xxx\"}", str);
+    }
+
+    @Data
+    static class Bean1 {
         private String oAuthName;
         private Integer age;
     }
