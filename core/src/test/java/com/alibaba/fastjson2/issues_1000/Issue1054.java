0ea9135d1142f7cd3875061f2091dba94bfbbbf2
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-01-06 22:09:31 +0800

    bug fix for @JSONType(deserializeFeatures=JSONReader.Feature.FieldBased), for issue #1054

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1054.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1054.java
@@ -1,13 +1,16 @@
 package com.alibaba.fastjson2.issues_1000;
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.annotation.JSONField;
+import com.alibaba.fastjson2.annotation.JSONType;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Issue1054 {
     @Test
@@ -17,6 +20,11 @@ public class Issue1054 {
         assertEquals(new BigDecimal("24.4801"), temperature.value);
     }
 
+    @JSONType(
+            serializeFeatures = JSONWriter.Feature.FieldBased,
+            deserializeFeatures = {JSONReader.Feature.UseDefaultConstructorAsPossible, JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean},
+            ignores = {"dataTime", "id", "sensorId"}
+    )
     public class SensorSoilTemperature
             extends SensorModel {
         @JSONField(name = "value")
@@ -32,5 +40,34 @@ public class Issue1054 {
     }
 
     public static class SensorModel {
+    }
+
+    @Test
+    public void test1() {
+        String str = "{\n" +
+                "    \"success\": false,\n" +
+                "    \"requestId\": \"requestId\",\n" +
+                "    \"error\": [\n" +
+                "        {\n" +
+                "            \"code\": \"111\",\n" +
+                "            \"message\": \"message\",\n" +
+                "            \"longMessage\": \"--\",\n" +
+                "            \"source\": \"666\"\n" +
+                "        }\n" +
+                "    ],\n" +
+                "    \"data\": {\n" +
+                "        \"requestString\": \"{\\\"scope\\\":\\\"1\\\",\\\"action\\\":\\\"1\\\",\\\"OrderNumber\\\":\\\"1\\\"}\"\n" +
+                "    }\n" +
+                "}";
+
+        Bean bean = JSON.parseObject(str, Bean.class);
+        assertNotNull(bean);
+    }
+
+    public static class Bean {
+        public boolean success;
+        public String requestId;
+        public String data;
+        public String error;
     }
 }
