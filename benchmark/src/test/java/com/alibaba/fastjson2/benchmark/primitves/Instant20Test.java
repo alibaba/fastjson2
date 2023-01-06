8a0067ffa4834eec7314a53e59ee88ac725c65e2
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-01-06 19:44:44 +0800

    update benchmark

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/primitves/Instant20Test.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/primitves/Instant20Test.java
@@ -32,33 +32,29 @@ public class Instant20Test {
         // oracle-jdk-18.0.2 :
     }
 
-    public static void fastjson2_jsonb_test() {
-        for (int i = 0; i < 10; i++) {
-            fastjson2_jsonb();
-        }
-    }
-
     public static void fastjson2_jsonb() {
-        Instant20 benchmark = new Instant20();
+        for (int j = 0; j < 10; j++) {
+            Instant20 benchmark = new Instant20();
 
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000 * 1000; ++i) {
-            benchmark.fastjson2_jsonb(BH);
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 1000 * 1000; ++i) {
+                benchmark.fastjson2_jsonb(BH);
+            }
+            long millis = System.currentTimeMillis() - start;
+            System.out.println("Instant20-fastjson2 : " + millis);
+
+            // zulu8.62.0.19 : 310
+            // zulu11.52.13 :
+            // zulu17.32.13 :
+            // zulu18.28.13 :
+            // zulu19.0.47 :
+            // corretto-8 :
+            // corretto-11 :
+            // corretto-17 : 298
+            // corretto-18 :
+            // oracle-jdk-17.0.4 :
+            // oracle-jdk-18.0.2 :
         }
-        long millis = System.currentTimeMillis() - start;
-        System.out.println("Instant20-fastjson2 : " + millis);
-
-        // zulu8.62.0.19 :
-        // zulu11.52.13 :
-        // zulu17.32.13 :
-        // zulu18.28.13 :
-        // zulu19.0.47 :
-        // corretto-8 :
-        // corretto-11 :
-        // corretto-17 : 298
-        // corretto-18 :
-        // oracle-jdk-17.0.4 :
-        // oracle-jdk-18.0.2 :
     }
 
     public static void fastjson1_test() throws Exception {
@@ -91,7 +87,7 @@ public class Instant20Test {
 
     public static void main(String[] args) throws Exception {
 //        fastjson2_test();
-        fastjson2_jsonb_test();
+        fastjson2_jsonb();
 //        fastjson1_test();
     }
 }
