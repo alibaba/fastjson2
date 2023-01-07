1a43e72ad89ad3f8ec5a8784a2822d735d0c348c
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-01-07 14:01:27 +0800

    add jsonb testcase

--- core/src/test/java/com/alibaba/fastjson2/jsonb/IntegerTest.java
+++ core/src/test/java/com/alibaba/fastjson2/jsonb/IntegerTest.java
@@ -3,12 +3,22 @@ package com.alibaba.fastjson2.jsonb;
 import com.alibaba.fastjson2.JSONB;
 import org.junit.jupiter.api.Test;
 
-import static com.alibaba.fastjson2.JSONB.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntegerTest {
+    static final int INT32_SHORT_MIN = -0x40000; // -262144
+    static final int INT32_SHORT_MAX = 0x3ffff;  // 262143
+
+    static final int INT32_BYTE_MIN = -0x800; // -2048
+    static final int INT32_BYTE_MAX = 0x7ff;  // 2047
+    static final byte BC_INT32_SHORT_ZERO = 68;
+    static final byte BC_INT32_BYTE_ZERO = 56;
+
     @Test
     public void testIntNum() {
+        final byte BC_INT32_NUM_MIN = -16; // 0xf0
+        final byte BC_INT32_NUM_MAX = 47;  // 0x2f
+
         for (int i = BC_INT32_NUM_MIN; i <= BC_INT32_NUM_MAX; i++) {
             byte[] bytes = JSONB.toBytes(i);
             assertEquals(1, bytes.length);
@@ -18,6 +28,9 @@ public class IntegerTest {
 
     @Test
     public void testIntByte() {
+        final byte BC_INT32_NUM_MIN = -16; // 0xf0
+        final byte BC_INT32_NUM_MAX = 47;  // 0x2f
+
         for (int i = INT32_BYTE_MIN; i <= INT32_BYTE_MAX; i++) {
             if (i >= BC_INT32_NUM_MIN && i <= BC_INT32_NUM_MAX) {
                 continue;
@@ -36,9 +49,6 @@ public class IntegerTest {
     @Test
     public void testIntShort() {
         for (int i = INT32_SHORT_MIN; i <= INT32_SHORT_MAX; i++) {
-            if (i >= BC_INT32_NUM_MIN && i <= BC_INT32_NUM_MAX) {
-                continue;
-            }
             if (i >= INT32_BYTE_MIN && i <= INT32_BYTE_MAX) {
                 continue;
             }
