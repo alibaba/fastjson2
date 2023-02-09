49c8667c1d81e937a64dbeef2e651ee10ba188e7
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-02-10 01:20:33 +0800

    fix process enum field annotation, for issue #1062

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_2000/Issue2065C.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_2000/Issue2065C.java
@@ -3,16 +3,18 @@ package com.alibaba.fastjson2.v1issues.issue_2000;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONException;
+import com.alibaba.fastjson2.JSONReader;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class Issue2065C {
     @Test
     public void test_for_issue() throws Exception {
         Exception error = null;
         try {
-            JSON.parseObject("{\"code\":1}", Model.class);
+            JSON.parseObject("{\"code\":0}", Model.class, JSONReader.Feature.ErrorOnEnumNotMatch);
         } catch (JSONException e) {
             error = e;
         }
@@ -23,7 +25,7 @@ public class Issue2065C {
     public void test_for_issue_01() {
         Exception error = null;
         try {
-            JSON.parseObject("1", EnumClass.class);
+            JSON.parseObject("0", EnumClass.class, JSONReader.Feature.ErrorOnEnumNotMatch);
         } catch (JSONException e) {
             error = e;
         }
@@ -32,12 +34,14 @@ public class Issue2065C {
 
     @Test
     public void test_for_issue_02() {
-        JSON.parseObject("0", EnumClass.class);
+        assertSame(EnumClass.A, JSON.parseObject("1", EnumClass.class));
+        assertSame(EnumClass.B, JSON.parseObject("2", EnumClass.class));
     }
 
     @Test
     public void test_for_issue_03() {
-        JSON.parseObject("{\"code\":0}", Model.class);
+        assertSame(EnumClass.A, JSON.parseObject("{\"code\":1}", Model.class).code);
+        assertSame(EnumClass.B, JSON.parseObject("{\"code\":2}", Model.class).code);
     }
 
     public static class Model {
@@ -57,7 +61,8 @@ public class Issue2065C {
     }
 
     public static enum EnumClass {
-        A(1);
+        A(1),
+        B(2);
 
         @JSONField
         private int code;
