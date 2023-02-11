9e156a2f97baa45f808711108e911c29b514e994
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-02-11 15:03:28 +0800

    Eishay benchmark VO use private field

--- benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/mixin/MediaMixin.java
+++ benchmark/src/main/java/com/alibaba/fastjson2/benchmark/eishay/mixin/MediaMixin.java
@@ -16,31 +16,31 @@ import static com.alibaba.fastjson2.reader.ObjectReaders.*;
 public class MediaMixin {
     public static final ObjectWriter<Media> objectWriter = ObjectWriters.objectWriter(
             Media.class,
-            ObjectWriters.fieldWriter("bitrate", (Media e) -> e.bitrate),
-            ObjectWriters.fieldWriter("duration", (Media e) -> e.duration),
-            ObjectWriters.fieldWriter("format", (Media e) -> e.format),
-            ObjectWriters.fieldWriter("height", (Media e) -> e.height),
-            ObjectWriters.fieldWriterList("persons", String.class, (Media e) -> e.persons),
-            ObjectWriters.fieldWriter("player", Media.Player.class, (Media e) -> e.player),
-            ObjectWriters.fieldWriter("size", (Media e) -> e.size),
-            ObjectWriters.fieldWriter("title", (Media e) -> e.title),
-            ObjectWriters.fieldWriter("uri", (Media e) -> e.uri),
-            ObjectWriters.fieldWriter("width", (Media e) -> e.width),
-            ObjectWriters.fieldWriter("copyright", (Media e) -> e.copyright)
+            ObjectWriters.fieldWriter("bitrate", (Media e) -> e.getBitrate()),
+            ObjectWriters.fieldWriter("duration", (Media e) -> e.getDuration()),
+            ObjectWriters.fieldWriter("format", (Media e) -> e.getFormat()),
+            ObjectWriters.fieldWriter("height", (Media e) -> e.getHeight()),
+            ObjectWriters.fieldWriterList("persons", String.class, (Media e) -> e.getPersons()),
+            ObjectWriters.fieldWriter("player", Media.Player.class, (Media e) -> e.getPlayer()),
+            ObjectWriters.fieldWriter("size", (Media e) -> e.getSize()),
+            ObjectWriters.fieldWriter("title", (Media e) -> e.getTitle()),
+            ObjectWriters.fieldWriter("uri", (Media e) -> e.getUri()),
+            ObjectWriters.fieldWriter("width", (Media e) -> e.getWidth()),
+            ObjectWriters.fieldWriter("copyright", (Media e) -> e.getCopyright())
     );
 
     public static final ObjectReader<Media> objectReader = ObjectReaders.of(
             Media::new,
-            fieldReaderInt("bitrate", (Media o, int v) -> o.bitrate = v),
-            fieldReaderLong("duration", (Media o, long v) -> o.duration = v),
-            fieldReaderString("format", (Media o, String v) -> o.format = v),
-            fieldReaderInt("height", (Media o, int v) -> o.height = v),
-            fieldReaderList("persons", String.class, ArrayList::new, (Media o, List<String> v) -> o.persons = v),
-            fieldReader("player", Media.Player.class, (Media o, Media.Player v) -> o.player = v),
-            fieldReaderLong("size", (Media o, long v) -> o.size = v),
-            fieldReaderString("title", (Media o, String v) -> o.title = v),
-            fieldReaderString("uri", (Media o, String v) -> o.uri = v),
-            fieldReaderInt("width", (Media o, int v) -> o.width = v),
-            fieldReaderString("copyright", (Media o, String v) -> o.copyright = v)
+            fieldReaderInt("bitrate", Media::setBitrate),
+            fieldReaderLong("duration", Media::setDuration),
+            fieldReaderString("format", Media::setFormat),
+            fieldReaderInt("height", Media::setHeight),
+            fieldReaderList("persons", String.class, ArrayList::new, (Media o, List<String> v) -> o.setPersons(v)),
+            fieldReader("player", Media.Player.class, (Media o, Media.Player v) -> o.setPlayer(v)),
+            fieldReaderLong("size", Media::setSize),
+            fieldReaderString("title", Media::setTitle),
+            fieldReaderString("uri", Media::setUri),
+            fieldReaderInt("width", Media::setWidth),
+            fieldReaderString("copyright", Media::setCopyright)
     );
 }
