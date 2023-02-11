6b3968aad2f59172ba346512461e426b8388c44b
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-02-11 15:42:06 +0800

    update fury benchmark data

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryParseTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryParseTest.java
@@ -6,17 +6,17 @@ public class EishayFuryParseTest {
     static final EishayFuryParse benchmark = new EishayFuryParse();
     static final int COUNT = 10_000_000;
 
-    public static void fastjson2JSONB() throws Exception {
-        System.out.println("EishayFuryParse-fastjson2_jsonb size " + benchmark.fastjson2JSONBBytes.length); // 282
+    public static void jsonb() throws Exception {
+        System.out.println("EishayFuryParse-jsonb size " + benchmark.jsonbBytes.length); // 282
         System.out.println();
 
         for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
             for (int i = 0; i < COUNT; ++i) {
-                benchmark.fastjson2JSONB(BH);
+                benchmark.jsonb(BH);
             }
             long millis = System.currentTimeMillis() - start;
-            System.out.println("EishayFuryParse-fastjson2_jsonb millis : " + millis);
+            System.out.println("EishayFuryParse-jsonb millis : " + millis);
             // zulu8.62.0.19 : 2513
             // zulu11.52.13 : 2352
             // zulu17.38.21 : 2336
@@ -41,7 +41,7 @@ public class EishayFuryParseTest {
     }
 
     public static void main(String[] args) throws Exception {
-        fastjson2JSONB();
+        jsonb();
 //        fury();
     }
 }
--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryWriteTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryWriteTest.java
@@ -6,17 +6,18 @@ public class EishayFuryWriteTest {
     static final EishayFuryWrite benchmark = new EishayFuryWrite();
     static final int COUNT = 10_000_000;
 
-    public static void fastjson2JSONB() throws Exception {
+    public static void jsonb() throws Exception {
         for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
             for (int i = 0; i < COUNT; ++i) {
-                benchmark.fastjson2JSONB(BH);
+                benchmark.jsonb(BH);
             }
             long millis = System.currentTimeMillis() - start;
-            System.out.println("EishayFuryWrite-fastjson2_jsonb millis : " + millis);
+            System.out.println("EishayFuryWrite-jsonb millis : " + millis);
             // zulu8.62.0.19 : 2007
             // zulu11.52.13 : 1483
             // zulu17.38.21 : 1496
+            // oracle-jdk-17.0.6.jdk 1523
         }
     }
 
@@ -31,11 +32,12 @@ public class EishayFuryWriteTest {
             // zulu8.62.0.19 : 2208
             // zulu11.52.13 : 1740
             // zulu17.38.21 : 1710
+            // oracle-jdk-17.0.6.jdk 1659
         }
     }
 
     public static void main(String[] args) throws Exception {
-        fastjson2JSONB();
+        jsonb();
 //        fury();
     }
 }
