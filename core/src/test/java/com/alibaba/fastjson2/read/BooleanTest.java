ead58c2619d9d1c43bea0b6a2db365d5d84614c9
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-02-14 21:02:44 +0800

    JSONPath.extract support NonZeroNumberCastToBooleanAsTrue

--- core/src/test/java/com/alibaba/fastjson2/read/BooleanTest.java
+++ core/src/test/java/com/alibaba/fastjson2/read/BooleanTest.java
@@ -1,11 +1,14 @@
 package com.alibaba.fastjson2.read;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.util.DateUtils;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
+
 import static com.alibaba.fastjson2.JSONReader.Feature.NonZeroNumberCastToBooleanAsTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BooleanTest {
     @Test
@@ -24,6 +27,37 @@ public class BooleanTest {
         assertTrue(JSON.parseObject("{\"value\":1}").toJavaObject(Bean.class, NonZeroNumberCastToBooleanAsTrue).value);
         assertTrue(JSON.parseObject("{\"value\":3}").toJavaObject(Bean.class, NonZeroNumberCastToBooleanAsTrue).value);
         assertTrue(JSON.parseObject("{\"value\":true}").toJavaObject(Bean.class, NonZeroNumberCastToBooleanAsTrue).value);
+    }
+
+    @Test
+    public void testPath() {
+        JSONPath path = JSONPath.of(
+                new String[]{"$.id", "$.value"},
+                new Type[]{int.class, boolean.class},
+                new String[2],
+                new long[]{0, 0},
+                DateUtils.SHANGHAI_ZONE_ID,
+                NonZeroNumberCastToBooleanAsTrue
+        );
+        Object[] values = (Object[]) path.extract("{\"id\":123,\"value\":3}");
+        assertEquals(Boolean.TRUE, values[1]);
+    }
+
+    @Test
+    public void testPath1() {
+        JSONPath path = JSONPath.of(
+                new String[]{"$.id", "$.value"},
+                new Type[]{int.class, boolean.class},
+                new String[2],
+                new long[]{0, 0},
+                DateUtils.SHANGHAI_ZONE_ID
+        );
+        assertEquals(Boolean.TRUE, ((Object[]) path.extract("{\"id\":123,\"value\":\"true\"}"))[1]);
+        assertEquals(Boolean.TRUE, ((Object[]) path.extract("{\"id\":123,\"value\":\"TRUE\"}"))[1]);
+        assertEquals(Boolean.TRUE, ((Object[]) path.extract("{\"id\":123,\"value\":\"1\"}"))[1]);
+        assertEquals(Boolean.FALSE, ((Object[]) path.extract("{\"id\":123,\"value\":\"false\"}"))[1]);
+        assertEquals(Boolean.FALSE, ((Object[]) path.extract("{\"id\":123,\"value\":\"FALSE\"}"))[1]);
+        assertEquals(Boolean.FALSE, ((Object[]) path.extract("{\"id\":123,\"value\":\"0\"}"))[1]);
     }
 
     public static class Bean {
