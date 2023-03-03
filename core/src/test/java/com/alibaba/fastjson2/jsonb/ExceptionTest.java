0291b894ef995d7758fc6b18231db2002a8dfd25
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-03-03 10:34:11 +0800

    add testcase

--- core/src/test/java/com/alibaba/fastjson2/jsonb/ExceptionTest.java
+++ core/src/test/java/com/alibaba/fastjson2/jsonb/ExceptionTest.java
@@ -24,11 +24,58 @@ public class ExceptionTest {
         };
         byte[] bytes = JSONB.toBytes(objects, features);
 
-        System.out.println(JSONB.toJSONString(bytes));
+//        System.out.println(JSONB.toJSONString(bytes));
 
         Object[] parsed = JSONB.parseObject(bytes, objects.getClass(), JSONReader.Feature.SupportAutoType,
                 JSONReader.Feature.UseNativeObject,
                 JSONReader.Feature.FieldBased);
         assertEquals(objects.length, parsed.length);
+    }
+
+    @Test
+    public void test_exception1() {
+        Exception exception = null;
+        try {
+            error();
+        } catch (Exception e) {
+            exception = e;
+        }
+        Object[] objects = new Object[]{exception};
+        JSONWriter.Feature[] features = {
+                JSONWriter.Feature.WriteClassName,
+                JSONWriter.Feature.FieldBased,
+                JSONWriter.Feature.ReferenceDetection,
+                JSONWriter.Feature.WriteNulls,
+                JSONWriter.Feature.NotWriteDefaultValue,
+                JSONWriter.Feature.NotWriteHashMapArrayListClassName,
+                JSONWriter.Feature.WriteNameAsSymbol
+        };
+        byte[] bytes = JSONB.toBytes(objects, features);
+
+//        System.out.println(JSONB.toJSONString(bytes));
+
+        Object[] parsed = JSONB.parseObject(bytes, objects.getClass(), JSONReader.Feature.SupportAutoType,
+                JSONReader.Feature.UseNativeObject,
+                JSONReader.Feature.FieldBased);
+        assertEquals(objects.length, parsed.length);
+    }
+
+    public void error() throws Exception {
+        try {
+            errorNull();
+        } catch (Exception e) {
+            throw new MyException(e);
+        }
+    }
+
+    public void errorNull() {
+        throw new NullPointerException();
+    }
+
+    public static class MyException
+            extends Exception {
+        public MyException(Throwable cause) {
+            super(cause);
+        }
     }
 }
