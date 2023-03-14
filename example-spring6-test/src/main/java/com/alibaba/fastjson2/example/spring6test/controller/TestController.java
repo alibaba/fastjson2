36e178ea88482d7301c22b8ffc480f45a4bfd7f6
lx11915 <lx11915@ly.com>
2023-03-14 14:59:34 +0800

    commit with Fastjson2Decoder & Fastjson2Encoder on webclient.

--- example-spring6-test/src/main/java/com/alibaba/fastjson2/example/spring6test/controller/TestController.java
+++ example-spring6-test/src/main/java/com/alibaba/fastjson2/example/spring6test/controller/TestController.java
@@ -2,12 +2,16 @@ package com.alibaba.fastjson2.example.spring6test.controller;
 
 import cn.hutool.core.io.resource.ClassPathResource;
 import com.alibaba.fastjson2.example.spring6test.entity.User;
+import com.alibaba.fastjson2.example.spring6test.entity.WebFluxMockBean;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author jiangqiang
@@ -30,5 +34,35 @@ public class TestController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("/webFluxGet")
+    public WebFluxMockBean webFluxGet() {
+        return WebFluxMockBean.builder()
+                .name("name")
+                .upperName("NAME")
+                .innerBean(WebFluxMockBean.InnerBean.builder()
+                        .id("id1")
+                        .upperId("ID1")
+                        .build())
+                .upperInnerBean(WebFluxMockBean.InnerBean.builder()
+                        .id("id2")
+                        .upperId("ID2")
+                        .build())
+                .build();
+    }
+
+    @PostMapping("/webFluxPost")
+    public WebFluxMockBean webFluxPost(@RequestBody WebFluxMockBean request) {
+        if (Objects.isNull(request)) {
+            return null;
+        }
+        request.setName("res=" + request.getName());
+        request.setUpperName("RES=" + request.getUpperName());
+        request.getInnerBean().setId("res=" + request.getInnerBean().getId());
+        request.getInnerBean().setUpperId("RES=" + request.getInnerBean().getUpperId());
+        request.getUpperInnerBean().setId("res=" + request.getUpperInnerBean().getId());
+        request.getUpperInnerBean().setUpperId("RES=" + request.getUpperInnerBean().getUpperId());
+        return request;
     }
 }
---
+++ extension-spring5/src/main/java/com/alibaba/fastjson2/support/spring/http/codec/Fastjson2Encoder.java
@@ -0,0 +1,151 @@
+package com.alibaba.fastjson2.support.spring.http.codec;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.filter.*;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.writer.ObjectWriter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.codec.Hints;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.log.LogFormatUtils;
+import org.springframework.http.codec.json.AbstractJackson2Decoder;
+import org.springframework.http.codec.json.AbstractJackson2Encoder;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.MimeType;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Fastjson for Spring MVC Converter.
+ *
+ * @author Xi.Liu
+ * @see AbstractJackson2Decoder
+ */
+public class Fastjson2Encoder
+        extends AbstractJackson2Encoder {
+    private final FastJsonConfig config;
+
+    public Fastjson2Encoder(ObjectMapper mapper, MimeType... mimeTypes) {
+        super(mapper, mimeTypes);
+        this.config = new FastJsonConfig();
+    }
+
+    public Fastjson2Encoder(ObjectMapper mapper, FastJsonConfig config, MimeType... mimeTypes) {
+        super(mapper, mimeTypes);
+        this.config = config;
+    }
+
+    @NonNull
+    @Override
+    public DataBuffer encodeValue(@Nullable Object value,
+                                  @NonNull DataBufferFactory bufferFactory,
+                                  @NonNull ResolvableType valueType,
+                                  MimeType mimeType,
+                                  Map<String, Object> hints) {
+        try {
+            logValue(hints, value);
+            if (value instanceof String && JSON.isValidObject((String) value)) {
+                byte[] strBytes = ((String) value).getBytes(this.config.getCharset());
+                DataBuffer buffer = bufferFactory.allocateBuffer(strBytes.length);
+                buffer.write(strBytes, 0, strBytes.length);
+                Hints.touchDataBuffer(buffer, hints, logger);
+                return buffer;
+            }
+            try (JSONWriter writer = JSONWriter.ofUTF8(this.config.getWriterFeatures())) {
+                if (value == null) {
+                    writer.writeNull();
+                } else {
+                    writer.setRootObject(value);
+                    configFilter(writer.context, this.config.getWriterFilters());
+                    Class<?> valueClass = value.getClass();
+                    ObjectWriter<?> objectWriter = writer.getObjectWriter(valueClass, valueClass);
+                    objectWriter.write(writer, value, null, null, 0);
+                }
+                DataBuffer buffer = bufferFactory.allocateBuffer(writer.size());
+                writer.flushTo(buffer.asOutputStream());
+                Hints.touchDataBuffer(buffer, hints, logger);
+                return buffer;
+            } catch (IOException e) {
+                throw new JSONException("JSON#writeTo cannot serialize '" + value + "' to 'OutputStream'", e);
+            }
+        } catch (JSONException ex) {
+            throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
+        }
+    }
+
+    private void configFilter(JSONWriter.Context context, Filter... filters) {
+        if (filters == null || filters.length == 0) {
+            return;
+        }
+        for (Filter filter : filters) {
+            if (filter instanceof NameFilter) {
+                NameFilter f = (NameFilter) filter;
+                if (context.getNameFilter() == null) {
+                    context.setNameFilter(f);
+                } else {
+                    context.setNameFilter(NameFilter.compose(context.getNameFilter(), f));
+                }
+            }
+
+            if (filter instanceof ValueFilter) {
+                ValueFilter f = (ValueFilter) filter;
+                if (context.getValueFilter() == null) {
+                    context.setValueFilter(f);
+                } else {
+                    context.setValueFilter(ValueFilter.compose(context.getValueFilter(), f));
+                }
+            }
+
+            if (filter instanceof PropertyFilter) {
+                PropertyFilter f = (PropertyFilter) filter;
+                context.setPropertyFilter(f);
+            }
+
+            if (filter instanceof PropertyPreFilter) {
+                PropertyPreFilter f = (PropertyPreFilter) filter;
+                context.setPropertyPreFilter(f);
+            }
+
+            if (filter instanceof BeforeFilter) {
+                BeforeFilter f = (BeforeFilter) filter;
+                context.setBeforeFilter(f);
+            }
+
+            if (filter instanceof AfterFilter) {
+                AfterFilter f = (AfterFilter) filter;
+                context.setAfterFilter(f);
+            }
+
+            if (filter instanceof LabelFilter) {
+                LabelFilter f = (LabelFilter) filter;
+                context.setLabelFilter(f);
+            }
+
+            if (filter instanceof ContextValueFilter) {
+                ContextValueFilter f = (ContextValueFilter) filter;
+                context.setContextValueFilter(f);
+            }
+
+            if (filter instanceof ContextNameFilter) {
+                ContextNameFilter f = (ContextNameFilter) filter;
+                context.setContextNameFilter(f);
+            }
+        }
+    }
+
+    private void logValue(@Nullable Map<String, Object> hints, Object value) {
+        if (!Hints.isLoggingSuppressed(hints)) {
+            LogFormatUtils.traceDebug(logger, traceOn -> {
+                String formatted = LogFormatUtils.formatValue(value, !traceOn);
+                return Hints.getLogPrefix(hints) + "Encoding [" + formatted + "]";
+            });
+        }
+    }
+}
