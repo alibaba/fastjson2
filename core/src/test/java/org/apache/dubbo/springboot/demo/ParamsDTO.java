14e74ab827cb749866789fbe045be1b6658b6bbe
PanLongfei <244014926@qq.com>
2023-03-16 15:18:30 +0800

    #1244 fix EnumSet deserialization fail

--- core/src/test/java/org/apache/dubbo/springboot/demo/ParamsDTO.java
+++ core/src/test/java/org/apache/dubbo/springboot/demo/ParamsDTO.java
@@ -3,10 +3,15 @@ package org.apache.dubbo.springboot.demo;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Data
 public class ParamsDTO
         implements Serializable {
     private List<ParamsItemDTO> paramsItems;
+
+    private EnumSet<TimeUnit> paramsItemSet;
 }
