5369f3921a486c0f3b5aa6d8d9078d89360ce26a
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-03-18 02:03:05 +0800

    add testcase & remove unused code

---
+++ core/src/test/java/com/alibaba/fastjson2/codec/SeeAlsoTest5.java
@@ -0,0 +1,37 @@
+package com.alibaba.fastjson2.codec;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.annotation.JSONType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeeAlsoTest5 {
+    @Test
+    public void test() {
+        Bean bean = (Bean) JSON.parseObject(
+                "{\"@type\":\"com.alibaba.fastjson2.codec.SeeAlsoTest5$Bean\",\"id\":123}",
+                IBean.class,
+                JSONReader.Feature.SupportAutoType
+        );
+        assertEquals(123, bean.id);
+    }
+
+    @JSONType(seeAlso = {})
+    public interface IBean {
+    }
+
+    public static class Bean
+            implements IBean {
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+}
