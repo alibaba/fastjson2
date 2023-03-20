4c8a93a38bdd37b324295ec1567fba364c115f11
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-03-21 04:04:26 +0800

    MoneySupport use lambda replace reflect

--- core/src/main/java/com/alibaba/fastjson2/writer/ObjectWriterImplBigDecimal.java
+++ core/src/main/java/com/alibaba/fastjson2/writer/ObjectWriterImplBigDecimal.java
@@ -5,25 +5,40 @@ import com.alibaba.fastjson2.JSONWriter;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.function.Function;
 
 final class ObjectWriterImplBigDecimal
         extends ObjectWriterBaseModule.PrimitiveImpl {
-    static final ObjectWriterImplBigDecimal INSTANCE = new ObjectWriterImplBigDecimal(null);
+    static final ObjectWriterImplBigDecimal INSTANCE = new ObjectWriterImplBigDecimal(null, null);
 
     private final DecimalFormat format;
 
-    public ObjectWriterImplBigDecimal(DecimalFormat format) {
+    final Function<Object, BigDecimal> function;
+
+    public ObjectWriterImplBigDecimal(DecimalFormat format, Function<Object, BigDecimal> function) {
         this.format = format;
+        this.function = function;
     }
 
     @Override
     public void writeJSONB(JSONWriter jsonWriter, Object object, Object fieldName, Type fieldType, long features) {
-        jsonWriter.writeDecimal((BigDecimal) object);
+        BigDecimal decimal;
+        if (function != null && object != null) {
+            decimal = function.apply(object);
+        } else {
+            decimal = (BigDecimal) object;
+        }
+        jsonWriter.writeDecimal(decimal);
     }
 
     @Override
     public void write(JSONWriter jsonWriter, Object object, Object fieldName, Type fieldType, long features) {
-        BigDecimal decimal = (BigDecimal) object;
+        BigDecimal decimal;
+        if (function != null && object != null) {
+            decimal = function.apply(object);
+        } else {
+            decimal = (BigDecimal) object;
+        }
 
         if (format != null) {
             String str = format.format(object);
