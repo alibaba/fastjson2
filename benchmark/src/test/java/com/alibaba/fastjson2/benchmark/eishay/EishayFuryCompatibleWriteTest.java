a1e59a132c183644c5a78fc3fdd07c8548d87d55
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-03-26 21:07:53 +0800

    update benchmark data

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryCompatibleWriteTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayFuryCompatibleWriteTest.java
@@ -7,6 +7,7 @@ public class EishayFuryCompatibleWriteTest {
     static final int COUNT = 10_000_000;
 
     public static void jsonb() throws Exception {
+        System.out.println("jsonb size " + benchmark.jsonbSize()); // 409
         for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
             for (int i = 0; i < COUNT; ++i) {
@@ -14,14 +15,14 @@ public class EishayFuryCompatibleWriteTest {
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayFuryCompatibleWrite-jsonb millis : " + millis);
-            // zulu8.62.0.19 : 3850
-            // zulu11.52.13 : 3229
-            // zulu17.38.21 : 3295
-            // oracle-jdk-17.0.6 3265
+            // zulu8.68.0.21_AppleM1Max : 3839
+            // zulu11.62.17_AppleM1Max : 3327
+            // zulu17.40.19_AppleM1Max : 3287
         }
     }
 
     public static void fury() throws Exception {
+        System.out.println("fury size " + benchmark.furySize()); // 661
         for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
             for (int i = 0; i < COUNT; ++i) {
@@ -29,15 +30,14 @@ public class EishayFuryCompatibleWriteTest {
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayFuryCompatibleWrite-fury millis : " + millis);
-            // zulu8.62.0.19 : 2523
-            // zulu11.52.13 : 2270
-            // zulu17.38.21 : 3049
-            // oracle-jdk-17.0.6
+            // zulu8.68.0.21_AppleM1Max : 2644
+            // zulu11.62.17_AppleM1Max : 2270
+            // zulu17.40.19_AppleM1Max : 2296
         }
     }
 
     public static void main(String[] args) throws Exception {
-//        jsonb();
-        fury();
+        jsonb();
+//        fury();
     }
 }
