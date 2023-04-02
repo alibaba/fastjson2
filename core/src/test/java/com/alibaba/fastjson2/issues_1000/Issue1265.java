3b7fdd9a640e9061638b4cb300e5037fd2d8a59f
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-04-02 22:11:27 +0800

    add testcase for issue #1265

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1265.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1265.java
@@ -1,8 +1,10 @@
 package com.alibaba.fastjson2.issues_1000;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONFactory;
 import com.alibaba.fastjson2.PropertyNamingStrategy;
 import com.alibaba.fastjson2.filter.NameFilter;
+import com.alibaba.fastjson2.writer.ObjectWriter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,6 +27,33 @@ public class Issue1265 {
     }
 
     public static class Bean {
+        public int userId;
+        public int groupId;
+
+        public String userName;
+        public String groupName;
+    }
+
+    @Test
+    public void test1() {
+        Bean1 bean = new Bean1();
+        bean.userName = "a";
+        bean.groupName = "b";
+
+        NameFilter nameFilter = (Object object, String name, Object value) -> {
+            if (name.endsWith("Name")) {
+                return PropertyNamingStrategy.SnakeCase.fieldName(name);
+            }
+            return name;
+        };
+        ObjectWriter objectWriter = JSONFactory.getDefaultObjectWriterProvider().getObjectWriter(Bean1.class);
+        objectWriter.setFilter(nameFilter);
+
+        String str = JSON.toJSONString(bean);
+        assertEquals("{\"groupId\":0,\"group_name\":\"b\",\"userId\":0,\"user_name\":\"a\"}", str);
+    }
+
+    public static class Bean1 {
         public int userId;
         public int groupId;
 
