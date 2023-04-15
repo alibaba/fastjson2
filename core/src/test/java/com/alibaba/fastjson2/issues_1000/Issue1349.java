b4fc877f7963756777ed01284e0d502b7b72148d
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-04-16 07:57:28 +0800

    add testcase for #1349

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1349.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1349.java
@@ -19,4 +19,48 @@ public class Issue1349 {
         @JSONField(locale = "zh", format = "yyyy-mm-dd")
         public Date date;
     }
+
+    @Test
+    public void test1() {
+        Bean1 bean = JSON.parseObject("{\"date\":\"1863792000000\"}", Bean1.class);
+        assertEquals(1863792000000L, bean.date.getTime());
+    }
+
+    public static class Bean1 {
+        private final Date date;
+
+        public Bean1(@JSONField(locale = "zh", format = "yyyy-mm-dd")Date date) {
+            this.date = date;
+        }
+    }
+
+    @Test
+    public void test2() {
+        Bean2 bean = JSON.parseObject("{\"date\":\"1863792000000\"}", Bean2.class);
+        assertEquals(1863792000000L, bean.date.getTime());
+    }
+
+    private static class Bean2 {
+        @JSONField(locale = "zh", format = "yyyy-mm-dd")
+        public Date date;
+    }
+
+    @Test
+    public void test3() {
+        Bean2 bean = JSON.parseObject("{\"date\":\"1863792000000\"}", Bean2.class);
+        assertEquals(1863792000000L, bean.date.getTime());
+    }
+
+    public static class Bean3 {
+        @JSONField(locale = "zh", format = "yyyy-mm-dd")
+        private Date date;
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+    }
 }
