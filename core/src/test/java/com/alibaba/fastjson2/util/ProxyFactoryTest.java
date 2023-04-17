f4878d09ebfb15ff699d5fef21fcb515b9cac3db
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-04-17 19:15:21 +0800

    refactor, move newProxyInstance to TypeUtils

--- core/src/test/java/com/alibaba/fastjson2/util/ProxyFactoryTest.java
+++ core/src/test/java/com/alibaba/fastjson2/util/ProxyFactoryTest.java
@@ -9,7 +9,7 @@ public class ProxyFactoryTest {
     @Test
     public void test() {
         JSONObject object = new JSONObject();
-        Bean bean = ProxyFactory.newProxyInstance(Bean.class, object);
+        Bean bean = TypeUtils.newProxyInstance(Bean.class, object);
         bean.setId(123);
         assertEquals(123, bean.getId());
     }
@@ -23,7 +23,7 @@ public class ProxyFactoryTest {
     @Test
     public void test1() {
         JSONObject object = new JSONObject();
-        Bean1 bean = ProxyFactory.newProxyInstance(Bean1.class, object);
+        Bean1 bean = TypeUtils.newProxyInstance(Bean1.class, object);
         bean.setId((byte) 123);
         assertEquals(123, bean.getId());
     }
@@ -37,7 +37,7 @@ public class ProxyFactoryTest {
     @Test
     public void test2() {
         JSONObject object = new JSONObject();
-        Bean2 bean = ProxyFactory.newProxyInstance(Bean2.class, object);
+        Bean2 bean = TypeUtils.newProxyInstance(Bean2.class, object);
         bean.setId((short) 123);
         assertEquals(123, bean.getId());
     }
@@ -51,7 +51,7 @@ public class ProxyFactoryTest {
     @Test
     public void test3() {
         JSONObject object = new JSONObject();
-        Bean3 bean = ProxyFactory.newProxyInstance(Bean3.class, object);
+        Bean3 bean = TypeUtils.newProxyInstance(Bean3.class, object);
         bean.setId(123);
         assertEquals(123, bean.getId());
     }
@@ -65,7 +65,7 @@ public class ProxyFactoryTest {
     @Test
     public void test4() {
         JSONObject object = new JSONObject();
-        Bean4 bean = ProxyFactory.newProxyInstance(Bean4.class, object);
+        Bean4 bean = TypeUtils.newProxyInstance(Bean4.class, object);
         bean.setId(123);
         assertEquals(123, bean.getId());
     }
@@ -79,7 +79,7 @@ public class ProxyFactoryTest {
     @Test
     public void test5() {
         JSONObject object = new JSONObject();
-        Bean5 bean = ProxyFactory.newProxyInstance(Bean5.class, object);
+        Bean5 bean = TypeUtils.newProxyInstance(Bean5.class, object);
         bean.setId(123);
         assertEquals(123, bean.getId());
     }
@@ -93,7 +93,7 @@ public class ProxyFactoryTest {
     @Test
     public void test6() {
         JSONObject object = new JSONObject();
-        Bean6 bean = ProxyFactory.newProxyInstance(Bean6.class, object);
+        Bean6 bean = TypeUtils.newProxyInstance(Bean6.class, object);
         bean.setId(true);
         assertEquals(true, bean.getId());
     }
@@ -107,7 +107,7 @@ public class ProxyFactoryTest {
     @Test
     public void test7() {
         JSONObject object = new JSONObject();
-        Bean7 bean = ProxyFactory.newProxyInstance(Bean7.class, object);
+        Bean7 bean = TypeUtils.newProxyInstance(Bean7.class, object);
         bean.setId('a');
         assertEquals('a', bean.getId());
     }
@@ -121,7 +121,7 @@ public class ProxyFactoryTest {
     @Test
     public void test8() {
         JSONObject object = new JSONObject();
-        Bean8 bean = ProxyFactory.newProxyInstance(Bean8.class, object);
+        Bean8 bean = TypeUtils.newProxyInstance(Bean8.class, object);
         bean.setId("123");
         assertEquals("123", bean.getId());
     }
@@ -135,7 +135,7 @@ public class ProxyFactoryTest {
     @Test
     public void test9() {
         JSONObject object = new JSONObject();
-        Bean9 bean = ProxyFactory.newProxyInstance(Bean9.class, object);
+        Bean9 bean = TypeUtils.newProxyInstance(Bean9.class, object);
         bean.setId("123");
         assertEquals("123", bean.getId());
     }
