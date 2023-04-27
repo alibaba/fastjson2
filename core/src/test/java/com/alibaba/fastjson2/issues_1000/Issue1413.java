c65f86a13c5ca518454237e7b791394ed0b623a9
陆之岇 <kraty@krait.cn>
2023-04-27 22:06:39 +0800

    Fixed index out of bounds for #1413 (#1419)
    * fix: index out of bounds for #1413
    
    ---------
    
    Signed-off-by: Kraity <kraty@krait.cn>

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1413.java
@@ -0,0 +1,20 @@
+package com.alibaba.fastjson2.issues_1000;
+
+import com.alibaba.fastjson2.JSON;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Issue1413 {
+    @Test
+    public void test() {
+        assertEquals("foo", JSON.parse("\"foo\"  "));
+        assertEquals("foo", JSON.parse("  \"foo\""));
+        assertEquals("foo", JSON.parse("  \"foo\","));
+        assertEquals("foo", JSON.parse("  \"foo\"  "));
+        assertEquals("foo", JSON.parse("  \"foo\"  \n"));
+        assertEquals("foo", JSON.parse("  \"foo\"  \n,"));
+        assertEquals("foo", JSON.parse("  \"foo\"  \r\n,  \r\n"));
+        assertEquals("foo", JSON.parse("\r\n  \"foo\"  \r\n,  \r\n"));
+    }
+}
