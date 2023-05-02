7abb765efef94d6f4a6062b05a57b857d0a97a7a
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-05-02 21:40:25 +0800

    update benchmark data

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryArrayMappingTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseBinaryArrayMappingTest.java
@@ -3,19 +3,20 @@ package com.alibaba.fastjson2.benchmark.eishay;
 import static com.alibaba.fastjson2.benchmark.JMH.BH;
 
 public class EishayParseBinaryArrayMappingTest {
+    static final int LOOP = 10_000_000;
     static final EishayParseBinaryArrayMapping benchmark = new EishayParseBinaryArrayMapping();
 
     public static void kryo() throws Exception {
-        System.out.println("kryo size " + EishayParseBinaryArrayMapping.kryoBytes.length); // 214
+        System.out.println("kryo size " + EishayParseBinaryArrayMapping.kryoBytes.length); // 213
         System.out.println();
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.kryo(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseBinaryArrayMapping-kryo millis : " + millis);
-            // zulu8.58.0.13 : 457
+            // zulu8.58.0.13 : 4483
             // zulu11.52.13 :
             // zulu17.32.13 :
         }
@@ -25,16 +26,16 @@ public class EishayParseBinaryArrayMappingTest {
         System.out.println("EishayParseBinaryArrayMapping-jsonb size " + EishayParseBinaryArrayMapping.fastjson2JSONBBytes.length); // 223
         System.out.println();
 
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.jsonb(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseBinaryArrayMapping-fastjson2_jsonb millis : " + millis);
-            // zulu8.58.0.13 : 193 188
-            // zulu11.52.13 : 160
-            // zulu17.32.13 : 166 148 139
+            // zulu8.68.0.21 : 1873
+            // zulu11.52.13 :
+            // zulu17.32.13 :
         }
     }
 
@@ -42,14 +43,14 @@ public class EishayParseBinaryArrayMappingTest {
         System.out.println("protobuf size " + EishayParseBinaryArrayMapping.protobufBytes.length); // 235
         System.out.println();
 
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.protobuf(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseBinaryArrayMapping-protobuf millis : " + millis);
-            // zulu8.58.0.13 : 531
+            // zulu8.68.0.21 : 5211
             // zulu11.52.13 :
             // zulu17.32.13 :
         }
@@ -59,23 +60,23 @@ public class EishayParseBinaryArrayMappingTest {
         System.out.println("fastjson2UTF8Bytes size " + EishayParseBinaryArrayMapping.fastjson2UTF8Bytes.length); // 235
         System.out.println();
 
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.fastjson2UTF8Bytes(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseBinaryArrayMapping-fastjson2UTF8Bytes millis : " + millis);
-            // zulu8.58.0.13 : 531
+            // zulu8.68.0.21 : 4655
             // zulu11.52.13 :
             // zulu17.32.13 :
         }
     }
 
     public static void main(String[] args) throws Exception {
-//        jsonb();
+        jsonb();
 //        kryo();
 //        protobuf();
-        fastjson2UTF8Bytes();
+//        fastjson2UTF8Bytes();
     }
 }
--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseStringTest.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/eishay/EishayParseStringTest.java
@@ -1,163 +1,101 @@
 package com.alibaba.fastjson2.benchmark.eishay;
 
-import org.openjdk.jmh.infra.Blackhole;
+import static com.alibaba.fastjson2.benchmark.JMH.BH;
 
 public class EishayParseStringTest {
-    static final Blackhole BH = new Blackhole("Today's password is swordfish. I understand instantiating Blackholes directly is dangerous.");
+    static final int LOOP = 10_000_000;
     static final EishayParseString benchmark = new EishayParseString();
 
     public static void fastjson2() {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.fastjson2(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("fastjson2 millis : " + millis);
-            // zulu8.70.0.23 : 550
-            // zulu11.64.19 : 507
-            // zulu17.42.19 : 545
-
-            // zulu8.62.0.19 : 666 578 554 571 560 554 550 542 528
-            // zulu11.52.13 : 821 546 521 494 492 502 485 492 474 486 476
-            // zulu17.32.13 : 601 552 532 516 511 530 517 511 501
-            // zulu18.28.13 : 598 555 534 527 512 509 498
-            // zulu19.0.75 : 515
-            // corretto-8 : 558 550
-            // corretto-11 : 516 488
-            // corretto-17 : 512 517 498
-            // corretto-18 : 510 508
-            // oracle-jdk-17.0.4 : 562 536
-            // oracle-jdk-18.0.2 : 552 530
-            // ibm-aarch64_mac_11.0.15_10 : 1240
-            // ibm-aarch64_mac_17.0.3_7 : 1311
-            // graalvm-ce-java17-22.2.0 : 711
-            // graalvm-ee-java17-22.2.0 : 589
-
-            // reflect-zulu8.62.0.19 661 646 553
-            // reflect-zulu11.52.13 : 726 644
-            // reflect-zulu17.32.13 : 738 645
+            // zulu8.70.0.23 : 5569
+            // zulu11.64.19 : 5022
+            // zulu17.42.19 : 5377
         }
     }
 
     public static void fastjson2Mixin() {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.fastjson2Mixin(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("fastjson2Mixin millis : " + millis);
-            // zulu8.62.0.19 : 639
-            // zulu11.52.13 : 575
+            // zulu8.70.0.23 : 6432
+            // zulu11.52.13 :
             // zulu17.32.13 :
-            // zulu18.28.13 :
-            // zulu19.0.75 :
-            // corretto-8 :
-            // corretto-11 :
-            // corretto-17 :
-            // corretto-18 :
-            // oracle-jdk-17.0.4 :
-            // oracle-jdk-18.0.2 :
-            // graalvm-ce-java17-22.2.0 :
-            // graalvm-ee-java17-22.2.0 :
         }
     }
 
     public static void fastjson1() throws Exception {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.fastjson1(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("fastjson1 millis : " + millis);
-            // zulu8.62.0.19 : 664
-            // zulu11.52.13 : 677
-            // zulu17.32.13 : 522
-            // zulu18.28.13 :
-            // zulu19.0.47 :
-            // corretto-8 :
-            // corretto-11 :
-            // corretto-17 :
-            // corretto-18 :
-            // oracle-jdk-17.0.4 :
-            // oracle-jdk-18.0.2 :
+            // zulu8.70.0.23 : 6436
+            // zulu11.52.13 :
+            // zulu17.32.13 :
         }
     }
 
     public static void jackson() throws Exception {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.jackson(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("jackson millis : " + millis);
-            // zulu8.62.0.19 : 1266
-            // zulu11.52.13 : 1314
-            // zulu17.32.13 : 1246
-            // zulu18.28.13 : 1279
-            // zulu19.0.47 :
-            // corretto-8 :
-            // corretto-11 :
-            // corretto-17 :
-            // corretto-18 :
-            // oracle-jdk-17.0.4 :
-            // oracle-jdk-18.0.2 :
+            // zulu8.70.0.23 : 13191
+            // zulu11.52.13 :
+            // zulu17.32.13 :
         }
     }
 
     public static void wastjson() throws Exception {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.wastjson(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseString-wastjson millis : " + millis);
-            // zulu8.62.0.19 : 466
-            // zulu11.52.13 : 466
-            // zulu17.32.13 : 468
-            // zulu18.28.13 :
-            // zulu19.0.47 :
-            // corretto-8 :
-            // corretto-11 :
-            // corretto-17 :
-            // corretto-18 :
-            // oracle-jdk-17.0.4 :
-            // oracle-jdk-18.0.2 :
+            // zulu8.70.0.23 : 5458
+            // zulu11.64.19 : 4574
+            // zulu17.42.19 : 4515
         }
     }
 
     public static void gson() throws Exception {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 5; j++) {
             long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000 * 1000; ++i) {
+            for (int i = 0; i < LOOP; ++i) {
                 benchmark.gson(BH);
             }
             long millis = System.currentTimeMillis() - start;
             System.out.println("EishayParseString-gson millis : " + millis);
-            // zulu8.62.0.19 : 1449
-            // zulu11.52.13 :
+            // zulu8.70.0.23 : 15017
+            // zulu11.64.19 : 13143
             // zulu17.32.13 :
-            // zulu18.28.13 :
-            // zulu19.0.47 :
-            // corretto-8 :
-            // corretto-11 :
-            // corretto-17 :
-            // corretto-18 :
-            // oracle-jdk-17.0.4 :
-            // oracle-jdk-18.0.2 :
         }
     }
 
     public static void main(String[] args) throws Exception {
-        fastjson2();
+//        fastjson2();
 //        fastjson2Mixin();
-//        jackson_test();
-//        fastjson1_test();
-//        gson_test();
-//        wastjson();
+//        jackson();
+//        fastjson1();
+//        gson();
+        wastjson();
     }
 }
