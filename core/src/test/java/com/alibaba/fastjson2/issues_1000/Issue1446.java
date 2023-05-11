4265433df0a7a3f1aa01205c8daa3853a4b4cb4d
kx <2749522335@qq.com>
2023-05-11 19:06:18 +0800

    fix JSONArray and checkstyle

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1446.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1446.java
@@ -1,7 +1,9 @@
 package com.alibaba.fastjson2.issues_1000;
 
+import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import org.junit.jupiter.api.Test;
+
 import java.time.*;
 import java.time.chrono.HijrahDate;
 import java.time.chrono.JapaneseDate;
@@ -9,39 +11,36 @@ import java.time.chrono.MinguoDate;
 import java.time.chrono.ThaiBuddhistDate;
 import java.time.temporal.TemporalAccessor;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class Issue1446 {
-
-
     @Test
     public void test() {
-
         testTemporal(LocalDate.now(), LocalDateTime.now(),
                         LocalTime.now(), ZonedDateTime.now(),
-                        OffsetDateTime.now(),Instant.now(),
+                        OffsetDateTime.now(), Instant.now(),
                         HijrahDate.now(), JapaneseDate.now(),
                         OffsetTime.now(), MinguoDate.now(),
-                        ThaiBuddhistDate.now(),YearMonth.now(),
-                        MonthDay.now(),DayOfWeek.of(3));
-
-
+                        ThaiBuddhistDate.now(), YearMonth.now(),
+                        MonthDay.now(), DayOfWeek.of(3));
     }
-
-
-    void testTemporal(TemporalAccessor... targets){
-
+    void testTemporal(TemporalAccessor... targets) {
         com.alibaba.fastjson.JSONObject j1 = new com.alibaba.fastjson.JSONObject();
         JSONObject j2 = new JSONObject();
 
+        com.alibaba.fastjson.JSONArray array1 = new com.alibaba.fastjson.JSONArray();
+        JSONArray array2 = new JSONArray();
+
+        int i = 0;
         for (TemporalAccessor target : targets) {
-
-            j1.put("data",target);
-            j2.put("data",target);
-
+            j1.put("data", target);
+            j2.put("data", target);
             assertEquals(j1.getString("data"), j2.getString("data"));
+
+            array1.add(target);
+            array2.add(target);
+            assertEquals(array1.getString(i), array2.getString(i));
+
+            i++;
         }
     }
-
 }
