0bfceeee51cc66a6f51ba1fad1377645e212ea9a
Jerry Lee <oldratlee@gmail.com>
2023-05-19 19:32:30 +0800

    chore: simplify `moditect` configuration 📦

---
+++ core/src/main/moditect/module-info.java
@@ -0,0 +1,18 @@
+// https://www.oracle.com/corporate/features/understanding-java-9-modules.html
+
+open module com.alibaba.fastjson2 {
+    requires transitive jdk.unsupported;
+    requires java.management;
+
+    exports com.alibaba.fastjson2;
+    exports com.alibaba.fastjson2.annotation;
+    exports com.alibaba.fastjson2.codec;
+    exports com.alibaba.fastjson2.filter;
+    exports com.alibaba.fastjson2.function;
+    exports com.alibaba.fastjson2.modules;
+    exports com.alibaba.fastjson2.reader;
+    exports com.alibaba.fastjson2.schema;
+    exports com.alibaba.fastjson2.support.money;
+    exports com.alibaba.fastjson2.writer;
+    exports com.alibaba.fastjson2.util;
+}
