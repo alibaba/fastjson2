490475d5c0715e1f45dbca5dbf4bef2372c22af6
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-05-20 18:50:35 +0800

    add testcase

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/util/IOUtilsTest.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/util/IOUtilsTest.java
@@ -2,7 +2,9 @@ package com.alibaba.fastjson.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IOUtilsTest {
     @Test
@@ -29,5 +31,23 @@ public class IOUtilsTest {
 
         IOUtils.getChars((byte) 12, 2, chars);
         assertEquals("12", new String(chars));
+    }
+
+    @Test
+    public void isValidJsonpQueryParam() {
+        assertTrue(IOUtils.isValidJsonpQueryParam("abc"));
+        assertTrue(IOUtils.isValidJsonpQueryParam("abc.ff"));
+        assertFalse(IOUtils.isValidJsonpQueryParam("*"));
+        assertFalse(IOUtils.isValidJsonpQueryParam(""));
+        assertFalse(IOUtils.isValidJsonpQueryParam(null));
+    }
+
+    @Test
+    public void decodeUTF8() {
+        String str = "中国";
+        byte[] utf8 = str.getBytes(StandardCharsets.UTF_8);
+        char[] chars = new char[utf8.length];
+        int size = IOUtils.decodeUTF8(utf8, 0, utf8.length, chars);
+        assertEquals(str, new String(chars, 0, size));
     }
 }
