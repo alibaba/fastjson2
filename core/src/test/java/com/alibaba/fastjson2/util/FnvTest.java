cac2f1f8d6cff879a1205ee2ebb138f50d163602
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-05-22 18:56:32 +0800

    add Fnv final

--- core/src/test/java/com/alibaba/fastjson2/util/FnvTest.java
+++ core/src/test/java/com/alibaba/fastjson2/util/FnvTest.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static com.alibaba.fastjson2.JSONFactory.MIXED_HASH_ALGORITHM;
+import static com.alibaba.fastjson2.util.Fnv.MAGIC_HASH_CODE;
 import static com.alibaba.fastjson2.util.Fnv.MAGIC_PRIME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,14 +23,14 @@ public class FnvTest {
             if (MIXED_HASH_ALGORITHM) {
                 hash0 = (byte) c0;
             } else {
-                long hash = Fnv.MAGIC_HASH_CODE;
+                long hash = MAGIC_HASH_CODE;
                 hash ^= c0;
                 hash *= MAGIC_PRIME;
                 hash0 = hash;
             }
 
             assertEquals(hash0, Fnv.hashCode64(str0));
-            assertEquals(hash0, Fnv.hashCode64(new byte[]{(byte) c0}));
+            assertEquals(hash0, hashCode64(new byte[]{(byte) c0}));
 
             for (char c1 = 1; c1 < 0x7F; ++c1) {
                 chars[1] = c1;
@@ -40,7 +41,7 @@ public class FnvTest {
                 if (MIXED_HASH_ALGORITHM) {
                     hash1 = ((byte) c1 << 8) + c0;
                 } else {
-                    long hash = Fnv.MAGIC_HASH_CODE;
+                    long hash = MAGIC_HASH_CODE;
 
                     hash ^= c0;
                     hash *= MAGIC_PRIME;
@@ -56,7 +57,7 @@ public class FnvTest {
                 );
                 assertEquals(
                         hash1,
-                        Fnv.hashCode64(new byte[]{(byte) c0, (byte) c1})
+                        hashCode64(new byte[]{(byte) c0, (byte) c1})
                 );
             }
         }
@@ -89,5 +90,86 @@ public class FnvTest {
             byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
             System.out.println(((int) c0) + "\t" + Arrays.toString(bytes));
         }
+    }
+
+    public static long hashCode64(byte... name) {
+        if (MIXED_HASH_ALGORITHM && name.length > 0 && name.length <= 8 && name[0] != 0) {
+            long nameValue = 0;
+            switch (name.length) {
+                case 1:
+                    nameValue = name[0];
+                    break;
+                case 2:
+                    nameValue
+                            = ((name[1]) << 8)
+                            + (name[0] & 0xFF);
+                    break;
+                case 3:
+                    nameValue
+                            = ((name[2]) << 16)
+                            + ((name[1] & 0xFF) << 8)
+                            + (name[0] & 0xFF);
+                    break;
+                case 4:
+                    nameValue
+                            = (name[3] << 24)
+                            + ((name[2] & 0xFF) << 16)
+                            + ((name[1] & 0xFF) << 8)
+                            + (name[0] & 0xFF);
+                    break;
+                case 5:
+                    nameValue
+                            = (((long) name[4]) << 32)
+                            + ((name[3] & 0xFFL) << 24)
+                            + ((name[2] & 0xFFL) << 16)
+                            + ((name[1] & 0xFFL) << 8)
+                            + (name[0] & 0xFFL);
+                    break;
+                case 6:
+                    nameValue
+                            = (((long) name[5]) << 40)
+                            + ((name[4] & 0xFFL) << 32)
+                            + ((name[3] & 0xFFL) << 24)
+                            + ((name[2] & 0xFFL) << 16)
+                            + ((name[1] & 0xFFL) << 8)
+                            + (name[0] & 0xFFL);
+                    break;
+                case 7:
+                    nameValue
+                            = (((long) name[6]) << 48)
+                            + ((name[5] & 0xFFL) << 40)
+                            + ((name[4] & 0xFFL) << 32)
+                            + ((name[3] & 0xFFL) << 24)
+                            + ((name[2] & 0xFFL) << 16)
+                            + ((name[1] & 0xFFL) << 8)
+                            + (name[0] & 0xFFL);
+                    break;
+                case 8:
+                    nameValue
+                            = (((long) name[7]) << 56)
+                            + ((name[6] & 0xFFL) << 48)
+                            + ((name[5] & 0xFFL) << 40)
+                            + ((name[4] & 0xFFL) << 32)
+                            + ((name[3] & 0xFFL) << 24)
+                            + ((name[2] & 0xFFL) << 16)
+                            + ((name[1] & 0xFFL) << 8)
+                            + (name[0] & 0xFFL);
+                    break;
+                default:
+                    break;
+            }
+
+            if (nameValue != 0) {
+                return nameValue;
+            }
+        }
+
+        long hashCode = MAGIC_HASH_CODE;
+        for (int i = 0; i < name.length; ++i) {
+            char ch = (char) name[i];
+            hashCode ^= ch;
+            hashCode *= MAGIC_PRIME;
+        }
+        return hashCode;
     }
 }
