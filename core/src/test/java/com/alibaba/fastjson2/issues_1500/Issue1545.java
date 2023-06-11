a1648bd05bf1b16aea76e83c333b9b3442348db7
yanxutao89 <910135896@qq.com>
2023-06-11 18:18:07 +0800

    fix add fail-fast to avoid OOM for ObjectReader when field value is null and field type belongs to collection, for issue #1545

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_1500/Issue1545.java
@@ -0,0 +1,55 @@
+package com.alibaba.fastjson2.issues_1500;
+
+import com.alibaba.fastjson2.JSONException;
+import com.alibaba.fastjson2.JSONObject;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Issue1545 {
+    @Test
+    public void testString() {
+        String s = "{\n" +
+                "    \"fieldPermissions\": [\n" +
+                "        {\n" +
+                "            \"mode\": \"READ\",\n" +
+                "            \"fieldId\": \"updatedTime\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        Exception ex = null;
+        try {
+            JSONObject.parseObject(s, ApproveActionConfig.class);
+        } catch (Exception e) {
+            ex = e;
+        }
+        assertTrue(ex instanceof JSONException);
+        assertEquals("ObjectReaderImplEnum parses error, found null value when field type belongs to collection to avoid OOM", ex.getMessage());
+    }
+
+    @Data
+    public class ApproveActionConfig {
+        private List<FieldPerm.PermMode> fieldPermissions;
+    }
+
+    @Data
+    public class FieldPerm {
+
+        private String fieldId;
+
+        private PermMode mode;
+
+        public enum PermMode {
+
+            READ,
+
+            WRITE,
+
+            NONE
+        }
+    }
+}
