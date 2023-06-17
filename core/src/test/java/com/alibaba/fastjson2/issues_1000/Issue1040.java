33adc89ea592d2433fe79c9d0ff3062e583abf68
Kraity <kraty@krait.cn>
2023-06-17 21:17:59 +0800

    test: repair file separator testcases in Windows
    Signed-off-by: Kraity <kraty@krait.cn>

--- core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1040.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1000/Issue1040.java
@@ -1,8 +1,10 @@
 package com.alibaba.fastjson2.issues_1000;
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -16,6 +18,12 @@ public class Issue1040 {
         Path path = FileSystems.getDefault().getPath("/usr/bin");
         Map<String, Object> ext = new HashMap<>();
         ext.put("test1", path);
-        assertEquals("{\"test1\":\"/usr/bin\"}", JSONObject.toJSONString(ext));
+
+        byte[] fs = JSON.toJSONBytes(File.separator);
+        String sep = new String(fs, 1, fs.length - 2);
+
+        // Linux: {"test1":"/usr/bin"}
+        // Windows: {"test1":"\\usr\\bin"}
+        assertEquals("{\"test1\":\"" + sep + "usr" + sep + "bin\"}", JSONObject.toJSONString(ext));
     }
 }
