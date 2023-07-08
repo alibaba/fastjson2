e981962ebc47296315af65059ea88291242a3258
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-07-09 06:20:08 +0800

    update benchmark result

--- benchmark/src/test/java/com/alibaba/fastjson2/benchmark/ReadResult.java
+++ benchmark/src/test/java/com/alibaba/fastjson2/benchmark/ReadResult.java
@@ -8,16 +8,16 @@ import java.util.Map;
 
 public class ReadResult {
     public static void main(String[] args) throws Exception {
-        File outFile = new File("/Users/wenshao/Work/git/fastjson2/docs/benchmark/benchmark_2.0.34_raw.md");
+        File outFile = new File("/Users/wenshao/Work/git/fastjson2/docs/benchmark/benchmark_2.0.35_raw.md");
 //        File file = new File("/Users/wenshao/Downloads/result_2.0.25.out");
 
         Map<String, String> files = new LinkedHashMap<>();
-        files.put("aliyun_ecs.g8i.large", "/Users/wenshao/Downloads/result_2.0.34_g8i.out");
+        files.put("aliyun_ecs.c8i.large", "/Users/wenshao/Downloads/result_2.0.35_g8i.out");
 //        files.put("aliyun_ecs.g7.large", "/Users/wenshao/Downloads/result_2.0.33_g7.out");
-        files.put("aliyun_ecs.g8y.large", "/Users/wenshao/Downloads/result_2.0.34_g8y.out");
+        files.put("aliyun_ecs.c8y.large", "/Users/wenshao/Downloads/result_2.0.35_g8y.out");
 //        files.put("aws_ecs.c6g.large", "/Users/wenshao/Downloads/result_2.0.33_aws_c6g.out");
 //        files.put("aws_ecs.c7g.large", "/Users/wenshao/Downloads/result_2.0.33_aws_c7g.out");
-        files.put("orangepi5p", "/Users/wenshao/Downloads/result_2.0.34_orangepi5.out");
+        files.put("orangepi5p", "/Users/wenshao/Downloads/result_2.0.35_orangepi5.out");
 
         PrintStream out = new PrintStream(new FileOutputStream(outFile));
         files.forEach((k, v) -> {
