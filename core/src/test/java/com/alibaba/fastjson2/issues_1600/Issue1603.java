25d837ff02e0cf109012aa5886a34029eea2f876
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-07-09 09:17:15 +0800

    bug fix for serialize null value

--- core/src/test/java/com/alibaba/fastjson2/issues_1600/Issue1603.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1600/Issue1603.java
@@ -2,6 +2,7 @@ package com.alibaba.fastjson2.issues_1600;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,8 +12,20 @@ public class Issue1603 {
     public void test() {
         String text = "{\"deviceId\":\"16584\",\"srcAddress\":\"109.55.156.215\",\"ssssss\":null}";
         JSONObject jsonObject = JSON.parseObject(text);
-        byte[] jsonBytes = JSON.toJSONBytes(jsonObject);
-        String s = new String(jsonBytes);
-        assertEquals("{\"deviceId\":\"16584\",\"srcAddress\":\"109.55.156.215\"}", s);
+
+        {
+            byte[] jsonBytes = JSON.toJSONBytes(jsonObject);
+            String s = new String(jsonBytes);
+            String expected = "{\"deviceId\":\"16584\",\"srcAddress\":\"109.55.156.215\"}";
+            assertEquals(expected, s);
+            assertEquals(expected, JSON.toJSONString(jsonObject));
+        }
+        {
+            byte[] jsonBytes = JSON.toJSONBytes(jsonObject, JSONWriter.Feature.WriteMapNullValue);
+            String s = new String(jsonBytes);
+            String expected = "{\"deviceId\":\"16584\",\"srcAddress\":\"109.55.156.215\",\"ssssss\":null}";
+            assertEquals(expected, s);
+            assertEquals(expected, JSON.toJSONString(jsonObject, JSONWriter.Feature.WriteMapNullValue));
+        }
     }
 }
