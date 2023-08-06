736cdf0befe75cee630b6a9bf5ae0402d2efe8f0
Kraity <kraty@krait.cn>
2023-08-06 15:30:24 +0800

    fix: illegal array to cause OOM for #1717
    Signed-off-by: Kraity <kraty@krait.cn>

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_1700/Issue1717.java
@@ -0,0 +1,27 @@
+package com.alibaba.fastjson2.issues_1700;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author kraity
+ */
+public class Issue1717 {
+    @Test
+    public void test() {
+        assertThrows(JSONException.class, () -> JSON.parseArray("[jia]", int.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[jia]", long.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[jia]", Long.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[jia]", Integer.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[jia]", Long.class, Integer.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[1,jia]", Long.class, Integer.class));
+
+        assertThrows(JSONException.class, () -> JSON.parseArray("[]", Long.class, Integer.class)); // element length mismatch
+        assertThrows(JSONException.class, () -> JSON.parseArray("[1]", Long.class, Integer.class)); // element length mismatch
+        assertDoesNotThrow(() -> JSON.parseArray("[1,2]", Long.class, Integer.class));
+        assertThrows(JSONException.class, () -> JSON.parseArray("[1,2,3]", Long.class, Integer.class)); // element length mismatch
+    }
+}
