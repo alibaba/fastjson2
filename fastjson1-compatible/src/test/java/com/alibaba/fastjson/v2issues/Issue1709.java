be3f0ab0c5ef8b3c60bb50dfce93f29e15eb384e
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-08-13 22:50:40 +0800

    fix ContextValueFilter param field is null, for issue #1709

---
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/v2issues/Issue1709.java
@@ -0,0 +1,35 @@
+package com.alibaba.fastjson.v2issues;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.filter.BeanContext;
+import com.alibaba.fastjson2.filter.ContextValueFilter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class Issue1709 {
+    @Test
+    public void test() throws Exception {
+        JSON.toJSONString(error(), new Myfilter());
+    }
+
+    public static class Myfilter
+            implements ContextValueFilter {
+        @Override
+        public Object process(BeanContext context, Object object, String name, Object value) {
+            assertNotNull(context.getField());
+            assertNotNull(context.getMethod(), context.getField().toString());
+            return value;
+        }
+    }
+
+    public Exception error() {
+        Exception error = null;
+        try {
+            int i = 5 / 0;
+        } catch (Exception e) {
+            error = e;
+        }
+        return error;
+    }
+}
