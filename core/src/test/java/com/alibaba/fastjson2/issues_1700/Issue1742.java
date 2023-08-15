e250d8f95e65ee36873bbdf6aeae5a98b60f9805
Gabriel Hwang <huang.erce@gmail.com>
2023-08-15 19:35:57 +0800

    fix checkstyle

--- core/src/test/java/com/alibaba/fastjson2/issues_1700/Issue1742.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1700/Issue1742.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import lombok.Getter;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,15 +15,15 @@ public class Issue1742 {
     public static class TestFastJson {
         private long a = 1443605444282772302L;
         private List<Long> b = new ArrayList<Long>() {{
-            add(1443605444282772302L);
-            add(1443605444282772303L);
-            add(1L);
-        }};
+                add(1443605444282772302L);
+                add(1443605444282772303L);
+                add(1L);
+            }};
     }
     @Test
     public void test() {
         TestFastJson testFastJson = new TestFastJson();
-        String jsonString = JSON.toJSONString(testFastJson,JSONWriter.Feature.BrowserCompatible);
+        String jsonString = JSON.toJSONString(testFastJson, JSONWriter.Feature.BrowserCompatible);
         assertEquals("{\"a\":\"1443605444282772302\",\"b\":[\"1443605444282772302\",\"1443605444282772303\",1]}", jsonString);
     }
 }
