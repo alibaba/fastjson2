7a14b9a171e164ef0394de4b0984da9a15a101ab
Gabriel Hwang <huang.erce@gmail.com>
2023-08-15 19:09:27 +0800

    fix browserCompatible issue

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_1700/Issue1742.java
@@ -0,0 +1,28 @@
+package com.alibaba.fastjson2.issues_1700;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue1742 {
+    @Getter
+    public static class TestFastJson {
+        private long a = 1443605444282772302L;
+        private List<Long> b = new ArrayList<Long>() {{
+            add(1443605444282772302L);
+            add(1443605444282772303L);
+            add(1L);
+        }};
+    }
+    @Test
+    public void test() {
+        TestFastJson testFastJson = new TestFastJson();
+        String jsonString = JSON.toJSONString(testFastJson,JSONWriter.Feature.BrowserCompatible);
+        assertEquals("{\"a\":\"1443605444282772302\",\"b\":[\"1443605444282772302\",\"1443605444282772303\",1]}", jsonString);
+    }
+}
