282a5af49f7a16bdd51858895703867e9190a5cb
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-09-01 00:09:53 +0800

    fix jsonb serialize error, for issue #1812

--- core/src/test/java/com/alibaba/fastjson2/issues_1800/Issue1812.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1800/Issue1812.java
@@ -1,76 +1,29 @@
 package com.alibaba.fastjson2.issues_1800;
 
-import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONB;
 import lombok.Data;
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.Serializable;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Issue1812 {
     @Test
     public void test() throws Exception {
-        URL resource = Issue1812.class.getClassLoader().getResource("issues/issue1812.json");
-        String str = FileUtils.readFileToString(new File(resource.getFile()));
-        List<DriverEntity> entity = JSON.parseArray(str, DriverEntity.class);
-        JSONB.toBytes(entity);
+        List<DriverEntity> list = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            DriverEntity entity = new DriverEntity();
+            entity.values = new ArrayList<>();
+            entity.values.add("a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890a1234567890" + i);
+            list.add(entity);
+        }
+        JSONB.toBytes(list);
     }
 
     @Data
     public class DriverEntity
             implements Serializable {
-        private String id;
-        private String uid;
-        private Integer type;
-        private String userName;
-        private String gender;
-        private String qualificationcertificatenumber;
-        private String phoneNumber;
-        private String iconUrl;
-        private String carNumber;
-        private Integer carMode;
-        private Integer carDetailMode;
-        //    private Map<String, String> images;
-        private Integer state;
-        private Integer cmsState;
-        private String verifyReason;
-        private Integer freeze;
-        private String driversLicense;
-        private String drivingLicense;
-        private String operationLicense;
-        private String idCard;
-        private Double maxWeight;
-        private Double maxVolume;
-        private Integer created;
-        private Integer updated;
-        private Integer applyTime;
-        private Integer verifiedTime;
-        //    private PathEntity path;
-        private Integer cityCode;
-        private String cityName;
-        private Double weight;
-        private Double volume;
-        private List<String> virtualGidList;
-        private List<String> virtualGidList2;
-        private Integer carNumberType;
-        private Set<String> vehicleTagIdList;
-        private String bindingVehicle;
-        private Integer vehicleState;
-        private Integer vehicleFreeze;
-        private Integer carType;
-        private String colour;
-        private String validityTime;
-        private Integer registerPlace;
-        private Integer auditstatus;
-        private String noPassCheckReason;
-        private String carModeName;
-        private String carDetilModeName;
-        private String permitnumber;
-        private Integer lbsStatus;
+        public List<String> values;
     }
 }
