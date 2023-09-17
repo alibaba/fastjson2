fd7e52e50ca5f5cc28eb13563bce331998a34de1
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-09-17 09:13:13 +0800

    add testcase for issue #1849

--- core/src/test/java/com/alibaba/fastjson2/issues_1800/Issue1849.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_1800/Issue1849.java
@@ -7,11 +7,16 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class Issue1849 {
     static final String str = "\"Indosat\",\"JAN6B41111111111\",\"1111111111\",\"1\",\"成功\",\"\",\"\",\"3\",\"3\",\"6\",\"1365000\",\"11\",\"15015000\"\n" +
+            "\"Indosat\",\"JAZ6A72222222222\",\"2222222222\",\"1\",\"失败\",\"3\",\"IOT_SHell_Login: err: connect rt:-215\",\"\",\"\",\"\",\"\",\"\",\"\"\n";
+
+    static final String str1 = "C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13\n" +
+            "\"Indosat\",\"JAN6B41111111111\",\"1111111111\",\"1\",\"成功\",\"\",\"\",\"3\",\"3\",\"6\",\"1365000\",\"11\",\"15015000\"\n" +
             "\"Indosat\",\"JAZ6A72222222222\",\"2222222222\",\"1\",\"失败\",\"3\",\"IOT_SHell_Login: err: connect rt:-215\",\"\",\"\",\"\",\"\",\"\",\"\"\n";
 
     @Test
@@ -63,6 +68,66 @@ public class Issue1849 {
                 Integer.class
         );
 
+        while (true) {
+            Object[] values = reader.readLineValues();
+            if (values == null) {
+                break;
+            }
+            assertEquals(13, values.length);
+        }
+    }
+
+    @Test
+    public void test1() {
+        StringReader stringReader = new StringReader(str1);
+        CSVReader<?> reader = CSVReader.of(
+                stringReader,
+                String.class,
+                String.class,
+                String.class,
+                int.class,
+                String.class,
+                Integer.class,
+                String.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class
+        );
+        List<String> headers = reader.readHeader();
+        assertEquals(13, headers.size());
+        while (true) {
+            Object[] values = reader.readLineValues();
+            if (values == null) {
+                break;
+            }
+            assertEquals(13, values.length);
+        }
+    }
+
+    @Test
+    public void test1UTF8() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(str1.getBytes(StandardCharsets.UTF_8));
+        CSVReader<?> reader = CSVReader.of(
+                inputStream,
+                String.class,
+                String.class,
+                String.class,
+                int.class,
+                String.class,
+                Integer.class,
+                String.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class,
+                Integer.class
+        );
+        List<String> headers = reader.readHeader();
+        assertEquals(13, headers.size());
         while (true) {
             Object[] values = reader.readLineValues();
             if (values == null) {
