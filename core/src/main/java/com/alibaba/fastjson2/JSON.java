84d850d28b828ccdd0e92df6973762556ddab4b7
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-10-06 10:25:52 +0800

    2.0.42.android4 init

--- core/src/main/java/com/alibaba/fastjson2/JSON.java
+++ core/src/main/java/com/alibaba/fastjson2/JSON.java
@@ -31,7 +31,7 @@ public interface JSON {
     /**
      * fastjson2 version name
      */
-    String VERSION = "2.0.41";
+    String VERSION = "2.0.42";
 
     /**
      * Parses the json string as a {@link JSONArray} or {@link JSONObject}.
