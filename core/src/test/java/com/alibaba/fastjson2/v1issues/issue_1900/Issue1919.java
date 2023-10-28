73ce66eb4df22b8ea90f7a4cce0110ff41752266
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-10-28 20:57:25 +0800

    improve testcase

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1900/Issue1919.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_1900/Issue1919.java
@@ -1,7 +1,11 @@
 package com.alibaba.fastjson2.v1issues.issue_1900;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Issue1919 {
     @Test
@@ -10,7 +14,8 @@ public class Issue1919 {
         for (int i = 0; i < 2500; i++) {
             buf.append('0');
         }
-        String a = buf.append("}").toString();
-        JSON.parseObject(a);
+        String str = buf.append("}").toString();
+        JSONObject jsonObject = JSON.parseObject(str);
+        assertEquals(str, jsonObject.toJSONString(JSONWriter.Feature.WriteBigDecimalAsPlain));
     }
 }
