d2d0cd447af07090f4f8d1bab2768819617372dd
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-12-01 16:19:35 +0800

    support writeAs

---
+++ core/src/test/java/com/alibaba/fastjson2/JSONWriterWriteAs.java
@@ -0,0 +1,27 @@
+package com.alibaba.fastjson2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JSONWriterWriteAs {
+    @Test
+    public void test() {
+        B b = new B();
+        b.id = 123;
+        b.name = "abc";
+
+        JSONWriter jsonWriter = JSONWriter.of();
+        jsonWriter.writeAs(b, A.class);
+        assertEquals("{\"id\":123}", jsonWriter.toString());
+    }
+
+    public static class A {
+        public int id;
+    }
+
+    public static class B
+            extends A {
+        public String name;
+    }
+}
