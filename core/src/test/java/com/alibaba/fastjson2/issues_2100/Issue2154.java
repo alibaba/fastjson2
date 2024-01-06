37a1dc8225da99f8c3b8df1c01e982d3b4077035
rowstop <xeepoo@yeah.net>
2024-01-06 15:22:19 +0800

    fix non public enum deserialize error, for issue #2154

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_2100/Issue2154.java
@@ -0,0 +1,54 @@
+package com.alibaba.fastjson2.issues_2100;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.annotation.JSONField;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author 张治保
+ * @since 2024/1/6
+ */
+public class Issue2154 {
+    // failure
+    @Test
+    public void intEnumDeserialize1() {
+        Bean1 bean1 = JSON.parseObject("{\"type\":102}", Bean1.class);
+        assertEquals(102, bean1.type.getCode());
+    }
+
+    // success
+    @Test
+    public void intEnumDeserialize2() {
+        // add this
+        {
+            Bean1 bean = new Bean1();
+            bean.type = Type.S;
+            JSON.toJSONString(bean);
+        }
+
+        intEnumDeserialize1();
+    }
+
+    class Bean1 {
+        public Type type;
+    }
+
+    enum Type {
+        X(101),
+        M(102),
+        S(103);
+
+        private final int code;
+
+        Type(int code) {
+            this.code = code;
+        }
+
+        @JSONField(value = true)
+        public int getCode() {
+            return code;
+        }
+    }
+}
