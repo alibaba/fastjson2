cb687f97888f65c685c05b540f4cbf0ee3676db6
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-01-27 10:34:37 +0800

    support fastjson 1.x syntax 'new Date(millis)', for issue #2086

--- core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderImplObject.java
+++ core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderImplObject.java
@@ -216,8 +216,7 @@ public final class ObjectReaderImplObject
                         value = jsonReader.readBoolValue();
                         break;
                     case 'n':
-                        jsonReader.readNull();
-                        value = null;
+                        value = jsonReader.readNullOrNewDate();
                         break;
                     case 'S':
                         if (jsonReader.nextIfSet()) {
