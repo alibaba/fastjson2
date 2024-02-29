f444eb81e71a82f87f2b49072ab8306aa01e0519
shenfeng <shenfeng197@gmail.com>
2024-02-29 14:15:46 +0800

    fix test

--- core/src/test/java/com/alibaba/fastjson2/issues_2200/Issue2279.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2200/Issue2279.java
@@ -2,6 +2,7 @@ package com.alibaba.fastjson2.issues_2200;
 
 import com.alibaba.fastjson2.*;
 import com.alibaba.fastjson2.util.ParameterizedTypeImpl;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
@@ -15,16 +16,8 @@ public class Issue2279 {
 
         byte[] bytes = JSONB.toBytes(result, JSONWriter.Feature.WriteClassName, JSONWriter.Feature.FieldBased, JSONWriter.Feature.ErrorOnNoneSerializable, JSONWriter.Feature.ReferenceDetection, JSONWriter.Feature.WriteNulls, JSONWriter.Feature.NotWriteDefaultValue, JSONWriter.Feature.NotWriteHashMapArrayListClassName, JSONWriter.Feature.WriteNameAsSymbol);
         ParameterizedTypeImpl parameterizedType = new ParameterizedTypeImpl(Map.class, Integer.class, new ParameterizedTypeImpl(List.class, Integer.class));
-        Object o = JSONB.parseObject(bytes, parameterizedType);
-        System.out.println(JSONObject.toJSONString(o));
-    }
-    @Test
-    public void test02() {
-        List<Integer> list = Arrays.asList(1, 1);
-
-        byte[] bytes = JSONB.toBytes(list, JSONWriter.Feature.WriteClassName, JSONWriter.Feature.FieldBased, JSONWriter.Feature.ErrorOnNoneSerializable, JSONWriter.Feature.ReferenceDetection, JSONWriter.Feature.WriteNulls, JSONWriter.Feature.NotWriteDefaultValue, JSONWriter.Feature.NotWriteHashMapArrayListClassName, JSONWriter.Feature.WriteNameAsSymbol);
-        ParameterizedTypeImpl parameterizedType = new ParameterizedTypeImpl(List.class, Integer.class);
-        Object o = JSONB.parseObject(bytes, parameterizedType);
-        System.out.println(JSONObject.toJSONString(o));
+        Map<Integer, List<Integer>> o = JSONB.parseObject(bytes, parameterizedType);
+        Integer i = o.get(1).get(0);
+        Assert.assertEquals(-1,(int) i);
     }
 }
