581eff78858e29f684a2d383c6694f40283537b7
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-03-17 12:52:26 +0800

    support new Feature#NonErrorOnNumberOverflow, for issue #2305

--- core/src/main/java/com/alibaba/fastjson2/JSONReader.java
+++ core/src/main/java/com/alibaba/fastjson2/JSONReader.java
@@ -809,8 +809,12 @@ public abstract class JSONReader
                     return (int) longValue;
                 }
                 if (number instanceof BigInteger) {
+                    BigInteger bigInt = (BigInteger) number;
+                    if ((context.features & Feature.NonErrorOnNumberOverflow.mask) != 0) {
+                        return bigInt.intValue();
+                    }
                     try {
-                        return ((BigInteger) number).intValueExact();
+                        return bigInt.intValueExact();
                     } catch (ArithmeticException e) {
                         throw numberError();
                     }
@@ -864,8 +868,12 @@ public abstract class JSONReader
                 }
                 Number number = getNumber();
                 if (number instanceof BigInteger) {
+                    BigInteger bigInt = (BigInteger) number;
+                    if ((context.features & Feature.NonErrorOnNumberOverflow.mask) != 0) {
+                        return bigInt.longValue();
+                    }
                     try {
-                        return ((BigInteger) number).longValueExact();
+                        return bigInt.longValueExact();
                     } catch (ArithmeticException e) {
                         throw numberError();
                     }
@@ -4422,9 +4430,14 @@ public abstract class JSONReader
 
         /**
          *  empty string "" convert to null
-         * since 2.0.48
+         * @since 2.0.48
          */
-        EmptyStringAsNull(1 << 27);
+        EmptyStringAsNull(1 << 27),
+
+        /**
+         * @since 2.0.48
+         */
+        NonErrorOnNumberOverflow(1 << 28);
 
         public final long mask;
 
