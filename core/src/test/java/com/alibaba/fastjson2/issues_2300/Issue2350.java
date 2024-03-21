8f1c3c67dac2fa114ccb84761df716850993e20c
yanxutao89 <910135896@qq.com>
2024-03-21 12:20:02 +0800

    fix support FieldBased feature on field for issue #2350

--- core/src/test/java/com/alibaba/fastjson2/issues_2300/Issue2350.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2300/Issue2350.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class Issue2350 {
-
     @Test
     public void test() {
         String json = "{\n" +
