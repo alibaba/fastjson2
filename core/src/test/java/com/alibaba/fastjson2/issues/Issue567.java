1d9105044442f9b74aff88078bc06f52db51f2cc
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-03-25 00:20:23 +0800

    fix testcase

--- core/src/test/java/com/alibaba/fastjson2/issues/Issue567.java
+++ core/src/test/java/com/alibaba/fastjson2/issues/Issue567.java
@@ -1,10 +1,13 @@
 package com.alibaba.fastjson2.issues;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONReader;
 import com.alibaba.fastjson2.JSONWriter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Issue567 {
     @Test
@@ -15,10 +18,9 @@ public class Issue567 {
         Bean test = new Bean();
         test.setConfig(catConfig);
         String json = JSON.toJSONString(test, JSONWriter.Feature.WriteClassName);
-        System.out.println(json);
         //JSON结果 {"@type":"com.example.fastjson2.Test","config":{"@type":"com.example.fastjson2.Test$CatConfig","height":"200","width":"100"}}
-        Bean t = (Bean) JSON.parseObject(json, Bean.class);
-        System.out.println(t.getConfig());
+        Bean t = (Bean) JSON.parseObject(json, Bean.class, JSONReader.Feature.SupportAutoType);
+        assertTrue(t.getConfig() instanceof Bean.CatConfig);
         //对象结果：Test.Config()
     }
 
