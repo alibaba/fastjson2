42b6abcd1759bc3d86a6656f519d996b654b905f
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-04-13 14:20:23 +0800

    add testcase for issue #2426

--- core/src/test/java/com/alibaba/fastjson2/issues_2400/Issue2426.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2400/Issue2426.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson2.JSONFactory;
 import com.alibaba.fastjson2.JSONWriter;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,11 +15,19 @@ public class Issue2426 {
     public void test() {
         String defaultWriterFormat = JSONFactory.getDefaultWriterFormat();
         try {
-            JSON.configWriterDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-            Date date = new Date(1712988987882L);
+            String dateFormat = "yyyy-MM-dd'T'HH:mm:ss";
+            JSON.configWriterDateFormat(dateFormat);
+
             JSONWriter.Context writeContext = JSONFactory.createWriteContext();
+            assertEquals(dateFormat, writeContext.getDateFormat());
+
+            Date date = new Date(1712988987882L);
             String json = JSON.toJSONString(date, writeContext);
             assertEquals("\"2024-04-13T14:16:27\"", json);
+
+            java.sql.Timestamp ts = new Timestamp(date.getTime());
+            String json1 = JSON.toJSONString(date, writeContext);
+            assertEquals("\"2024-04-13T14:16:27\"", json1);
         } finally {
             JSON.configWriterDateFormat(defaultWriterFormat);
         }
