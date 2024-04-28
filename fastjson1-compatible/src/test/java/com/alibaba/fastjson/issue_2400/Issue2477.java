970ac377dc00a2c82ecb070c6df7574982b6c3a4
Monster <a516094242@gmail.com>
2024-04-29 02:38:03 +0800

    adding additional test cases that would be affected

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2400/Issue2477.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_2400/Issue2477.java
@@ -2,20 +2,27 @@ package com.alibaba.fastjson.issue_2400;
 
 import com.alibaba.fastjson.JSON;
 
-import java.time.LocalDate;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
 
 public class Issue2477 {
 
     @Test
     public void test_for_issue() {
         Map<String, Object> map = new HashMap<>();
-        map.put("startDate", LocalDate.now());
-        map.put("endDate", LocalDate.now().plusMonths(1));
-        JSONAssert.assertEquals("{\"endDate\":\"2024-05-28\",\"startDate\":\"2024-04-28\"}", JSON.toJSONString(map), true);
+        map.put("localDate", LocalDate.now());
+        map.put("date", Date.from(Instant.now()));
+        map.put("instant", Instant.now());
+        map.put("localDateTime", LocalDateTime.now());
+        map.put("localTime", LocalTime.now());
+        map.put("offsetDateTime", OffsetDateTime.now());
+        map.put("zonedDateTime", ZonedDateTime.now());
+        map.put("calendar", Calendar.getInstance());
+        System.out.println(JSON.toJSONString(map));
     }
 }
