a8da517d75f0074e0150be81afb40314b6586172
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-05-02 23:31:12 +0800

    fix jdk9 module error, for issue #2451

--- core/src/main/moditect/module-info.java
+++ core/src/main/moditect/module-info.java
@@ -1,6 +1,7 @@
 // https://www.oracle.com/corporate/features/understanding-java-9-modules.html
 
 open module com.alibaba.fastjson2 {
+    requires transitive fastjson
     requires transitive jdk.unsupported;
     requires java.management;
 
