8bf95d5735c0a496ce8634fe8e080f1e9ac8feec
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-05-12 23:50:11 +0800

    fix code style

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/v2issues/Issue2477.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/v2issues/Issue2477.java
@@ -13,8 +13,8 @@ public class Issue2477 {
     @Test
     public void jsonTest() {
         Map<String, Object> map = new LinkedHashMap<>();
-        map.put("startDate", LocalDate.of(2019,7, 14));
-        map.put("endDate", LocalDate.of(2019,8, 14));
+        map.put("startDate", LocalDate.of(2019, 7, 14));
+        map.put("endDate", LocalDate.of(2019, 8, 14));
         assertEquals("{\"startDate\":\"2019-07-14\",\"endDate\":\"2019-08-14\"}", JSON.toJSONString(map));
     }
 }
