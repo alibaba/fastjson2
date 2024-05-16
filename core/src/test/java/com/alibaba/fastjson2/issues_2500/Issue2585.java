5893fc864ea0f214f6e26df1f42e278063fc1611
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2024-05-17 07:42:06 +0800

    fix JSONPath#extract, for issue #2584 #2585

--- core/src/test/java/com/alibaba/fastjson2/issues_2500/Issue2585.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2500/Issue2585.java
@@ -29,8 +29,6 @@ public class Issue2585 {
                 "  \"data\": [1,2], \n" +
                 "  \"message\": \"code: 1003,以【你好】开头的句子的长度不符合要求, 长度限制：3~500，实际长度：2\"\n" +
                 "}\n";
-        System.out.println(str1);
-        System.out.println(str2);
         assertEquals(
                 JSONPath.eval(str1, "$.data[0][0]"),
                 JSONPath.eval(str2, "$.data[0][0]"));
@@ -44,8 +42,6 @@ public class Issue2585 {
                 "  \"data\": [{\"id\":\"1\"}], \n" +
                 "  \"message\": \"code: 1003,以【你好】开头的句子的长度不符合要求, 长度限制：3~500，实际长度：2\"\n" +
                 "}\n";
-        System.out.println(str1);
-        System.out.println(str2);
         assertEquals(
                 JSONPath.eval(str1, "$.data[0][0]"),
                 JSONPath.eval(str2, "$.data[0][0]"));
