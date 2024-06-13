005c75e83f450a4947b0b04a7b41beb75f330853
xumoujun <xumoujun@szy.cn>
2024-06-13 09:53:58 +0800

    PMD

--- core/src/test/java/com/alibaba/fastjson2/writer/ObjectWriterImplListEnumTest.java
+++ core/src/test/java/com/alibaba/fastjson2/writer/ObjectWriterImplListEnumTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectWriterImplListEnumTest {
-
     @Test
     public void testNullEnumObjInList() {
         Bean bean = new Bean();
