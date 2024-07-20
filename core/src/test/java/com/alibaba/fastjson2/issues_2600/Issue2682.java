459aa124625229407424cf46b97c9b348816d68c
poo0054 <poo0054@outlook.com>
2024-07-20 16:46:36 +0800

    fix BeanUtils#getEnumValueField, for issue #2682

--- core/src/test/java/com/alibaba/fastjson2/issues_2600/Issue2682.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2600/Issue2682.java
@@ -1,14 +1,29 @@
 package com.alibaba.fastjson2.issues_2600;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
 import com.alibaba.fastjson2.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.EnumValue;
 import com.baomidou.mybatisplus.annotation.IEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
+import sun.misc.VM;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * @author yanxutao89
+ * @author poo00
+ */
 public class Issue2682 {
     @Test
     public void test() {
@@ -17,9 +32,18 @@ public class Issue2682 {
         assertEquals(BizType.COMMON, vm.getBizType());
     }
 
+    @Test
+    public void test1() throws IOException {
+        String str = "{'bizType':'common','baseType':'123啊'}";
+        VM vm = JSON.parseObject(str, VM.class);
+        assertEquals(RobotActEnum.CARTON_SCAN, vm.getBaseType());
+    }
+
     @Data
     public static class VM {
         private BizType bizType;
+
+        private RobotActEnum baseType;
     }
 
     public enum BizType
@@ -45,6 +69,68 @@ public class Issue2682 {
         }
 
         public static void test123() {
+        }
+    }
+
+
+    public enum RobotActEnum implements IEnum<String> {
+        /**
+         * 123啊
+         */
+        CARTON_SCAN("123啊", "我是123"),
+
+        /**
+         * 456啊
+         */
+        PALLET_ON_SHELF("456啊", "我是456"),
+
+        /**
+         * 789啊
+         */
+        OUTBOUND_BIND_DOCK("789啊", "我是789"),
+        /**
+         *147啊
+         */
+        GET_PICK_BILL("147啊", "我是147"),
+        ;
+
+
+        @JSONField(value = true)
+        private final String value;
+
+        private final String message;
+
+
+        RobotActEnum(String value, String message) {
+            this.value = value;
+            this.message = message;
+        }
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public RobotActEnum getEnum() {
+            return this;
+        }
+
+        public static String staticMessage(RobotActEnum robotActEnum) {
+            if (null == robotActEnum) {
+                return null;
+            }
+            return robotActEnum.getMessage();
+        }
+
+        public static String staticValue(RobotActEnum robotActEnum) {
+            if (null == robotActEnum) {
+                return null;
+            }
+            return robotActEnum.getValue();
         }
     }
 }
