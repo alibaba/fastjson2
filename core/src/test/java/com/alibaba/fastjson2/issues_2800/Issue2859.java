ebb86f760d647c25dc5cf53fec79a87703264acc
高铁 <shaojin.wensj@alibaba-inc.com>
2024-08-11 02:23:06 +0800

    bug fix deserialize EnumMap, for issue #2859

--- core/src/test/java/com/alibaba/fastjson2/issues_2800/Issue2859.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2800/Issue2859.java
@@ -1,11 +1,17 @@
 package com.alibaba.fastjson2.issues_2800;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -50,5 +56,20 @@ public class Issue2859 {
 
     static class Bean1 {
         public LocalDate date;
+    }
+
+    @Test
+    public void testEnumMap() {
+        Map<ApplicationType, List<Bean>> map = new EnumMap<>(ApplicationType.class);
+        map.put(ApplicationType.AI, new ArrayList<>());
+        String str = JSON.toJSONString(map);
+        System.out.println(str);
+        Type type = new TypeReference<EnumMap<ApplicationType, List<Bean>>>() {}.getType();
+        Map<ApplicationType, List<Bean>> parsed = JSON.parseObject(str, type);
+        assertEquals(map.get(ApplicationType.AI), parsed.get(ApplicationType.AI));
+    }
+
+    public enum ApplicationType {
+        AI, Normal
     }
 }
