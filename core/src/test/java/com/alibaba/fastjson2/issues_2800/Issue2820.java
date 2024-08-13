6a326b14201e5528a5292d2ee4661bfc3967b74d
高铁 <shaojin.wensj@alibaba-inc.com>
2024-08-14 01:46:59 +0800

    fix build error

--- core/src/test/java/com/alibaba/fastjson2/issues_2800/Issue2820.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2800/Issue2820.java
@@ -3,7 +3,6 @@ package com.alibaba.fastjson2.issues_2800;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.annotation.JSONField;
-import org.citygml4j.core.model.CityGMLVersion;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,6 +24,7 @@ public class Issue2820 {
 
         assertEquals(CityGMLVersion.v2_0, foo.getVersion());
     }
+
     public class FOO {
         @JSONField(serializeFeatures = JSONWriter.Feature.WriteEnumUsingToString)
         private CityGMLVersion version;
@@ -41,6 +41,35 @@ public class Issue2820 {
         public FOO setVersion(String version) {
             this.version = CityGMLVersion.fromValue(version);
             return this;
+        }
+    }
+
+    public enum CityGMLVersion {
+        v3_0("3.0"),
+        v2_0("2.0"),
+        v1_0("1.0");
+
+        private final String value;
+
+        CityGMLVersion(String value) {
+            this.value = value;
+        }
+
+        public String toValue() {
+            return this.value;
+        }
+
+        public static CityGMLVersion fromValue(String value) {
+            for (CityGMLVersion v : values()) {
+                if (v.value.equals(value)) {
+                    return v;
+                }
+            }
+            return null;
+        }
+
+        public String toString() {
+            return this.value;
         }
     }
 }
