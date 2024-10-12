16c4f0a2ed5190d14dcbe8ec860f5da76b07ea90
Mukta13 <mukta@jaiswal.in>
2024-10-12 12:40:04 -0500

    Fix flaky test

--- core/src/test/java/com/alibaba/fastjson2/issues_2600/Issue2672.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_2600/Issue2672.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.filter.PropertyPreFilter;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,9 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Issue2672 {
     @Test
     public void test() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("k", "v");
+        Map<String, Object> map = new LinkedHashMap<>();
         map.put("selfRef", map);
+        map.put("k", "v");
         assertEquals("{\"selfRef\":{\"$ref\":\"..\"},\"k\":\"v\"}",
                 JSON.toJSONString(map, JSONWriter.Feature.ReferenceDetection));
 
