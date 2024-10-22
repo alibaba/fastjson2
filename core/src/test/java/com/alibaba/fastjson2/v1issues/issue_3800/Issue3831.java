ca1d527e6407cc53683ac83f6ae40ab346ad34d2
muktaj2 <mukta@jaiswal.in>
2024-10-22 09:51:11 -0500

    Fix non-determinsm

--- core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3800/Issue3831.java
+++ core/src/test/java/com/alibaba/fastjson2/v1issues/issue_3800/Issue3831.java
@@ -2,7 +2,7 @@ package com.alibaba.fastjson2.v1issues.issue_3800;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,31 +10,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Issue3831 {
     @Test
     public void test_for_issue3831() {
-        Map<String, Object> longType = new HashMap<>();
+        Map<String, Object> longType = new LinkedHashMap<>();
         longType.put("type", "long");
 
-        Map<String, Object> textType = new HashMap<>();
+        Map<String, Object> textType = new LinkedHashMap<>();
         textType.put("type", "text");
         textType.put("analyzer", "standard");
 
-        Map<String, Object> raw = new HashMap<>();
+        Map<String, Object> raw = new LinkedHashMap<>();
         raw.put("type", "keyword");
         raw.put("doc_values", "false");
 
-        Map<String, Object> fields = new HashMap<>();
+        Map<String, Object> fields = new LinkedHashMap<>();
         fields.put("raw", raw);
 
-        Map<String, Object> rawLongType = new HashMap<>();
+        Map<String, Object> rawLongType = new LinkedHashMap<>();
         rawLongType.put("type", "long");
         rawLongType.put("fields", fields);
 
-        Map<String, Object> properties = new HashMap<>();
+        Map<String, Object> properties = new LinkedHashMap<>();
         properties.put("id", longType);
         properties.put("name", textType);
         properties.put("cityId", longType);
         properties.put("categoryId", rawLongType);
 
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> result = new LinkedHashMap<>();
         result.put("properties", properties);
 
         assertEquals(com.alibaba.fastjson2.JSON.toJSONString(result), new com.google.gson.Gson().toJson(result));
