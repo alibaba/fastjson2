99611e8becf2dfce0be30f848a5bf7a5f608a82c
ra1nz <378243950@qq.com>
2024-12-22 22:26:55 +0800

    fix maven validate

--- core/src/test/java/com/alibaba/fastjson2/issues_3200/Issue3208.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_3200/Issue3208.java
@@ -1,19 +1,27 @@
 package com.alibaba.fastjson2.issues_3200;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 public class Issue3208 {
-
     @Test
     public void testArray() {
         byte[] bytes = new byte[]{1, 2, 3};
-        String jsonString = JSON.toJSONString(bytes, SerializerFeature.WriteClassName);
+        String jsonString = JSON.toJSONString(new byte[]{1, 2, 3}, SerializerFeature.WriteClassName);
         byte[] bytes1 = (byte[]) JSON.parse(jsonString);
         Assertions.assertTrue(Arrays.equals(bytes, bytes1));
+    }
+
+    @Test
+    public void testObjectArray() {
+        byte[] jsonBytes = JSON.toJSONBytes(new Object[]{JSON.toJSONString(ImmutableMap.of("a", 1))}, new SerializeConfig(true), JSON.DEFAULT_GENERATE_FEATURE, SerializerFeature.WriteClassName);
+        Object parsed = JSON.parse(new String(jsonBytes));
+        Assertions.assertNotNull(parsed);
     }
 }
