b25d78d2371a538c60a0e16b030d3547fc177978
wenshao <shaojin.wensj@alibaba-inc.com>
2025-01-11 20:56:31 +0800

    fix testcase

--- core/src/test/java/com/alibaba/fastjson2/JSONFactoryTest.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONFactoryTest.java
@@ -26,6 +26,8 @@ public class JSONFactoryTest {
     @Test
     public void test1() {
         JSONFactory.setUseJacksonAnnotation(false);
+        JSONFactory.setUseGsonAnnotation(false);
+        assertFalse(JSONFactory.isUseGsonAnnotation());
         assertFalse(JSONFactory.isUseJacksonAnnotation());
         JSONFactory.setUseJacksonAnnotation(true);
         JSONFactory.setUseGsonAnnotation(true);
