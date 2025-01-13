4d45413932ea6544698a1514d40d9faff4874908
wenshao <shaojin.wensj@alibaba-inc.com>
2025-01-13 19:54:52 +0800

    use writeU4Hex2

--- core/src/main/java/com/alibaba/fastjson2/JSONWriterUTF16.java
+++ core/src/main/java/com/alibaba/fastjson2/JSONWriterUTF16.java
@@ -625,25 +625,9 @@ class JSONWriterUTF16
                 case 5:
                 case 6:
                 case 7:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('0' + (int) ch);
-                    off += 6;
-                    break;
                 case 11:
                 case 14:
                 case 15:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('a' + (ch - 10));
-                    off += 6;
-                    break;
                 case 16:
                 case 17:
                 case 18:
@@ -654,26 +638,13 @@ class JSONWriterUTF16
                 case 23:
                 case 24:
                 case 25:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('0' + (ch - 16));
-                    off += 6;
-                    break;
                 case 26:
                 case 27:
                 case 28:
                 case 29:
                 case 30:
                 case 31:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('a' + (ch - 26));
+                    writeU4Hex2(chars, off, ch);
                     off += 6;
                     break;
                 case '<':
@@ -739,25 +710,9 @@ class JSONWriterUTF16
                 case 5:
                 case 6:
                 case 7:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('0' + (int) ch);
-                    off += 6;
-                    break;
                 case 11:
                 case 14:
                 case 15:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('a' + (ch - 10));
-                    off += 6;
-                    break;
                 case 16:
                 case 17:
                 case 18:
@@ -768,26 +723,13 @@ class JSONWriterUTF16
                 case 23:
                 case 24:
                 case 25:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('0' + (ch - 16));
-                    off += 6;
-                    break;
                 case 26:
                 case 27:
                 case 28:
                 case 29:
                 case 30:
                 case 31:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('a' + (ch - 26));
+                    writeU4Hex2(chars, off, ch);
                     off += 6;
                     break;
                 case '<':
@@ -989,25 +931,9 @@ class JSONWriterUTF16
                 case 5:
                 case 6:
                 case 7:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('0' + (int) ch);
-                    off += 6;
-                    break;
                 case 11:
                 case 14:
                 case 15:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '0';
-                    chars[off + 5] = (char) ('a' + (ch - 10));
-                    off += 6;
-                    break;
                 case 16:
                 case 17:
                 case 18:
@@ -1018,26 +944,13 @@ class JSONWriterUTF16
                 case 23:
                 case 24:
                 case 25:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('0' + (ch - 16));
-                    off += 6;
-                    break;
                 case 26:
                 case 27:
                 case 28:
                 case 29:
                 case 30:
                 case 31:
-                    chars[off] = '\\';
-                    chars[off + 1] = 'u';
-                    chars[off + 2] = '0';
-                    chars[off + 3] = '0';
-                    chars[off + 4] = '1';
-                    chars[off + 5] = (char) ('a' + (ch - 26));
+                    writeU4Hex2(chars, off, ch);
                     off += 6;
                     break;
                 default:
