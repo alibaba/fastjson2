d1ecfc9f9d40a68bb23cf7b25f80eef282913a0e
CodePlayer <codeplayer360@gmail.com>
2025-01-24 12:58:22 +0800

    Remove unnecessary assertions

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1900/Issue1996.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1900/Issue1996.java
@@ -3,6 +3,8 @@ package com.alibaba.fastjson.issue_1900;
 import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Issue1996 {
@@ -22,14 +24,11 @@ public class Issue1996 {
 
         // ok
         String json1 = JSON.toJSONString(s);
-        byte[] bytes = json1.getBytes("utf-8");
+        byte[] bytes = json1.getBytes(StandardCharsets.UTF_8);
 
         byte[] bytes2 = JSON.toJSONBytes(s);
         assertEquals(new String(bytes), new String(bytes2));
 
         assertEquals(bytes.length, bytes2.length);
-        for (int i = 0; i < bytes.length; i++) {
-            assertEquals(bytes[i], bytes[i]);
-        }
     }
 }
