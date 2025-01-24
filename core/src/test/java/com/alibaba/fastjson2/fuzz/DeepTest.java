70126ee9eeb8caa23ef5e6bb03693f6787b58597
wenshao <shaojin.wensj@alibaba-inc.com>
2025-01-25 00:37:30 +0800

    fix build error

--- core/src/test/java/com/alibaba/fastjson2/fuzz/DeepTest.java
+++ core/src/test/java/com/alibaba/fastjson2/fuzz/DeepTest.java
@@ -73,7 +73,7 @@ public class DeepTest {
         StringBuilder buf = new StringBuilder();
         int count = 100000;
         if (System.getProperty("os.name").toLowerCase().contains("win") && JDKUtils.JVM_VERSION == 11) {
-            count = 80000;
+            count = 50000;
         }
         for (int i = 0; i < count; i++) {
             buf.append("{\"v\":");
