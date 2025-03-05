451e6959d1d8848982f7fa2e8718143f41a178ac
wenshao <shaojin.wensj@alibaba-inc.com>
2025-03-05 13:13:30 +0800

    fix checkstyle

--- core/src/main/java/com/alibaba/fastjson2/reader/FieldReaderListParam.java
+++ core/src/main/java/com/alibaba/fastjson2/reader/FieldReaderListParam.java
@@ -3,12 +3,9 @@ package com.alibaba.fastjson2.reader;
 import com.alibaba.fastjson2.schema.JSONSchema;
 import com.alibaba.fastjson2.util.Fnv;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
 import java.util.Locale;
-import java.util.function.BiConsumer;
 
 public class FieldReaderListParam
         extends FieldReaderList {
