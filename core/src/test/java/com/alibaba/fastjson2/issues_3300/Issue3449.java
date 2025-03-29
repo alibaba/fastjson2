177256b111f9ff812d3738ce4d5df10911972ab8
wenshao <shaojin.wensj@alibaba-inc.com>
2025-03-29 22:18:14 +0800

    add testcase

--- core/src/test/java/com/alibaba/fastjson2/issues_3300/Issue3449.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_3300/Issue3449.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Issue3449 {
     @Test
-    public void test() {
+    public void testDoubleNegativeNaN() {
         assertEquals("null", JSON.toJSONString(Double.NaN));
         assertEquals("null", new String(JSON.toJSONBytes(Double.NaN)));
 
@@ -15,6 +15,19 @@ public class Issue3449 {
         long bits = Double.doubleToLongBits(nan);
         long newBits = bits | 0x8000000000000000L;   // 转为负数
         double newNan = Double.longBitsToDouble(newBits);
+        assertEquals("null", JSON.toJSONString(newNan));
+        assertEquals("null", new String(JSON.toJSONBytes(newNan)));
+    }
+
+    @Test
+    public void testFloatNegativeNan() {
+        assertEquals("null", JSON.toJSONString(Float.NaN));
+        assertEquals("null", new String(JSON.toJSONBytes(Float.NaN)));
+
+        float nan = Float.NaN;
+        int bits = Float.floatToIntBits(nan);
+        int newBits = bits | 0x80000000;   // 转为负数
+        float newNan = Float.intBitsToFloat(newBits);
         assertEquals("null", JSON.toJSONString(newNan));
         assertEquals("null", new String(JSON.toJSONBytes(newNan)));
     }
