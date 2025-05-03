23ac0978d13f66c49e1ca2ed60bf0c2e943c42ed
yanxutao89 <910135896@qq.com>
2025-05-03 20:08:58 +0800

    Fix 3185 (#3532)
    * fix adapt result recursively, for issue #3185
    
    * fix rename variable
    
    * fix checkstyle

--- fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1300/Issue_for_zuojian.java
+++ fastjson1-compatible/src/test/java/com/alibaba/fastjson/issue_1300/Issue_for_zuojian.java
@@ -9,21 +9,21 @@ import java.util.Date;
 public class Issue_for_zuojian {
     @Test
     public void test_for_issue() throws Exception {
-        JSON.DEFFAULT_DATE_FORMAT = "yyyyMMddHHmmssSSSZ";
+        JSON.DEFAULT_DATE_FORMAT = "yyyyMMddHHmmssSSSZ";
         String json = "{\"value\":\"20180131022733000-0800\"}";
         JSON.parseObject(json, Model.class);
-        JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+        JSON.DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     }
 
     @Test
     public void test_for_issue_1() throws Exception {
-        JSON.DEFFAULT_DATE_FORMAT = "yyyyMMddHHmmssSSSZ";
+        JSON.DEFAULT_DATE_FORMAT = "yyyyMMddHHmmssSSSZ";
 
         String json = "{\"value\":\"20180131022733000-0800\"}";
         JSONObject object = JSON.parseObject(json);
         object.getObject("value", Date.class);
 
-        JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+        JSON.DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     }
 
     public static class Model {
