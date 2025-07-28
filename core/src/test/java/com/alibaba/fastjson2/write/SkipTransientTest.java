c7988685a7902f906f57d03fbd6bf400f34d13d0
jujn <2087687391@qq.com>
2025-07-28 20:15:00 +0800

    fix clear cache

--- core/src/test/java/com/alibaba/fastjson2/write/SkipTransientTest.java
+++ core/src/test/java/com/alibaba/fastjson2/write/SkipTransientTest.java
@@ -10,38 +10,41 @@ import java.beans.Transient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SkipTransientTest1 { // 测试全局配置
+public class SkipTransientTest { // 测试全局配置
     @Test
     public void test() {
-        JSONFactory.setDefaultSkipTransient(false);
-        JSONFactory.getDefaultObjectWriterProvider().clear();
+        try {
+            JSONFactory.setDefaultSkipTransient(false);
 
-        A a = new A();
-        a.id = 100;
-        a.name = "fast";
-        assertEquals("{\"id\":100,\"name\":\"fast\"}", JSON.toJSONString(a));
+            A a = new A();
+            a.id = 100;
+            a.name = "fast";
+            assertEquals("{\"id\":100,\"name\":\"fast\"}", JSON.toJSONString(a));
 
-        A2 a2 = new A2();
-        a2.id = 100;
-        assertEquals("{\"id\":100}", JSON.toJSONString(a2));
+            A2 a2 = new A2();
+            a2.id = 100;
+            assertEquals("{\"id\":100}", JSON.toJSONString(a2));
 
-        A3 a3 = new A3();
-        a3.id = 100;
-        assertEquals("{\"id\":100}", JSON.toJSONString(a3));
+            A3 a3 = new A3();
+            a3.id = 100;
+            assertEquals("{\"id\":100}", JSON.toJSONString(a3));
 
-        A4 a4 = new A4();
-        a4.id = 100;
-        assertEquals("{\"id\":100}", JSON.toJSONString(a4));
+            A4 a4 = new A4();
+            a4.id = 100;
+            assertEquals("{\"id\":100}", JSON.toJSONString(a4));
 
-        A5 a5 = new A5();
-        a5.id = 100;
-        assertEquals("{\"id\":100}", JSON.toJSONString(a5));
+            A5 a5 = new A5();
+            a5.id = 100;
+            assertEquals("{\"id\":100}", JSON.toJSONString(a5));
+        } finally {
+            JSONFactory.setDefaultSkipTransient(true);
+            JSONFactory.getDefaultObjectWriterProvider().clear();
+        }
     }
 
     @Test
     public void test_2() {
         JSONFactory.setDefaultSkipTransient(true);
-        JSONFactory.getDefaultObjectWriterProvider().clear();
 
         A a = new A();
         a.id = 100;
@@ -122,13 +125,17 @@ public class SkipTransientTest1 { // 测试全局配置
 
     @Test
     public void test_3() {
-        JSONFactory.setDefaultSkipTransient(false);
-        JSONFactory.getDefaultObjectWriterProvider().clear();
+        try {
+            JSONFactory.setDefaultSkipTransient(false);
 
-        B b = new B();
-        b.id = 100;
-        b.name = "fast";
-        assertEquals("{\"id\":100,\"name\":\"fast\"}", JSON.toJSONString(b));
+            B b = new B();
+            b.id = 100;
+            b.name = "fast";
+            assertEquals("{\"id\":100,\"name\":\"fast\"}", JSON.toJSONString(b));
+        } finally {
+            JSONFactory.setDefaultSkipTransient(true);
+            JSONFactory.getDefaultObjectWriterProvider().clear();
+        }
     }
 
     @JSONType(skipTransient = true)
