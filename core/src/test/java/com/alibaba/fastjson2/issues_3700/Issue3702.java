c3de5e0ab9ae0c6781479caa117ad74b459aa98d
jujn <2087687391@qq.com>
2025-08-07 19:21:49 +0800

    fixed bug for issue#3702

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_3700/Issue3702.java
@@ -0,0 +1,63 @@
+package com.alibaba.fastjson2.issues_3700;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue3702 {
+    @Test
+    public void test() {
+        assertEquals(JSON.toJSONString(new NumberDTO()), "{\"d\":0,\"f\":0,\"i\":0,\"l\":0}");
+        assertEquals(JSON.toJSONString(new NumberDTO2()), "{\"d\":0.0,\"f\":0.0,\"i\":0,\"l\":0}");
+        assertEquals(JSON.toJSONString(new NumberDTO3()), "{}");
+    }
+
+    @Data
+    public class NumberDTO
+            implements Serializable {
+        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+        private Integer i;
+        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+        private Double d;
+        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+        private Float f;
+        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+        private Long l;
+//        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+//        private Byte b;
+//        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+//        private Short s;
+//        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+//        private BigInteger bi;
+//        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+//        private BigDecimal bd;
+//        @JSONField(serializeFeatures = JSONWriter.Feature.WriteNullNumberAsZero)
+//        private Number n;
+    }
+    @Data
+    public class NumberDTO2
+            implements Serializable {
+        @JSONField(serializeFeatures = JSONWriter.Feature.NullAsDefaultValue)
+        private Integer i;
+        @JSONField(serializeFeatures = JSONWriter.Feature.NullAsDefaultValue)
+        private Double d;
+        @JSONField(serializeFeatures = JSONWriter.Feature.NullAsDefaultValue)
+        private Float f;
+        @JSONField(serializeFeatures = JSONWriter.Feature.NullAsDefaultValue)
+        private Long l;
+    }
+    @Data
+    public class NumberDTO3
+            implements Serializable {
+        private Integer integer;
+        private Double wrapperDouble;
+        private Float wrapperFloat;
+        private Long wrapperLong;
+    }
+}
