a678be0c70900b35e482af22bb04dc6df75a5258
高铁 <shaojin.wensj@alibaba-inc.com>
2025-09-21 13:25:17 +0800

    Fix IOUtils.isNumber() edge cases and add tests

--- core/src/main/java/com/alibaba/fastjson2/util/IOUtils.java
+++ core/src/main/java/com/alibaba/fastjson2/util/IOUtils.java
@@ -291,10 +291,14 @@ public class IOUtils {
     }
 
     public static boolean isNumber(String str) {
-        for (int i = 0; i < str.length(); ++i) {
+        int len = str.length();
+        if (len == 0) {
+            return false;
+        }
+        for (int i = 0; i < len; ++i) {
             char ch = str.charAt(i);
             if (ch == '+' || ch == '-') {
-                if (i != 0) {
+                if (i != 0 || len == 1) {
                     return false;
                 }
             } else if (ch < '0' || ch > '9') {
@@ -304,11 +308,14 @@ public class IOUtils {
         return true;
     }
 
-    public static boolean isNumber(char[] chars, int off, int len) {
+    public static boolean isNumber(char[] buf, int off, int len) {
+        if (len <= 0) {
+            return false;
+        }
         for (int i = off, end = off + len; i < end; ++i) {
-            char ch = chars[i];
+            char ch = buf[i];
             if (ch == '+' || ch == '-') {
-                if (i != 0) {
+                if (i != off || len == 1) {
                     return false;
                 }
             } else if (ch < '0' || ch > '9') {
@@ -318,11 +325,14 @@ public class IOUtils {
         return true;
     }
 
-    public static boolean isNumber(byte[] chars, int off, int len) {
+    public static boolean isNumber(byte[] buf, int off, int len) {
+        if (len <= 0) {
+            return false;
+        }
         for (int i = off, end = off + len; i < end; ++i) {
-            char ch = (char) chars[i];
+            char ch = (char) buf[i];
             if (ch == '+' || ch == '-') {
-                if (i != 0) {
+                if (i != off || len == 1) {
                     return false;
                 }
             } else if (ch < '0' || ch > '9') {
