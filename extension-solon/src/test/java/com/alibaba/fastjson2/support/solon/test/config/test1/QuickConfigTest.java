849b5cc0fb99bb3c9e91cf9f1327b67d09aed6c3
noear <noear@live.cn>
2025-11-05 08:28:00 +0800

    style: optimize fastjson2-extension-solon test code

--- extension-solon/src/test/java/com/alibaba/fastjson2/support/solon/test/config/test1/QuickConfigTest.java
+++ extension-solon/src/test/java/com/alibaba/fastjson2/support/solon/test/config/test1/QuickConfigTest.java
@@ -1,9 +1,9 @@
 package com.alibaba.fastjson2.support.solon.test.config.test1;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.support.solon.Fastjson2EntityConverter;
 import com.alibaba.fastjson2.support.solon.test._model.UserDo;
 import org.junit.jupiter.api.Test;
-import org.noear.snack.ONode;
 import org.noear.solon.annotation.Import;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.core.handle.ContextEmpty;
@@ -40,7 +40,7 @@ public class QuickConfigTest {
 
         System.out.println(output);
 
-        assert ONode.load(output).count() == 5;
+        assert JSONObject.parseObject(output).size() == 5;
 
         //完美
         assert "{\"b1\":true,\"d1\":1.0,\"map1\":{\"time\":\"2023-01-16 17:39:53\",\"long\":12,\"int\":12},\"n1\":1,\"s1\":\"noear\"}".equals(output);
