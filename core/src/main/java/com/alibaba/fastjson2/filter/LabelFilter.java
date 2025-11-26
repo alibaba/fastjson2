1794be53cdc41694387ff5722d7f4c23d692b073
jujn <2087687391@qq.com>
2025-11-26 09:12:04 +0800

    adjust class position

--- core/src/main/java/com/alibaba/fastjson2/filter/LabelFilter.java
+++ core/src/main/java/com/alibaba/fastjson2/filter/LabelFilter.java
@@ -1,8 +1,5 @@
 package com.alibaba.fastjson2.filter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface LabelFilter
         extends Filter {
     boolean apply(String label);
@@ -14,34 +11,5 @@ public interface LabelFilter
         }
 
         return new CompositeLabelFilter(before, after);
-    }
-
-    final class CompositeLabelFilter
-            implements LabelFilter {
-        private final List<LabelFilter> filters = new ArrayList<>();
-
-        CompositeLabelFilter(LabelFilter... filters) {
-            for (LabelFilter filter : filters) {
-                if (filter != null) {
-                    this.filters.add(filter);
-                }
-            }
-        }
-
-        void add(LabelFilter filter) {
-            if (filter != null) {
-                this.filters.add(filter);
-            }
-        }
-
-        @Override
-        public boolean apply(String label) {
-            for (LabelFilter filter : filters) {
-                if (!filter.apply(label)) {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
--- core/src/main/java/com/alibaba/fastjson2/filter/PropertyPreFilter.java
+++ core/src/main/java/com/alibaba/fastjson2/filter/PropertyPreFilter.java
@@ -2,9 +2,6 @@ package com.alibaba.fastjson2.filter;
 
 import com.alibaba.fastjson2.JSONWriter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface PropertyPreFilter
         extends Filter {
     boolean process(JSONWriter writer, Object source, String name);
@@ -16,34 +13,5 @@ public interface PropertyPreFilter
         }
 
         return new CompositePropertyPreFilter(before, after);
-    }
-
-    final class CompositePropertyPreFilter
-            implements PropertyPreFilter {
-        private final List<PropertyPreFilter> filters = new ArrayList<>();
-
-        public CompositePropertyPreFilter(PropertyPreFilter... filters) {
-            for (PropertyPreFilter filter : filters) {
-                if (filter != null) {
-                    this.filters.add(filter);
-                }
-            }
-        }
-
-        public void add(PropertyPreFilter filter) {
-            if (filter != null) {
-                this.filters.add(filter);
-            }
-        }
-
-        @Override
-        public boolean process(JSONWriter writer, Object source, String name) {
-            for (PropertyPreFilter filter : filters) {
-                if (!filter.process(writer, source, name)) {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
