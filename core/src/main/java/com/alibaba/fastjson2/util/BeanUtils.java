3fda8753b4b36c4309c8794bcfd7487ecf73199a
jujn <2087687391@qq.com>
2025-12-06 14:15:02 +0800

    fix refactor getRecordMethods to return all declared methods, for issue #3893

--- core/src/main/java/com/alibaba/fastjson2/util/BeanUtils.java
+++ core/src/main/java/com/alibaba/fastjson2/util/BeanUtils.java
@@ -1135,7 +1135,7 @@ public abstract class BeanUtils {
         Method[] methods;
         try {
             if (isRecord(objectClass)) {
-                methods = recordComponentsToMethods(objectClass);
+                methods = getRecordMethods(objectClass);
             } else {
                 methods = objectClass.getMethods();
             }
@@ -1145,7 +1145,7 @@ public abstract class BeanUtils {
         return methods;
     }
 
-    private static Method[] recordComponentsToMethods(Class<?> recordClass) {
+    private static Method[] getRecordMethods(Class<?> recordClass) {
         if (JVM_VERSION < 14 && ANDROID_SDK_INT < 33) {
             return new Method[0];
         }
@@ -1161,13 +1161,47 @@ public abstract class BeanUtils {
             }
 
             final Object[] components = (Object[]) RECORD_GET_RECORD_COMPONENTS.invoke(recordClass);
-            final Method[] methods = new Method[components.length];
+            final Method[] allMethods = recordClass.getMethods();
+
+            String[] componentNames = new String[components.length];
             for (int i = 0; i < components.length; i++) {
-                String componentName = (String) RECORD_COMPONENT_GET_NAME.invoke(components[i]);
-                methods[i] = recordClass.getMethod(componentName);
+                componentNames[i] = (String) RECORD_COMPONENT_GET_NAME.invoke(components[i]);
             }
 
-            return methods;
+            Method[] orderedComponents = new Method[components.length];
+            boolean[] isComponentMethod = new boolean[allMethods.length];
+
+            for (int i = 0; i < allMethods.length; i++) {
+                Method method = allMethods[i];
+                if (method.getParameterCount() == 0) {
+                    String methodName = method.getName();
+
+                    for (int j = 0; j < componentNames.length; j++) {
+                        if (componentNames[j].equals(methodName)) {
+                            if (orderedComponents[j] == null) {
+                                orderedComponents[j] = method;
+                                isComponentMethod[i] = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            Method[] results = new Method[allMethods.length];
+            int index = 0;
+            for (Method m : orderedComponents) {
+                if (m != null) {
+                    results[index++] = m;
+                }
+            }
+
+            for (int i = 0; i < allMethods.length; i++) {
+                if (!isComponentMethod[i]) {
+                    results[index++] = allMethods[i];
+                }
+            }
+
+            return results;
         } catch (Exception e) {
             throw new RuntimeException(String.format(
                     "Failed to access Methods needed to support `java.lang.Record`: (%s) %s",
