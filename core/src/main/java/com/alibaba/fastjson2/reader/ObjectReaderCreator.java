6bbb2fd265ea1450263aebe56d7b27ad7a984f17
LBS <130454150+ban-xiu@users.noreply.github.com>
2026-01-11 01:08:54 +0800

    fix_issus_3926 (#3931)
    * fix_issus_3926
    
    * support generic type resolution for custom collection subclasses and interfaces
    
    * move test
    
    * fix field-based mode
    
    * use resolveCollectionItemType
    
    * fix resolveCollectionItemType
    
    * fix fieldReaderList
    
    * fix fieldReaderList return

--- core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderCreator.java
+++ core/src/main/java/com/alibaba/fastjson2/reader/ObjectReaderCreator.java
@@ -2960,8 +2960,20 @@ public class ObjectReaderCreator {
 
                     return new FieldReaderList(fieldName, fieldTypeResolved, fieldClassResolved, itemType, itemClass, ordinal, features, format, locale, null, jsonSchema, method, null, null);
                 }
+            } else {
+                Type itemType = BeanUtils.resolveCollectionItemType(fieldTypeResolved, fieldClass);
+                Class itemClass;
+                if (itemType == null) {
+                    itemType = Object.class;
+                    itemClass = Object.class;
+                } else {
+                    itemClass = TypeUtils.getMapping(itemType);
+                    if (itemClass == String.class) {
+                        return new FieldReaderList(fieldName, fieldTypeResolved != null ? fieldTypeResolved : fieldType, fieldClassResolved != null ? fieldClassResolved : fieldClass, String.class, String.class, ordinal, features, format, locale, null, jsonSchema, method, null, null);
+                    }
+                }
+                return new FieldReaderList(fieldName, fieldTypeResolved != null ? fieldTypeResolved : fieldType, fieldClassResolved != null ? fieldClassResolved : fieldClass, itemType, itemClass, ordinal, features, format, locale, null, jsonSchema, method, null, null);
             }
-            return new FieldReaderList(fieldName, fieldType, fieldClass, Object.class, Object.class, ordinal, features, format, locale, null, jsonSchema, method, null, null);
         }
 
         if (fieldClass == Date.class) {
@@ -3367,6 +3379,8 @@ public class ObjectReaderCreator {
                 if (actualTypeArguments.length > 0) {
                     itemType = actualTypeArguments[0];
                 }
+            } else {
+                itemType = BeanUtils.resolveCollectionItemType(fieldTypeResolved, fieldClass);
             }
             if (itemType == null) {
                 itemType = Object.class;
