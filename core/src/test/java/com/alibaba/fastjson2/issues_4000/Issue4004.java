80ae6ed286cf62d1ece9302cf5aa49a96f7c0cee
zhou yong kang <mengnankkzhou@gmail.com>
2026-03-04 15:58:38 +0800

    fix: package

--- core/src/test/java/com/alibaba/fastjson2/issues_4000/Issue4004.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_4000/Issue4004.java
@@ -1,4 +1,4 @@
-package com.alibaba.fastjson2.issues;
+package com.alibaba.fastjson2.issues_4000;
 
 import com.alibaba.fastjson2.JSON;
 import org.junit.jupiter.api.Test;
@@ -23,19 +23,16 @@ public class Issue4004 {
     @Test
     public void test() {
         String json1 = "{\n  \"birthday\": \"2025-01-02\"\n}";
-
         Person p1 = JSON.parseObject(json1, Person.class);
         assertNotNull(p1);
         assertEquals(LocalDate.of(2025, 1, 2), p1.getBirthday());
 
         String json2 = "{\n  \"birthday\": \"2025-01-02\",\n}";
-
         Person p2 = JSON.parseObject(json2, Person.class);
         assertNotNull(p2);
         assertEquals(LocalDate.of(2025, 1, 2), p2.getBirthday());
 
         String json3 = "{\n  \"birthday\": \"2025-01-02 00:00:00\"\n}";
-
         Person p3 = JSON.parseObject(json3, Person.class);
         assertNotNull(p3);
         assertEquals(LocalDate.of(2025, 1, 2), p3.getBirthday());
