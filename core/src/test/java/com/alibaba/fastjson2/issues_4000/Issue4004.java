a9675fd31934e850c42f0770c60246c2d45d0d57
mengnankkkk <mengnankkzhou@gmail.com>
2026-03-11 23:05:12 +0800

    fix: add test

--- core/src/test/java/com/alibaba/fastjson2/issues_4000/Issue4004.java
+++ core/src/test/java/com/alibaba/fastjson2/issues_4000/Issue4004.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson2.JSON;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +18,18 @@ public class Issue4004 {
 
         public void setBirthday(LocalDate birthday) {
             this.birthday = birthday;
+        }
+    }
+
+    public static class Event {
+        private OffsetDateTime timestamp;
+
+        public OffsetDateTime getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(OffsetDateTime timestamp) {
+            this.timestamp = timestamp;
         }
     }
 
@@ -36,5 +49,18 @@ public class Issue4004 {
         Person p3 = JSON.parseObject(json3, Person.class);
         assertNotNull(p3);
         assertEquals(LocalDate.of(2025, 1, 2), p3.getBirthday());
+    }
+
+    @Test
+    public void testOffsetDateTime() {
+        String json1 = "{\n  \"timestamp\": \"2025-01-02T10:30:00+08:00\"\n}";
+        Event e1 = JSON.parseObject(json1, Event.class);
+        assertNotNull(e1);
+        assertNotNull(e1.getTimestamp());
+
+        String json2 = "{\n  \"timestamp\": \"2025-01-02T10:30:00+08:00\",\n}";
+        Event e2 = JSON.parseObject(json2, Event.class);
+        assertNotNull(e2);
+        assertNotNull(e2.getTimestamp());
     }
 }
