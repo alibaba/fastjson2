27aa75cd06b723d9c39256428616dd7cb59458ac
WordDealer <1711383@mail.nankai.edu.cn>
2026-08-22 16:18:56 +0800

    fix: guard truncated escapes across JSONReaderUTF16

--- core/src/test/java/com/alibaba/fastjson2/JSONReaderUTF16Test.java
+++ core/src/test/java/com/alibaba/fastjson2/JSONReaderUTF16Test.java
@@ -2,8 +2,6 @@ package com.alibaba.fastjson2;
 
 import org.junit.jupiter.api.Test;
 
-import java.nio.charset.StandardCharsets;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JSONReaderUTF16Test {
@@ -13,13 +11,65 @@ public class JSONReaderUTF16Test {
                 "\"\\",
                 "{\"a\":\"\\",
                 "[\"\\",
-                "\"ab\\"
+                "\"ab\\",
+                "\"中\\"
         };
 
         for (String input : inputs) {
             assertThrows(JSONException.class, () -> JSON.parse(input));
             assertThrows(JSONException.class, () -> JSON.parse(input.toCharArray()));
-            assertThrows(JSONException.class, () -> JSON.parse(input.getBytes(StandardCharsets.UTF_8)));
         }
+    }
+
+    @Test
+    public void truncatedEscapePublicPaths() {
+        assertThrows(JSONException.class, () -> JSON.parse("{\"a\\".toCharArray()));
+        String[] numberInputs = {
+                "{\"a\":\"\\",
+                "{\"a\":\"\\u",
+                "{\"a\":\"\\x"
+        };
+        for (String input : numberInputs) {
+            assertThrows(JSONException.class, () -> JSON.parseObject(input.toCharArray(), Bean.class));
+        }
+
+        assertThrows(JSONException.class, () -> JSON.parseObject("{\"x\":\"\\".toCharArray(), Bean.class));
+        assertThrows(
+                JSONException.class,
+                () -> JSON.parseObject("{a\\".toCharArray(), Object.class, JSONReader.Feature.AllowUnQuotedFieldNames)
+        );
+        assertThrows(
+                JSONException.class,
+                () -> JSON.parseObject("{a\\x".toCharArray(), Object.class, JSONReader.Feature.AllowUnQuotedFieldNames)
+        );
+        assertThrows(JSONException.class, () -> JSON.parseObject("\"ab\\".toCharArray(), Value.class));
+
+        for (String input : new String[]{"\"a\\u", "\"a\\x"}) {
+            try (JSONReader jsonReader = JSONReader.of(input.toCharArray())) {
+                assertThrows(JSONException.class, jsonReader::skipName);
+            }
+        }
+    }
+
+    @Test
+    public void truncatedEscapeInSubRange() {
+        char[] chars = "\"ab\\\"".toCharArray();
+        try (JSONReader jsonReader = JSONReader.of(chars, 0, 4)) {
+            assertThrows(JSONException.class, jsonReader::readString);
+        }
+
+        char[] unquotedName = "{a\\x12:1}".toCharArray();
+        try (JSONReader jsonReader = JSONReader.of(unquotedName, 0, 4)) {
+            jsonReader.getContext().config(JSONReader.Feature.AllowUnQuotedFieldNames);
+            assertThrows(JSONException.class, jsonReader::readObject);
+        }
+    }
+
+    public static class Bean {
+        public Number a;
+    }
+
+    public enum Value {
+        A
     }
 }
